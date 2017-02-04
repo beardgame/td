77d0edbaa2669cc9a6835b70701d28d4c89fe21c
amulware <amul@amulware.net>
2017-02-04 19:11:26 +0100

    :tada: Setup solution and main project

---
+++ src/Bearded.TD/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bearded.TD
+{
+    static class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("The game. You lost it.");
+        }
+    }
+}
