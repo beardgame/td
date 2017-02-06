771e89e6daf7350b9b9d2be1c29cc45b5e6833a8
amulware <amul@amulware.net>
2017-02-06 19:01:00 +0100

    :construction: Add game renderer

---
+++ src/Bearded.TD/Game/GameRenderer.cs
@@ -0,0 +1,20 @@
+﻿namespace Bearded.TD
+{
+    internal class GameRenderer
+    {
+        private readonly GameState state;
+
+        public GameRenderer(GameState state)
+        {
+            this.state = state;
+        }
+
+        public void Draw()
+        {
+            foreach (var obj in state.GameObjects)
+            {
+                obj.Draw();
+            }
+        }
+    }
+}
