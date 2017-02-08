6cd27a72aa7f2cd8100fdfe39f9dfcdc41ca1707
Tom <tom@tomrijnbeek.nl>
2017-02-08 22:18:01 +0000

    :construction: Add support for commands.

---
+++ src/Bearded.TD/Utilities/Console/CommandAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bearded.TD.Console
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    internal class CommandAttribute : Attribute
+    {
+        public CommandAttribute(string name, string parameterCompletion = null)
+        {
+            this.Name = name;
+            this.ParameterCompletion = parameterCompletion;
+        }
+
+        public string Name { get; }
+        public string ParameterCompletion { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    internal class DebugCommandAttribute : CommandAttribute
+    {
+        public DebugCommandAttribute(string name, string parameterCompletion = null)
+            : base(name, parameterCompletion)
+        {
+        }
+    }
+}
