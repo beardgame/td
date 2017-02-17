e312393b4d95d7fda7290be1f161d86058a9dc49
Tom <tom@tomrijnbeek.nl>
2017-02-17 23:33:59 +0000

    :bug::gun: prevent some future bugs.

--- src/Bearded.TD/EntryPoint.cs
+++ src/Bearded.TD/EntryPoint.cs
@@ -1,4 +1,7 @@
-﻿using Bearded.Utilities;
+﻿using System.Globalization;
+using System.Threading;
+using Bearded.Utilities;
+using OpenTK;
 
 namespace Bearded.TD
 {
@@ -6,16 +9,22 @@ namespace Bearded.TD
     {
         public static void Main(string[] args)
         {
-            var logger = new Logger();
+            using (Toolkit.Init(new ToolkitOptions() {Backend = PlatformBackend.PreferNative}))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
 
-            logger.Info.Log("");
-            logger.Info.Log("Creating game");
-            var game = new TheGame(logger);
+                var logger = new Logger();
 
-            logger.Info.Log("Running game");
-            game.Run(60);
+                logger.Info.Log("");
+                logger.Info.Log("Creating game");
+                var game = new TheGame(logger);
 
-            logger.Info.Log("Safely exited game");
+                logger.Info.Log("Running game");
+                game.Run(60);
+
+                logger.Info.Log("Safely exited game");
+            }
         }
     }
 }
