61b947756b35b80a6024f59a6a27a8f730466400
amulware <amul@amulware.net>
2017-02-18 17:38:45 +0100

    Complicate TileInfo

--- src/Bearded.TD/Game/World/TileInfo.cs
+++ src/Bearded.TD/Game/World/TileInfo.cs
@@ -1,7 +1,28 @@
-﻿namespace Bearded.TD.Game.World
+﻿using Bearded.TD.Game.Tilemap;
+
+namespace Bearded.TD.Game.World
 {
     class TileInfo
     {
+        private readonly Directions validDirections;
+
+        public Directions OpenDirections { get; private set; }
+
+        public TileInfo(Directions validDirections)
+        {
+            this.validDirections = validDirections;
+            OpenDirections = validDirections;
+        }
+
+        public void CloseTo(Direction direction)
+        {
+            OpenDirections = OpenDirections.Except(direction);
+        }
+
+        public void OpenTo(Direction direction)
+        {
+            OpenDirections = OpenDirections.And(direction).Intersect(validDirections);
+        }
 
     }
 }
