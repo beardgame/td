b17f72da4d35b6d9147aecd458a9086a7a6b79db
amulware <amul@amulware.net>
2017-03-05 19:39:17 +0100

    :skull: Add UnitDeath command

---
+++ src/Bearded.TD/Game/Commands/UnitDeath.cs
@@ -0,0 +1,19 @@
+﻿using Bearded.TD.Commands;
+using Bearded.TD.Game.Units;
+
+namespace Bearded.TD.Game.Commands
+{
+    class UnitDeath : ICommand
+    {
+        public static ICommand Command(GameUnit unit) => new UnitDeath(unit);
+
+        private readonly GameUnit unit;
+
+        private UnitDeath(GameUnit unit)
+        {
+            this.unit = unit;
+        }
+
+        public void Execute() => unit.Kill();
+    }
+}
