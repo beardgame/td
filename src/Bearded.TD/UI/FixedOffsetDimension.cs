10ba4895985433e8309f0983c3b8dca5b7ff5473
Tom <tom@tomrijnbeek.nl>
2017-03-05 17:46:09 +0000

    :lipstick: Rename `FixedOffsetDimension`

--- src/Bearded.TD/UI/FixedOffsetDimension.cs
+++ src/Bearded.TD/UI/FixedOffsetDimension.cs
@@ -1,12 +1,12 @@
 ﻿namespace Bearded.TD.UI
 {
-    class FixedOffsetComponent : IDimension
+    class FixedOffsetDimension : IDimension
     {
         private readonly IDimension parent;
         private readonly float offsetStart;
         private readonly float offsetEnd;
 
-        public FixedOffsetComponent(IDimension parent, float offsetStart, float offsetEnd)
+        public FixedOffsetDimension(IDimension parent, float offsetStart, float offsetEnd)
         {
             this.parent = parent;
             this.offsetStart = offsetStart;
