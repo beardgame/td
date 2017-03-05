d6b713a1979d6855607dbd0b1ca0530d2cda9cdd
amulware <amul@amulware.net>
2017-03-05 19:57:06 +0100

    :skull: Add GameOver command

---
+++ src/Bearded.TD/Game/Commands/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using Bearded.TD.Commands;
+
+namespace Bearded.TD.Game.Commands
+{
+    class GameOver : ICommand
+    {
+        public static ICommand Command(GameObject obj) => new GameOver(obj.Game);
+
+        private readonly GameState game;
+
+        private GameOver(GameState game)
+        {
+            this.game = game;
+        }
+
+        public void Execute() => game.Meta.DoGameOver();
+    }
+}
