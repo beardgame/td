e48dea5d2b9de520b8821b60751b1ee3446baa04
amulware <amul@amulware.net>
2017-03-31 21:05:13 +0200

    :video_game: Add non static input manager

---
+++ src/Bearded.TD/Utilities/AsyncAtomicUpdating.cs
@@ -0,0 +1,27 @@
+﻿using Bearded.Utilities;
+
+namespace Bearded.TD.Utilities
+{
+    class AsyncAtomicUpdating<T>
+        where T : struct
+    {
+        public T Current { get; private set; }
+        public T Previous { get; private set; }
+        private Box<T> lastRecorded = new Box<T>(default(T));
+
+        public void SetLastKnownState(T state)
+        {
+            lastRecorded = Box.This(state);
+        }
+
+        public void Update() => UpdateTo(lastRecorded.Value);
+
+        public void UpdateToDefault() => UpdateTo(default(T));
+
+        public void UpdateTo(T state)
+        {
+            Previous = Current;
+            Current = state;
+        }
+    }
+}
---
+++ src/Bearded.TD/Utilities/Input/GamePadStateManager.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK.Input;
+
+namespace Bearded.TD.Utilities.Input
+{
+    class GamePadStateManager
+    {
+        public int Id { get; }
+
+        public AsyncAtomicUpdating<GamePadState> State { get; } = new AsyncAtomicUpdating<GamePadState>();
+
+        public static GamePadStateManager ForId(int id) => new GamePadStateManager(id);
+
+        private GamePadStateManager(int id)
+        {
+            Id = id;
+        }
+
+        public void ProcessEventsAsync() => State.SetLastKnownState(GamePad.GetState(this.Id));
+
+        public void Update(bool windowIsActive)
+        {
+            if (windowIsActive)
+            {
+                State.Update();
+            }
+            else
+            {
+                State.UpdateTo(default(GamePadState));
+            }
+        }
+    }
+}
