0629d287e7fe38786e20596c7922ebc17e6dd9ee
Tom <tom@tomrijnbeek.nl>
2017-04-22 15:35:08 +0100

    :pencil2: Extract TextBox component for reuse with chat.

--- src/Bearded.TD/UI/Components/ConsoleTextComponent.cs
+++ src/Bearded.TD/UI/Components/ConsoleTextComponent.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using amulware.Graphics;
-using Bearded.TD.Rendering;
 using Bearded.Utilities;
-using OpenTK;
 
 namespace Bearded.TD.UI.Components
 {
-    class ConsoleTextComponent : UIComponent
+    class ConsoleTextComponent : TextBox<Logger.Entry>
     {
         private static readonly Dictionary<Logger.Severity, Color> colors = new Dictionary<Logger.Severity, Color>
         {
@@ -17,10 +16,7 @@ namespace Bearded.TD.UI.Components
             { Logger.Severity.Debug, Color.SpringGreen },
             { Logger.Severity.Trace, Color.SkyBlue },
         };
-
-        private const float fontSize = 14;
-        private const float lineHeight = 16;
-            
+        
 #if DEBUG
         private static readonly HashSet<Logger.Severity> visibleSeverities = new HashSet<Logger.Severity>
         {
@@ -33,32 +29,17 @@ namespace Bearded.TD.UI.Components
             Logger.Severity.Fatal, Logger.Severity.Error, Logger.Severity.Warning, Logger.Severity.Info
         };
 #endif
-
-        private readonly Logger logger;
-
-        public ConsoleTextComponent(Bounds bounds, Logger logger) : base(bounds)
+        
+        public ConsoleTextComponent(Bounds bounds, Logger logger)
+            : base(bounds, () => getLoggerEntries(logger), formatLoggerEntry)
         {
-            this.logger = logger;
         }
 
-        public override void Draw(GeometryManager geometries)
+        private static IList<Logger.Entry> getLoggerEntries(Logger logger)
         {
-            var logEntries = logger.GetSafeRecentEntries();
-
-            geometries.ConsoleFont.SizeCoefficient = new Vector2(1, 1);
-            geometries.ConsoleFont.Height = fontSize;
-
-            var y = Bounds.YEnd - lineHeight;
-            var i = logEntries.Count;
-
-            while (y >= -lineHeight && i > 0)
-            {
-                var entry = logEntries[--i];
-                if (!visibleSeverities.Contains(entry.Severity)) continue;
-                geometries.ConsoleFont.Color = colors[logEntries[i].Severity];
-                geometries.ConsoleFont.DrawString(new Vector2(Bounds.XStart, y), logEntries[i].Text);
-                y -= lineHeight;
-            }
+            return logger.GetSafeRecentEntries().Where(entry => visibleSeverities.Contains(entry.Severity)).ToList();
         }
+
+        private static (string, Color) formatLoggerEntry(Logger.Entry entry) => (entry.Text, colors[entry.Severity]);
     }
 }
---
+++ src/Bearded.TD/UI/Components/TextBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using amulware.Graphics;
+using Bearded.TD.Rendering;
+using OpenTK;
+
+namespace Bearded.TD.UI.Components
+{
+    class TextBox<T> : UIComponent
+    {
+        private const float fontSize = 14;
+        private const float lineHeight = 16;
+
+        private readonly Func<IList<T>> itemProvider;
+        private readonly Func<T, (string, Color)> itemTransformer;
+
+        public TextBox(Bounds bounds, Func<IList<T>> itemProvider, Func<T, (string, Color)> itemTransformer) : base(bounds)
+        {
+            this.itemProvider = itemProvider;
+            this.itemTransformer = itemTransformer;
+        }
+
+        public override void Draw(GeometryManager geometries)
+        {
+            var entries = itemProvider();
+
+            geometries.ConsoleFont.SizeCoefficient = new Vector2(1, 1);
+            geometries.ConsoleFont.Height = fontSize;
+
+            var y = Bounds.YEnd - lineHeight;
+            var i = entries.Count;
+
+            while (y >= -lineHeight && i > 0)
+            {
+                (var text, var color) = itemTransformer(entries[--i]);
+                geometries.ConsoleFont.Color = color;
+                geometries.ConsoleFont.DrawString(new Vector2(Bounds.XStart, y), text);
+                y -= lineHeight;
+            }
+        }
+    }
+}
