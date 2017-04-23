65b53acddca2f30604661ecc4e7f33e9f82eafef
Tom <tom@tomrijnbeek.nl>
2017-04-23 14:57:48 +0100

    :speech_balloon: Add chat UI component.

---
+++ src/Bearded.TD/UI/Components/ChatComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using amulware.Graphics;
+using Bearded.TD.Game.UI;
+
+namespace Bearded.TD.UI.Components
+{
+    class ChatComponent : CompositeComponent
+    {
+        private const float textInputHeight = 16;
+        private static readonly Color textColor = Color.White;
+        
+        private readonly TextInput textInput;
+
+        public ChatComponent(Bounds bounds, ChatLog chatLog) : base(bounds)
+        {
+            AddComponent(new TextBox<ChatMessage>(
+                Bounds.Within(bounds, 0, 0, textInputHeight, 0), () => chatLog.Messages.ToList(), msg => (msg.Text, textColor)));
+            AddComponent(textInput = new TextInput(Bounds.Within(bounds, bounds.Height - textInputHeight, 0, 0, 0)));
+
+            textInput.Submitted += sendChatMessage;
+        }
+
+        private void sendChatMessage(string chatMessage)
+        {
+            // Send chat message request.
+            textInput.Text = "";
+        }
+    }
+}
---
+++ src/Bearded.TD/UI/Components/CompositeComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using amulware.Graphics;
+using Bearded.TD.Rendering;
+
+namespace Bearded.TD.UI.Components
+{
+    abstract class CompositeComponent : UIComponent
+    {
+        private readonly List<UIComponent> components = new List<UIComponent>();
+
+        protected CompositeComponent(Bounds bounds) : base(bounds)
+        {
+        }
+
+        public override void Update(UpdateEventArgs args)
+        {
+            components.ForEach(c => c.Update(args));
+        }
+
+        public override void HandleInput(InputState inputState)
+        {
+            components.ForEach(c => c.HandleInput(inputState));
+        }
+
+        public override void Draw(GeometryManager geometries)
+        {
+            components.ForEach(c => c.Draw(geometries));
+        }
+
+        protected void AddComponent(UIComponent component)
+        {
+            components.Add(component);
+        }
+    }
+}
