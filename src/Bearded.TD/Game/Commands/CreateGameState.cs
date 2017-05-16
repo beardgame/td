7fe486c145abddf4a7cef1cab8b4e81fd19e0372
amulware <amul@amulware.net>
2017-05-16 22:19:44 +0200

    :bug: :poop:

--- src/Bearded.TD/Game/Commands/CreateGameState.cs
+++ src/Bearded.TD/Game/Commands/CreateGameState.cs
@@ -23,7 +23,7 @@ namespace Bearded.TD.Game.Commands
 
             public void Execute()
             {
-                var tilemap = new Tilemap<TileInfo>(radius);
+                var tilemap = new Tilemap<TileInfo>(radius, _ => new TileInfo(Directions.None, TileInfo.Type.Unknown));
                 var state = new GameState(game.Meta, new Level(tilemap));
                 game.InitialiseState(state);
             }
