93d3557ab3fb0cd4099520030e55061fa275cbb4
amulware <amul@amulware.net>
2017-05-27 01:11:06 +0200

    :paw_prints: Add footprint deserialisation and files

---
+++ src/Bearded.TD/Mods/Serialization/Converters/StepConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Bearded.TD.Game.Tiles;
+using Newtonsoft.Json;
+
+namespace Bearded.TD.Mods.Serialization.Converters
+{
+    sealed class StepConverter : JsonConverterBase<Step>
+    {
+        private static readonly Dictionary<string, Step> namedDirections =
+            Directions.All.Enumerate()
+                .ToDictionary(
+                    d => d == Direction.Unknown
+                        ? "base"
+                        // ReSharper disable once PossibleNullReferenceException
+                        : Enum.GetName(typeof(Direction), d).ToLowerInvariant(),
+                    d => d.Step()
+                );
+
+        protected override Step ReadJson(JsonReader reader, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                return fromString(reader);
+
+            throw new InvalidDataException($"Expected string value, encountered {reader.TokenType} when parsing step.");
+        }
+
+        private static Step fromString(JsonReader reader)
+        {
+            var value = reader.ReadAsString();
+            var name = value.ToLowerInvariant();
+
+            if (namedDirections.TryGetValue(name, out var step))
+                return step;
+
+            throw new InvalidDataException($"Encountered unknown step name {value}");
+        }
+    }
+}
