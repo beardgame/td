f697fb2f574cb85098020cc0c08da687a8ac14bb
Tom <tom@tomrijnbeek.nl>
2017-05-28 17:26:24 +0100

    :construction: Make footprint groups IIdable

--- src/Bearded.TD/Game/Tiles/FootprintGroup.cs
+++ src/Bearded.TD/Game/Tiles/FootprintGroup.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using Bearded.TD.Utilities;
 
 namespace Bearded.TD.Game.Tiles
 {
-    sealed class FootprintGroup
+    sealed class FootprintGroup : IIdable<FootprintGroup>
     {
-        public static FootprintGroup Single { get; } = new FootprintGroup(Footprint.Single);
-        public static FootprintGroup Triangle { get; } = new FootprintGroup(Footprint.TriangleUp, Footprint.TriangleDown);
-        public static FootprintGroup CircleSeven { get; } = new FootprintGroup(Footprint.CircleSeven);
+        public static FootprintGroup Single { get; } = new FootprintGroup(new Id<FootprintGroup>(1), Footprint.Single);
+        public static FootprintGroup Triangle { get; } = new FootprintGroup(new Id<FootprintGroup>(2), Footprint.TriangleUp, Footprint.TriangleDown);
+        public static FootprintGroup CircleSeven { get; } = new FootprintGroup(new Id<FootprintGroup>(3), Footprint.CircleSeven);
 
+        public Id<FootprintGroup> Id { get; }
         public ReadOnlyCollection<Footprint> Footprints { get; }
 
-        public FootprintGroup(IEnumerable<Footprint> footprints)
+        public FootprintGroup(Id<FootprintGroup> id, IEnumerable<Footprint> footprints)
         {
+            Id = id;
             Footprints = footprints.ToList().AsReadOnly();
 
             if (Footprints.Count == 0)
                 throw new ArgumentException("Footprint group must have at least one footprint.");
         }
 
-        public FootprintGroup(params Footprint[] footprints)
-            : this((IEnumerable<Footprint>)footprints)
+        public FootprintGroup(Id<FootprintGroup> id, params Footprint[] footprints)
+            : this(id, (IEnumerable<Footprint>)footprints)
         {
         }
     }
