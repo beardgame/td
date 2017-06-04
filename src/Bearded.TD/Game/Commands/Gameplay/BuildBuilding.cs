7b4c9c91bb1ab10f7d8001f653a678744a29a6e6
Tom <tom@tomrijnbeek.nl>
2017-06-04 17:26:49 +0100

    :construction: Start implementing BuildBuilding serializer.

--- src/Bearded.TD/Game/Commands/Gameplay/BuildBuilding.cs
+++ src/Bearded.TD/Game/Commands/Gameplay/BuildBuilding.cs
@@ -3,21 +3,23 @@ using Bearded.TD.Commands;
 using Bearded.TD.Game.Buildings;
 using Bearded.TD.Game.Resources;
 using Bearded.TD.Game.Tiles;
+using Bearded.TD.Networking.Serialization;
+using Bearded.TD.Utilities;
 
 namespace Bearded.TD.Game.Commands
 {
     static class BuildBuilding
     {
-        public static IRequest Request(GameState game, BuildingBlueprint blueprint, PositionedFootprint footprint)
+        public static IRequest Request(GameInstance game, BuildingBlueprint blueprint, PositionedFootprint footprint)
             => new Implementation(game, blueprint, footprint);
 
         private class Implementation : UnifiedRequestCommand
         {
-            private readonly GameState game;
+            private readonly GameInstance game;
             private readonly BuildingBlueprint blueprint;
             private readonly PositionedFootprint footprint;
 
-            public Implementation(GameState game, BuildingBlueprint blueprint, PositionedFootprint footprint)
+            public Implementation(GameInstance game, BuildingBlueprint blueprint, PositionedFootprint footprint)
             {
                 this.game = game;
                 this.blueprint = blueprint;
@@ -25,18 +27,44 @@ namespace Bearded.TD.Game.Commands
             }
 
             public override bool CheckPreconditions()
+                // TODO: check if the positionedfootprint matches the building blueprint conditions
                 => footprint.OccupiedTiles.All(tile => tile.IsValid && tile.Info.IsPassable);
 
             public override void Execute()
             {
                 var building = new PlayerBuilding(blueprint, footprint);
-                game.Add(building);
-                game.Add(new DebugWorker(building.BuildManager));
+                game.State.Add(building);
+                game.State.Add(new DebugWorker(building.BuildManager));
             }
 
-            protected override UnifiedRequestCommandSerializer GetSerializer()
+            protected override UnifiedRequestCommandSerializer GetSerializer() => new Serializer(blueprint, footprint);
+        }
+
+        private class Serializer : UnifiedRequestCommandSerializer
+        {
+            private Id<BuildingBlueprint> blueprint;
+            private PositionedFootprint footprint;
+
+            // ReSharper disable once UnusedMember.Local
+            public Serializer()
             {
-                throw new System.NotImplementedException();
+            }
+
+            public Serializer(BuildingBlueprint blueprint, PositionedFootprint footprint)
+            {
+                this.blueprint = blueprint.Id;
+                this.footprint = footprint;
+            }
+
+            protected override UnifiedRequestCommand GetSerialized(GameInstance game)
+            {
+                return new Implementation(game, game.Blueprints.Buildings[blueprint], footprint);
+            }
+
+            public override void Serialize(INetBufferStream stream)
+            {
+                stream.Serialize(ref blueprint);
+                // TODO: serialize footprint
             }
         }
     }
