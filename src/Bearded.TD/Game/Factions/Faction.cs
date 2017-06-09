04f7e3a6df1e5d649b0f1a897501c547d22f6dc3
Tom <tom@tomrijnbeek.nl>
2017-06-09 20:21:03 +0100

    :construction: Add Faction class

---
+++ src/Bearded.TD/Game/Factions/Faction.cs
@@ -0,0 +1,14 @@
+﻿using Bearded.TD.Utilities;
+
+namespace Bearded.TD.Game.Factions
+{
+    class Faction : IIdable<Faction>
+    {
+        public Id<Faction> Id { get; }
+
+        public Faction(Id<Faction> id, Faction parent)
+        {
+            Id = id;
+        }
+    }
+}
