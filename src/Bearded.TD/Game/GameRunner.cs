cd4bb64741587f15b76f8dd2a8e4ef0d3a6f0e9c
Tom <tom@tomrijnbeek.nl>
2017-06-30 21:00:46 +0100

    :bug: Fix faction resource distribution.

--- src/Bearded.TD/Game/GameRunner.cs
+++ src/Bearded.TD/Game/GameRunner.cs
@@ -37,7 +37,8 @@ namespace Bearded.TD.Game
             if (game.State.Meta.GameOver) return;
             var elapsedTime = new TimeSpan(args.ElapsedTimeInS);
             foreach (var f in game.State.Factions)
-                f.Resources.DistributeResources(elapsedTime);
+                if (f.HasResources)
+                    f.Resources.DistributeResources(elapsedTime);
             game.State.Navigator.Update();
             game.State.Advance(elapsedTime);
         }
