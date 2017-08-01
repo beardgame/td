95e1ff988dd3cb060c97dd8f4be50657ea736180
amulware <amul@amulware.net>
2017-08-01 23:53:59 +0200

    :mount_fuji: Improve level geometry

--- src/Bearded.TD/Rendering/Deferred/LevelGeometry.cs
+++ src/Bearded.TD/Rendering/Deferred/LevelGeometry.cs
@@ -21,44 +21,99 @@ namespace Bearded.TD.Rendering.Deferred
 
         public void DrawTile(Vector2 position, bool isOpen, Directions openDirections)
         {
-            /*   p2
-             *  /n0\
-             * p0--p1
-             *  \n1/
-             *   p3
-             */
+            var leftIsOpen = openDirections.Includes(Left);
+            var downIsOpen = openDirections.Includes(DownLeft);
+            var upIsOpen = openDirections.Includes(UpLeft);
 
-            var open0 = isOpen;
-            var open1 = openDirections.Includes(Left);
-            var open2 = openDirections.Includes(DownLeft);
-            var open3 = openDirections.Includes(UpLeft);
+            var floorZ = 0f;
+            var wallZ = 0.5f;
 
-            const float z = 1f;
+            var selfZ = isOpen ? floorZ : wallZ;
+            var leftZ = leftIsOpen ? floorZ : wallZ;
+            var downZ = downIsOpen ? floorZ : wallZ;
+            var upZ = upIsOpen ? floorZ : wallZ;
 
-            var p0 = position.WithZ(open0 ? 0 : z);
-            var p1 = (position + new Vector2(HexagonDistanceX, 0)).WithZ(open1 ? 0 : z);
-            var p2 = (position + new Vector2(HexagonDistanceX * 0.5f, HexagonDistanceY)).WithZ(open2 ? 0 : z);
-            var p3 = (position + new Vector2(HexagonDistanceX * 0.5f, -HexagonDistanceY)).WithZ(open3 ? 0 : z);
+            var selfScale = 0.8f;
+            var leftScale = 0.8f;
+            var downScale = 0.8f;
+            var upScale = 0.8f;
+            
+            var selfCorners = cornerOffsetsWithScale(selfScale);
 
-            var c0 = open0 ? openColor : closedColor;
-            var c1 = open1 ? openColor : closedColor;
-            var c2 = open2 ? openColor : closedColor;
-            var c3 = open3 ? openColor : closedColor;
+            var topV = (position + new Vector2(0, selfCorners.topY)).WithZ(selfZ);
+            var bottomV = (position + new Vector2(0, -selfCorners.topY)).WithZ(selfZ);
+            var topLeftV = (position + new Vector2(-selfCorners.sideX, selfCorners.sideY)).WithZ(selfZ);
+            var bottomLeftV = (position + new Vector2(-selfCorners.sideX, -selfCorners.sideY)).WithZ(selfZ);
+            var topRightV = (position + new Vector2(selfCorners.sideX, selfCorners.sideY)).WithZ(selfZ);
+            var bottomRightV = (position + new Vector2(selfCorners.sideX, -selfCorners.sideY)).WithZ(selfZ);
 
-            var n0 = Vector3.Cross(p1 - p0, p2 - p0).Normalized();
-            var n1 = Vector3.Cross(p3 - p0, p1 - p0).Normalized();
+            var n = Vector3.UnitZ;
+            var c = openColor;
 
-            var nUp = Vector3.UnitZ;
+            addTriangle(topV, topRightV, bottomRightV, n, c);
+            addTriangle(topV, bottomRightV, bottomV, n, c);
+            addTriangle(topV, bottomV, bottomLeftV, n, c);
+            addTriangle(topV, bottomLeftV, topLeftV, n, c);
 
+            var leftCorners = cornerOffsetsWithScale(leftScale);
+            var downCorners = cornerOffsetsWithScale(downScale);
+            var upCorners = cornerOffsetsWithScale(upScale);
+
+            var leftPosition = position + new Vector2(HexagonDistanceX, 0);
+            var upPosition = position + new Vector2(HexagonDistanceX / 2, HexagonDistanceY);
+            var downPosition = position + new Vector2(HexagonDistanceX / 2, -HexagonDistanceY);
+
+            var upBottomLeftV = (upPosition + new Vector2(-upCorners.sideX, -upCorners.sideY)).WithZ(downZ);
+            var upBottomV = (upPosition + new Vector2(0, -upCorners.topY)).WithZ(downZ);
+
+            addQuad(topV, upBottomLeftV, upBottomV, topRightV, c);
+
+            var downTopV = (downPosition + new Vector2(0, downCorners.topY)).WithZ(upZ);
+            var downTopLeftV = (downPosition + new Vector2(-selfCorners.sideX, selfCorners.sideY)).WithZ(upZ);
+
+            addQuad(bottomV, bottomRightV, downTopV, downTopLeftV, c);
+
+            var leftTopLeftV = (leftPosition + new Vector2(-leftCorners.sideX, leftCorners.sideY)).WithZ(leftZ);
+            var leftBottomLeftV = (leftPosition + new Vector2(-leftCorners.sideX, -leftCorners.sideY)).WithZ(leftZ);
+
+            addQuad(topRightV, leftTopLeftV, leftBottomLeftV, bottomRightV, c);
+
+            addTriangle(topRightV, upBottomV, leftTopLeftV, c);
+            addTriangle(bottomRightV, leftBottomLeftV, downTopV, c);
+        }
+
+        private static (float topY, float sideY, float sideX) cornerOffsetsWithScale(float scale)
+            => (HexagonSide * scale, HexagonSide / 2 * scale, HexagonWidth / 2 * scale);
+
+        private void addTriangle(Vector3 v0, Vector3 v1, Vector3 v2, Color c)
+        {
+            var normal = -Vector3.Cross(v1 - v0, v2 - v0).Normalized();
+            addTriangle(v0, v1, v2, normal, c);
+        }
+
+
+        private void addTriangle(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 n, Color c)
+        {
             surface.AddTriangle(
-                new LevelVertex(p0, open0 ? nUp : n0, Vector2.Zero, c0),
-                new LevelVertex(p2, open2 ? nUp : n0, Vector2.Zero, c2),
-                new LevelVertex(p1, open1 ? nUp : n0, Vector2.Zero, c1)
+                new LevelVertex(v0, n, Vector2.Zero, c),
+                new LevelVertex(v1, n, Vector2.Zero, c),
+                new LevelVertex(v2, n, Vector2.Zero, c)
             );
-            surface.AddTriangle(
-                new LevelVertex(p0, open0 ? nUp : n1, Vector2.Zero, c0),
-                new LevelVertex(p1, open1 ? nUp : n1, Vector2.Zero, c1),
-                new LevelVertex(p3, open3 ? nUp : n1, Vector2.Zero, c3)
+        }
+
+        private void addQuad(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3, Color c)
+        {
+            var normal = -Vector3.Cross(v1 - v0, v3 - v0).Normalized();
+            addQuad(v0, v1, v2, v3, normal, c);
+        }
+
+        private void addQuad(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3, Vector3 n, Color c)
+        {
+            surface.AddQuad(
+                new LevelVertex(v0, n, Vector2.Zero, c),
+                new LevelVertex(v1, n, Vector2.Zero, c),
+                new LevelVertex(v2, n, Vector2.Zero, c),
+                new LevelVertex(v3, n, Vector2.Zero, c)
             );
         }
     }
