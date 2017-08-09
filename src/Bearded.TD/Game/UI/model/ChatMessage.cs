eb6898ec30a55acc043f858b68a39e0cf0fc5738
Tom <tom@tomrijnbeek.nl>
2017-08-09 19:45:38 +0100

    :lipstick: Improve folder structure of Game/UI

---
+++ src/Bearded.TD/Game/UI/model/ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿using Bearded.TD.Game.Players;
+
+namespace Bearded.TD.Game.UI
+{
+    sealed class ChatMessage
+    {
+        public Player Player { get; }
+        public string Text { get; }
+
+        public ChatMessage(Player player, string text)
+        {
+            Player = player;
+            Text = text;
+        }
+
+        public string GetDisplayString() => Player == null
+            ? $"[System] {Text}"
+            : $"{Player.Name}: {Text}";
+    }
+}
