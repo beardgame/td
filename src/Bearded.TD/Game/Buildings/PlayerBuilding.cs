05d286dd99852e1058c0d5d939d886532ab026bb
Tom <tom@tomrijnbeek.nl>
2017-09-05 22:05:53 +0100

    :white_square_button: Remove building square

--- src/Bearded.TD/Game/Buildings/PlayerBuilding.cs
+++ src/Bearded.TD/Game/Buildings/PlayerBuilding.cs
@@ -1,9 +1,6 @@
-﻿using amulware.Graphics;
-using Bearded.TD.Game.Factions;
+﻿using Bearded.TD.Game.Factions;
 using Bearded.TD.Game.Tiles;
-using Bearded.TD.Rendering;
 using Bearded.Utilities;
-using OpenTK;
 
 namespace Bearded.TD.Game.Buildings
 {
@@ -12,18 +9,5 @@ namespace Bearded.TD.Game.Buildings
         public PlayerBuilding(Id<Building> id, BuildingBlueprint blueprint, PositionedFootprint footprint, Faction faction)
             : base(id, blueprint, footprint, faction)
         { }
-
-        public override void Draw(GeometryManager geometries)
-        {
-            var geo = geometries.ConsoleBackground;
-            geo.Color = Color.GrayScale(60);
-            if (BuildManager != null)
-            {
-                geo.Color = geo.Color.WithAlpha((float)BuildManager.CurrentProgressFraction * .5f + .5f).Premultiplied;
-            }
-            geo.DrawRectangle(Position.NumericValue - Vector2.One * .3f, Vector2.One * .6f);
-
-            base.Draw(geometries);
-        }
     }
 }
