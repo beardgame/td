7d34cead81a1f470e0a41c1f40fa782254c006d9
Tom <tom@tomrijnbeek.nl>
2017-09-17 15:09:34 +0100

    :video_game: Implement debug actions

---
+++ src/Bearded.TD/Game/UI/input/DebugSetTileTypeClickHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using amulware.Graphics;
+using Bearded.TD.Game.Commands;
+using Bearded.TD.Game.Tiles;
+using Bearded.TD.Game.World;
+using Bearded.TD.Utilities;
+
+namespace Bearded.TD.Game.UI
+{
+    class DebugSetTileTypeInteractionHandler : InteractionHandler
+    {
+        private readonly TileInfo.Type tileType;
+
+        public TileSelection Selection => TileSelection.FromFootprints(FootprintGroup.Single);
+
+        public DebugSetTileTypeInteractionHandler(GameInstance game, TileInfo.Type tileType)
+            : base(game)
+        {
+            this.tileType = tileType;
+        }
+
+        public override void Update(UpdateEventArgs args, ICursorHandler cursor)
+        {
+            if (cursor.ClickAction.Hit)
+                cursor.CurrentFootprint.OccupiedTiles
+                    .Where(t => t.IsValid)
+                    .ForEach(setTile);
+        }
+        
+        private void setTile(Tile<TileInfo> tile) => Game.Request(SetTileType.Request, tile, tileType);
+    }
+}
--- src/Bearded.TD/Game/UI/screenlayers/ActionBarScreenLayer.cs
+++ src/Bearded.TD/Game/UI/screenlayers/ActionBarScreenLayer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Bearded.TD.Game.UI.Components;
+using Bearded.TD.Game.World;
 using Bearded.TD.Rendering;
 using Bearded.TD.Screens;
 using Bearded.TD.UI;
@@ -38,23 +39,27 @@ namespace Bearded.TD.Game.UI
             pages.Add(buildingPage);
 
 #if DEBUG
-            /*var debugPage = new ActionBarItem.Content[Constants.Game.UI.ActionBarSize];
-            debugPage[0] = contentFor(new DebugToggleTileTypeClickHandler(), "Toggle tile");
-            debugPage[1] = contentFor(new DebugSetTileTypeClickHandler(TileInfo.Type.Crevice), "Dig Too Deep");
+            var debugPage = new ActionBarItem.Content[Constants.Game.UI.ActionBarSize];
+            debugPage[0] = contentFor(new DebugSetTileTypeInteractionHandler(game, TileInfo.Type.Floor), "Make floor");
+            debugPage[1] = contentFor(new DebugSetTileTypeInteractionHandler(game, TileInfo.Type.Wall), "Make wall");
+            debugPage[2] = contentFor(new DebugSetTileTypeInteractionHandler(game, TileInfo.Type.Crevice), "Make crevice");
             pages.Add(debugPage);
 
             var enemyPage = new ActionBarItem.Content[Constants.Game.UI.ActionBarSize];
-            enemyPage[0] = contentFor(new DebugSpawnEnemyClickHandler("debug"), "Default enemy");
-            enemyPage[1] = contentFor(new DebugSpawnEnemyClickHandler("fast"), "Fast enemy");
-            enemyPage[2] = contentFor(new DebugSpawnEnemyClickHandler("strong"), "Strong enemy");
-            enemyPage[3] = contentFor(new DebugSpawnEnemyClickHandler("tank"), "Tank enemy");
-            pages.Add(enemyPage);*/
+            enemyPage[0] = contentFor(new DebugSpawnEnemyInteractionHandler(game, "debug"), "Default enemy");
+            enemyPage[1] = contentFor(new DebugSpawnEnemyInteractionHandler(game, "fast"), "Fast enemy");
+            enemyPage[2] = contentFor(new DebugSpawnEnemyInteractionHandler(game, "strong"), "Strong enemy");
+            enemyPage[3] = contentFor(new DebugSpawnEnemyInteractionHandler(game, "tank"), "Tank enemy");
+            pages.Add(enemyPage);
 #endif
 
             return pages;
         }
 
         private ActionBarItem.Content contentFor(InteractionHandler interactionHandler, string description)
-            => new ActionBarItem.Content(() => { }, () => { }, description);
+            => new ActionBarItem.Content(
+                () => game.PlayerInput.SetInteractionHandler(interactionHandler),
+                () => game.PlayerInput.ResetInteractionHandler(),
+                description);
     }
 }
