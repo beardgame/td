0fa707b31584ec0a5951698cd744e621393fe168
Tom <tom@tomrijnbeek.nl>
2017-09-17 14:49:05 +0100

    :video_game: Implement debug command interaction handler

---
+++ src/Bearded.TD/Game/UI/DebugCommandInteractionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using amulware.Graphics;
+using Bearded.TD.Commands;
+using Bearded.TD.Game.Commands;
+using Bearded.TD.Game.Tiles;
+
+namespace Bearded.TD.Game.UI
+{
+    class DebugCommandInteractionHandler : InteractionHandler
+    {
+        private readonly Func<PositionedFootprint, IRequest> requestFactory;
+        protected override TileSelection TileSelection { get; }
+
+        public DebugCommandInteractionHandler(GameInstance game, Func<PositionedFootprint, IRequest> requestFactory)
+            : this(game, requestFactory, TileSelection.Single) { }
+
+        public DebugCommandInteractionHandler(
+            GameInstance game, Func<PositionedFootprint, IRequest> requestFactory, TileSelection tileSelection)
+            : base(game)
+        {
+            this.requestFactory = requestFactory;
+            TileSelection = tileSelection;
+        }
+
+        public override void Update(UpdateEventArgs args, GameInputContext inputContext, ICursorHandler cursor)
+        {
+            if (cursor.ClickAction.Hit)
+                Game.Request(requestFactory(cursor.CurrentFootprint));
+        }
+    }
+}
