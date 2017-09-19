375eba390bb0daed08963d93593eefc06ecb3175
Tom <tom@tomrijnbeek.nl>
2017-09-19 21:35:32 +0100

    :skull: Implement GameOver serializer

--- src/Bearded.TD/Game/Commands/gameplay/GameOver.cs
+++ src/Bearded.TD/Game/Commands/gameplay/GameOver.cs
@@ -3,18 +3,38 @@ using Bearded.TD.Networking.Serialization;
 
 namespace Bearded.TD.Game.Commands
 {
-    class GameOver : ICommand
+    static class GameOver
     {
-        public static ICommand Command(GameState game) => new GameOver(game);
+        public static ICommand Command(GameState game)
+            => new Implementation(game);
 
-        private readonly GameState game;
-
-        private GameOver(GameState game)
+        private class Implementation : ICommand
         {
-            this.game = game;
+            private readonly GameState game;
+
+            public Implementation(GameState game)
+            {
+                this.game = game;
+            }
+
+            public void Execute() => game.Meta.DoGameOver();
+
+            public ICommandSerializer Serializer => new Serializer();
         }
 
-        public void Execute() => game.Meta.DoGameOver();
-        public ICommandSerializer Serializer { get; }
+        private class Serializer : ICommandSerializer
+        {
+            // ReSharper disable once UnusedMember.Local
+            public Serializer()
+            {
+            }
+
+            public ICommand GetCommand(GameInstance game)
+                => new Implementation(game.State);
+
+            public void Serialize(INetBufferStream stream)
+            {
+            }
+        }
     }
 }
