e3c447d36d18fffa4c991e5cbf9d7353e35e9da5
Tom <tom@tomrijnbeek.nl>
2017-10-25 18:35:40 +0100

    :construction: Master server project setup

---
+++ src/Bearded.TD.MasterServer/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bearded.TD.MasterServer
+{
+    static class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
