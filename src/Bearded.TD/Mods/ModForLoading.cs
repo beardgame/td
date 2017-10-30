a84734a3409eaa8cc3ea6908f4ca8572858223fb
amulware <amul@amulware.net>
2017-10-30 22:56:27 +0100

    :floppy_disk: Refactor ModForLoading interface

--- src/Bearded.TD/Mods/ModForLoading.cs
+++ src/Bearded.TD/Mods/ModForLoading.cs
@@ -1,25 +1,48 @@
-﻿namespace Bearded.TD.Mods
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bearded.TD.Mods
 {
     sealed class ModForLoading
     {
         private readonly ModMetadata modMetadata;
-        private bool isLoaded;
+        private Mod mod;
+        private bool isLoading;
+
+        public bool IsLoaded { get; private set; }
 
         public ModForLoading(ModMetadata modMetadata)
         {
             this.modMetadata = modMetadata;
         }
 
-        public void Load()
+        public void StartLoading()
         {
-            isLoaded = true;
+            if (isLoading)
+                throw new InvalidOperationException("Cannot load mod more than once.");
+
+            isLoading = true;
+
+            Task.Run(load);
         }
 
-        public Mod AsMod()
+        private async Task load()
         {
-            if (!isLoaded)
-                Load();
-            return new Mod();
+            // do actual loading here
+
+            await Task.Delay(1000);
+
+            mod = new Mod();
+
+            IsLoaded = true;
+        }
+
+        public Mod GetLoadedMod()
+        {
+            if (mod == null)
+                throw new InvalidOperationException("Most finish loading mod.");
+
+            return mod;
         }
     }
 }
