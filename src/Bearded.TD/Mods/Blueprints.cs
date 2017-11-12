1b96325c54e339d1307ed0960ac26b18e2fe41c4
Tom <tom@tomrijnbeek.nl>
2017-11-12 15:54:40 +0000

    :lock: Make blueprints readonly once initialized

--- src/Bearded.TD/Mods/Blueprints.cs
+++ src/Bearded.TD/Mods/Blueprints.cs
@@ -1,14 +1,41 @@
-﻿using Bearded.TD.Game.Buildings;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bearded.TD.Game.Buildings;
 using Bearded.TD.Mods.Models;
-using Bearded.TD.Tiles;
 
 namespace Bearded.TD.Mods
 {
     sealed class Blueprints
     {
-        public BlueprintCollection<FootprintGroup> Footprints { get; } = new BlueprintCollection<FootprintGroup>();
-        public BlueprintCollection<ComponentFactory> Components { get; } = new BlueprintCollection<ComponentFactory>();
-        public BlueprintCollection<BuildingBlueprint> Buildings { get; } = new BlueprintCollection<BuildingBlueprint>();
-        public BlueprintCollection<UnitBlueprint> Units { get; } = new BlueprintCollection<UnitBlueprint>();
+        public ReadonlyBlueprintCollection<FootprintGroup> Footprints { get; }
+        public ReadonlyBlueprintCollection<ComponentFactory> Components { get; }
+        public ReadonlyBlueprintCollection<BuildingBlueprint> Buildings { get; }
+        public ReadonlyBlueprintCollection<UnitBlueprint> Units { get; }
+
+        public Blueprints(
+            ReadonlyBlueprintCollection<FootprintGroup> footprints,
+            ReadonlyBlueprintCollection<ComponentFactory> components,
+            ReadonlyBlueprintCollection<BuildingBlueprint> buildings,
+            ReadonlyBlueprintCollection<UnitBlueprint> units)
+        {
+            Footprints = footprints;
+            Components = components;
+            Buildings = buildings;
+            Units = units;
+        }
+
+        public static Blueprints Merge(IEnumerable<Blueprints> blueprints)
+        {
+            var blueprintsList = blueprints as IList<Blueprints> ?? blueprints.ToList();
+            return new Blueprints(
+                new ReadonlyBlueprintCollection<FootprintGroup>(
+                    blueprintsList.SelectMany(blueprint => blueprint.Footprints.All)),
+                new ReadonlyBlueprintCollection<ComponentFactory>(
+                    blueprintsList.SelectMany(blueprint => blueprint.Components.All)),
+                new ReadonlyBlueprintCollection<BuildingBlueprint>(
+                    blueprintsList.SelectMany(blueprint => blueprint.Buildings.All)),
+                new ReadonlyBlueprintCollection<UnitBlueprint>(
+                    blueprintsList.SelectMany(blueprint => blueprint.Units.All)));
+        }
     }
 }
