792d81b8e64fe13c3b0b6d4e711f12478fa36aa2
amulware <amul@amulware.net>
2017-11-12 17:18:32 +0100

    :arrows_counterclockwise: First functioning dependency resolver

--- src/Bearded.TD/Mods/DependencyResolver.cs
+++ src/Bearded.TD/Mods/DependencyResolver.cs
@@ -1,36 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Bearded.TD.Mods.Models;
 
 namespace Bearded.TD.Mods
 {
     sealed class DependencyResolver<T>
+        where T : IBlueprint
     {
         private readonly ModMetadata thisMod;
         private readonly Dictionary<string, Mod> mods;
+        private readonly ReadonlyBlueprintCollection<T> thisModsBlueprints;
+        private readonly Func<Mod, ReadonlyBlueprintCollection<T>> blueprintsOf;
 
-        public DependencyResolver(ModMetadata thisMod, IEnumerable<Mod> otherMods)
+        public DependencyResolver(ModMetadata thisMod, ReadonlyBlueprintCollection<T> thisModsBlueprints,
+            IEnumerable<Mod> otherMods, Func<Mod, ReadonlyBlueprintCollection<T>> blueprintCollectionSelector)
         {
             this.thisMod = thisMod;
+            this.thisModsBlueprints = thisModsBlueprints;
+            blueprintsOf = blueprintCollectionSelector;
             mods = otherMods.ToDictionary(m => m.Id);
         }
 
-        public T Resolve(string name)
-            => Resolve(ModAwareName.FromNameInMod(name, thisMod));
+        public T Resolve(string id)
+            => Resolve(ModAwareId.FromNameInMod(id, thisMod));
 
-        public T Resolve(ModAwareName name)
+        public T Resolve(ModAwareId id)
         {
-            if (name.ModName == thisMod.Name)
+            if (id.ModId == thisMod.Id)
             {
-                
+                return thisModsBlueprints[id.Id];
             }
 
-            if (mods.TryGetValue(name.ModName, out var mod))
+            if (mods.TryGetValue(id.ModId, out var mod))
             {
-                
+                return blueprintsOf(mod)[id.Id];
             }
             
-            throw new InvalidDataException($"Unknown mod in identifier {name}");
+            throw new InvalidDataException($"Unknown mod in identifier {id}");
         }
     }
 }
