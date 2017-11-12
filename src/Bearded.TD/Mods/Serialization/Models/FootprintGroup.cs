6944bf34bad7329e6ddcf3c9a1273c1bf549eb23
amulware <amul@amulware.net>
2017-11-12 18:01:03 +0100

    :house_with_garden: Load buildings from mods

--- src/Bearded.TD/Mods/Serialization/Models/FootprintGroup.cs
+++ src/Bearded.TD/Mods/Serialization/Models/FootprintGroup.cs
@@ -2,15 +2,16 @@
 using System.Linq;
 using Bearded.TD.Game.World;
 using Bearded.TD.Tiles;
+using Bearded.Utilities;
 
 namespace Bearded.TD.Mods.Serialization.Models
 {
-    sealed class FootprintGroup
+    sealed class FootprintGroup : IConvertsTo<Mods.Models.FootprintGroup, Void>
     {
         public string Name { get; set; }
         public List<List<Step>> Footprints { get; set; }
 
-        public Mods.Models.FootprintGroup ToGameModel()
+        public Mods.Models.FootprintGroup ToGameModel(Void _)
         {
             return new Mods.Models.FootprintGroup(Name,
                 Footprints.Select(steps => new Footprint(steps))
