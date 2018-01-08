9cf4d5bf2a9e83dee394cfebd4ec5b8aea0f60b2
Tom <tom@tomrijnbeek.nl>
2018-01-08 22:02:16 +0000

    :heart: Send heartbeat to master server to keep lobby alive

--- src/Bearded.TD/UI/Model/Lobby/ServerLobbyManager.cs
+++ src/Bearded.TD/UI/Model/Lobby/ServerLobbyManager.cs
@@ -14,20 +14,18 @@ namespace Bearded.TD.UI.Model.Lobby
 {
     class ServerLobbyManager : LobbyManager
     {
+        private const float heartbeatTimeSeconds = 10;
+
         private readonly ServerNetworkInterface networkInterface;
+        private float secondsUntilNextHeartbeat;
 
         public ServerLobbyManager(ServerNetworkInterface networkInterface, Logger logger, ContentManager contentManager)
             : base(new ServerGameContext(networkInterface, logger), contentManager)
         {
             this.networkInterface = networkInterface;
-            this.networkInterface.Master.RegisterLobby(
-				new Proto.Lobby
-				{
-					Name = $"{Game.Me.Name}'s game",
-					MaxNumPlayers = 4,
-					CurrentNumPlayers = 1,
-				}
-            );
+
+            // First heartbeat automatically registers lobby.
+            doHeartbeat();
         }
 
         public override void Update(UpdateEventArgs args)
@@ -49,8 +47,29 @@ namespace Bearded.TD.UI.Model.Lobby
             }
 
             if (Game.Players.All(p => p.ConnectionState == PlayerConnectionState.Ready))
+            {
                 Dispatcher.RunOnlyOnServer(
                     commandDispatcher => commandDispatcher.Dispatch(BeginLoadingGame.Command(Game)));
+            }
+
+            secondsUntilNextHeartbeat -= args.ElapsedTimeInSf;
+            if (secondsUntilNextHeartbeat <= 0)
+            {
+                doHeartbeat();
+            }
+        }
+
+        private void doHeartbeat()
+        {
+            networkInterface.Master.RegisterLobby(
+                new Proto.Lobby
+                {
+                    Name = $"{Game.Me.Name}'s game",
+                    MaxNumPlayers = 4,
+                    CurrentNumPlayers = 1,
+                }
+            );
+            secondsUntilNextHeartbeat = heartbeatTimeSeconds;
         }
 
         private void handleConnectionApproval(NetIncomingMessage msg)
