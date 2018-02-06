5c6186acd1a02b97b88e28081eca741283a64b9c
Tom <tom@tomrijnbeek.nl>
2018-02-06 20:02:55 +0000

    :dizzy: Implement two simple status effects

---
+++ src/Bearded.TD/Game/Units/StatusEffects/IUnitStatusEffect.cs
@@ -0,0 +1,9 @@
+﻿namespace Bearded.TD.Game.Units.StatusEffects
+{
+    interface IUnitStatusEffect
+    {
+        int MaxStackSize { get; }
+
+        void Apply(EnemyUnitProperties.Builder propertiesBuilder);
+    }
+}
---
+++ src/Bearded.TD/Game/Units/StatusEffects/UnitStatusEffects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bearded.TD.Game.Units.StatusEffects
+{
+    static class UnitStatusEffects
+    {
+        public static IUnitStatusEffect Slow = new SimpleStatusEffect(properties =>
+        {
+            properties.Speed *= .9f;
+            properties.TimeBetweenAttacks *= 1.11;
+        }, 3);
+
+        public static IUnitStatusEffect Weakened = new SimpleStatusEffect(properties => properties.Damage /= 2, 1);
+        
+        private class SimpleStatusEffect : IUnitStatusEffect
+        {
+            private readonly Action<EnemyUnitProperties.Builder> effect;
+
+            public int MaxStackSize { get; }
+
+            public SimpleStatusEffect(Action<EnemyUnitProperties.Builder> effect, int maxStackSize)
+            {
+                this.effect = effect;
+                MaxStackSize = maxStackSize;
+            }
+
+            public void Apply(EnemyUnitProperties.Builder propertiesBuilder) => effect(propertiesBuilder);
+        }
+    }
+}
