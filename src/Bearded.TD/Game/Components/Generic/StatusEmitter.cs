d09362273bc931b18cf72c51928a30722af0932e
Tom <tom@tomrijnbeek.nl>
2018-02-20 21:08:33 +0000

    :snowflake: Add status emitter component

---
+++ src/Bearded.TD/Game/Components/Generic/StatusEmitter.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using amulware.Graphics;
+using Bearded.TD.Game.Buildings;
+using Bearded.TD.Game.Units;
+using Bearded.TD.Game.Units.StatusEffects;
+using Bearded.TD.Game.World;
+using Bearded.TD.Mods.Models;
+using Bearded.TD.Rendering;
+using Bearded.TD.Tiles;
+using Bearded.TD.UI.Model;
+using Bearded.Utilities.SpaceTime;
+using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
+
+namespace Bearded.TD.Game.Components.Generic
+{
+    [Component("statusEmitter")]
+    sealed class StatusEmitter<T> : Component<T, StatusEmitterParameters>
+        where T : GameObject, IPositionable, ISelectable
+    {
+        private readonly HashSet<EnemyUnit> affectedUnits = new HashSet<EnemyUnit>();
+        private readonly List<StatusEffectSource> activeStatusEffects = new List<StatusEffectSource>();
+
+        private readonly IUnitStatusEffect statusEffect;
+        private Building ownerAsBuilding;
+
+        private Instant nextTilesInRangeRecalculationTime;
+        private Instant nextUnitsInRangeRecalculationTime;
+        private List<Tile<TileInfo>> tilesInRange;
+
+        public StatusEmitter(StatusEmitterParameters parameters) : base(parameters)
+        {
+            statusEffect = UnitStatusEffects.Slow;
+        }
+
+        protected override void Initialise()
+        {
+            nextTilesInRangeRecalculationTime = Owner.Game.Time;
+            nextUnitsInRangeRecalculationTime = Owner.Game.Time;
+            ownerAsBuilding = Owner as Building;
+
+            Owner.Deleting += onOwnerDeleted;
+        }
+
+        private void onOwnerDeleted()
+        {
+            foreach (var effect in activeStatusEffects)
+            {
+                effect.EndImmediately();
+            }
+        }
+
+        public override void Update(TimeSpan elapsedTime)
+        {
+            if (!ownerAsBuilding?.IsCompleted ?? false) return;
+
+            updateActiveStatusEffects();
+
+            if (nextUnitsInRangeRecalculationTime > Owner.Game.Time) return;
+
+            ensureTilesInRangeUpToDate();
+
+            var newUnitsInRange =
+                    tilesInRange.SelectMany(tile => tile.Info.Enemies).Where(enemy => !affectedUnits.Contains(enemy));
+            foreach (var unit in newUnitsInRange)
+            {
+                var statusEffectForUnit = new StatusEffectSource(unit, statusEffect, isTileInRange);
+                unit.ApplyStatusEffect(statusEffectForUnit);
+                activeStatusEffects.Add(statusEffectForUnit);
+                affectedUnits.Add(unit);
+            }
+
+            nextUnitsInRangeRecalculationTime = Owner.Game.Time + Parameters.RecalculateUnitsInRangeInterval;
+        }
+
+        private void updateActiveStatusEffects()
+        {
+            foreach (var effect in activeStatusEffects.Where(effect => effect.HasEnded))
+            {
+                affectedUnits.Remove(effect.Unit);
+            }
+        }
+
+        private void ensureTilesInRangeUpToDate()
+        {
+            if (Owner.Game.Time >= nextTilesInRangeRecalculationTime)
+            {
+                recalculateTilesInRange();
+            }
+        }
+
+        private void recalculateTilesInRange()
+        {
+            var rangeSquared = Parameters.Range.Squared;
+
+            tilesInRange = new LevelVisibilityChecker<TileInfo>()
+                    .EnumerateVisibleTiles(Owner.Game.Level, Owner.Position, Parameters.Range,
+                            t => !t.IsValid || !t.Info.IsPassableFor(TileInfo.PassabilityLayer.Projectile))
+                    .Where(t => !t.visibility.IsBlocking && t.visibility.VisiblePercentage > 0.2 &&
+                            (Owner.Game.Level.GetPosition(t.tile) - Owner.Position).LengthSquared < rangeSquared)
+                    .Select(t => t.tile)
+                    .ToList();
+
+            nextTilesInRangeRecalculationTime = Owner.Game.Time + Parameters.RecalculateTilesInRangeInterval;
+        }
+
+        public override void Draw(GeometryManager geometries)
+        {
+            if (ownerAsBuilding == null || ownerAsBuilding.SelectionState == SelectionState.Default) return;
+
+            recalculateTilesInRange();
+
+            var geo = geometries.ConsoleBackground;
+
+            geo.Color = Color.Green * (ownerAsBuilding.SelectionState == SelectionState.Selected ? 0.15f : 0.1f);
+
+            var level = ownerAsBuilding.Game.Level;
+
+            foreach (var tile in tilesInRange)
+            {
+                geo.DrawCircle(level.GetPosition(tile).NumericValue, Constants.Game.World.HexagonSide, true, 6);
+            }
+        }
+
+        private bool isTileInRange(Tile<TileInfo> tile)
+        {
+            return tilesInRange.Contains(tile);
+        }
+
+        private class StatusEffectSource : IStatusEffectSource
+        {
+            public EnemyUnit Unit { get; }
+            public IUnitStatusEffect Effect { get; }
+
+            private readonly Func<Tile<TileInfo>, bool> rangeChecker;
+
+            public bool HasEnded { get; private set; }
+
+            public StatusEffectSource(EnemyUnit unit, IUnitStatusEffect effect, Func<Tile<TileInfo>, bool> rangeChecker)
+            {
+                Unit = unit;
+                Effect = effect;
+                this.rangeChecker = rangeChecker;
+            }
+
+            public void Update(TimeSpan elapsedTime)
+            {
+                HasEnded = rangeChecker(Unit.CurrentTile);
+            }
+
+            public void EndImmediately()
+            {
+                HasEnded = true;
+            }
+        }
+    }
+}
---
+++ src/Bearded.TD/Mods/Models/components/StatusEmitterParameters.cs
@@ -0,0 +1,22 @@
+﻿using Bearded.TD.Utilities;
+using Bearded.Utilities.SpaceTime;
+using Newtonsoft.Json;
+
+namespace Bearded.TD.Mods.Models
+{
+    sealed class StatusEmitterParameters
+    {
+        public Unit Range { get; } = 5.U();
+
+        public TimeSpan RecalculateTilesInRangeInterval { get; } = 1.S();
+        public TimeSpan RecalculateUnitsInRangeInterval { get; } = .1.S();
+
+        public StatusEmitterParameters() { }
+
+        [JsonConstructor]
+        public StatusEmitterParameters(Unit? range)
+        {
+            Range = range ?? Range;
+        }
+    }
+}
