45f80aed130990e21ca4f1eac9850d451a705d02
amulware <amul@amulware.net>
2018-03-27 21:12:07 +0100

    :gun: :boom: :tokyo_tower:

--- src/Bearded.TD/Game/Components/Generic/Turret.cs
+++ src/Bearded.TD/Game/Components/Generic/Turret.cs
@@ -1,146 +1,31 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using amulware.Graphics;
-using Bearded.TD.Game.Buildings;
-using Bearded.TD.Game.Projectiles;
-using Bearded.TD.Game.Units;
-using Bearded.TD.Game.World;
-using Bearded.TD.Mods.Models;
+﻿using Bearded.TD.Game.Buildings;
+using Bearded.TD.Game.Weapons;
 using Bearded.TD.Rendering;
-using Bearded.TD.Tiles;
-using Bearded.TD.UI.Model;
-using Bearded.TD.Utilities;
 using Bearded.Utilities.SpaceTime;
 
 namespace Bearded.TD.Game.Components.Generic
 {
     [Component("turret")]
-    class Turret<T> : Component<T, WeaponParameters>
+    class Turret<T> : Component<T, TurretParameters>
         where T : BuildingBase<T>
     {
-        private Instant nextTileInRangeRecalculationTime;
-        private Instant nextPossibleShootTime;
-        private List<Tile<TileInfo>> tilesInRange;
-        private EnemyUnit target;
-        private Building ownerAsBuilding;
+        private Weapon<T> weapon;
 
-        public Turret(WeaponParameters parameters) : base(parameters) { }
+        public Turret(TurretParameters parameters) : base(parameters) { }
 
         protected override void Initialise()
         {
-            ownerAsBuilding = Owner as Building;
-            nextPossibleShootTime = Owner.Game.Time;
+            weapon = new Weapon<T>(Parameters.Weapon);
         }
 
         public override void Update(TimeSpan elapsedTime)
         {
-            if (typeof(T) != typeof(Building))
-                return;
-
-            if (!ownerAsBuilding.IsCompleted)
-            {
-                nextPossibleShootTime = Owner.Game.Time;
-                return;
-            }
-
-            updateForCompletedBuilding();
+            weapon.Update(elapsedTime);
         }
-
-        private void updateForCompletedBuilding()
-        {
-            var time = Owner.Game.Time;
-            while (nextPossibleShootTime <= time)
-            {
-                ensureTilesInRangeList();
-                ensureTargetingState();
-
-                if (target == null)
-                {
-                    while (nextPossibleShootTime <= time)
-                        nextPossibleShootTime += Parameters.IdleInterval;
-                    break;
-                }
-
-                shootTarget();
-
-                nextPossibleShootTime += Parameters.ShootInterval;
-            }
-        }
-
-        private void ensureTilesInRangeList()
-        {
-            if (nextTileInRangeRecalculationTime > Owner.Game.Time)
-                return;
-
-            recalculateTilesInRange();
-        }
-
-        private void recalculateTilesInRange()
-        {
-            var rangeSquared = Parameters.Range.Squared;
-
-            tilesInRange = new LevelVisibilityChecker<TileInfo>()
-                .EnumerateVisibleTiles(Owner.Game.Level, Owner.Position, Parameters.Range,
-                    t => !t.IsValid || !t.Info.IsPassableFor(TileInfo.PassabilityLayer.Projectile))
-                .Where(t => !t.visibility.IsBlocking && t.visibility.VisiblePercentage > 0.2 &&
-                            (Owner.Game.Level.GetPosition(t.tile) - Owner.Position).LengthSquared < rangeSquared)
-                .Select(t => t.tile)
-                .ToList();
-
-            nextTileInRangeRecalculationTime = Owner.Game.Time + Parameters.ReCalculateTilesInRangeInterval;
-        }
-
-        private void ensureTargetingState()
-        {
-            if (target?.Deleted == true)
-                target = null;
-
-            if (target != null && !tilesInRange.Contains(target.CurrentTile))
-                target = null;
-
-            if (target != null)
-                return;
-
-            tryFindTarget();
-        }
-
-        private void shootTarget()
-        {
-            var p = new Projectile(
-                Owner.Position,
-                (target.Position - Owner.Position).Direction,
-                20.U() / 1.S(),
-                Parameters.Damage,
-                ownerAsBuilding
-                );
-            
-            Owner.Game.Add(p);
-        }
-
-        private void tryFindTarget()
-        {
-            target = tilesInRange
-                .SelectMany(t => t.Info.Enemies)
-                .FirstOrDefault();
-        }
-
+        
         public override void Draw(GeometryManager geometries)
         {
-            if (Owner.SelectionState == SelectionState.Default)
-                return;
-            
-            recalculateTilesInRange();
-
-            var geo = geometries.ConsoleBackground;
-
-            geo.Color = Color.Green * (Owner.SelectionState == SelectionState.Selected ? 0.15f : 0.1f);
-
-            var level = Owner.Game.Level;
-
-            foreach (var tile in tilesInRange)
-            {
-                geo.DrawCircle(level.GetPosition(tile).NumericValue, Constants.Game.World.HexagonSide, true, 6);
-            }
+            weapon.Draw(geometries);
         }
     }
 }
---
+++ src/Bearded.TD/Game/Weapons/Weapon.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using amulware.Graphics;
+using Bearded.TD.Game.Buildings;
+using Bearded.TD.Game.Components;
+using Bearded.TD.Game.Components.Generic;
+using Bearded.TD.Game.Projectiles;
+using Bearded.TD.Game.Units;
+using Bearded.TD.Game.World;
+using Bearded.TD.Mods.Models;
+using Bearded.TD.Rendering;
+using Bearded.TD.Tiles;
+using Bearded.TD.UI.Model;
+using Bearded.TD.Utilities;
+using Bearded.Utilities.SpaceTime;
+
+namespace Bearded.TD.Game.Weapons
+{
+    [ComponentOwner]
+    class Weapon<T>
+        where T : BuildingBase<T>
+    {
+        private readonly WeaponParameters parameters;
+        private readonly Turret<T> turret;
+        private readonly Building ownerAsBuilding;
+
+        private Instant nextTileInRangeRecalculationTime;
+        private Instant nextPossibleShootTime;
+        private List<Tile<TileInfo>> tilesInRange;
+        private EnemyUnit target;
+
+        public Weapon(WeaponParameters parameters, Turret<T> turret)
+        {
+            this.parameters = parameters;
+            this.turret = turret;
+            ownerAsBuilding = turret.Owner as Building;
+            nextPossibleShootTime = turret.Owner.Game.Time;
+        }
+
+        public void Update(TimeSpan elapsedTime)
+        {
+            if (typeof(T) != typeof(Building))
+                return;
+
+            if (!ownerAsBuilding.IsCompleted)
+            {
+                nextPossibleShootTime = ownerAsBuilding.Game.Time;
+                return;
+            }
+
+            updateForCompletedBuilding();
+        }
+
+        private void updateForCompletedBuilding()
+        {
+            var time = ownerAsBuilding.Game.Time;
+            while (nextPossibleShootTime <= time)
+            {
+                ensureTilesInRangeList();
+                ensureTargetingState();
+
+                if (target == null)
+                {
+                    while (nextPossibleShootTime <= time)
+                        nextPossibleShootTime += parameters.IdleInterval;
+                    break;
+                }
+
+                shootTarget();
+
+                nextPossibleShootTime += parameters.ShootInterval;
+            }
+        }
+
+        private void ensureTilesInRangeList()
+        {
+            if (nextTileInRangeRecalculationTime > ownerAsBuilding.Game.Time)
+                return;
+
+            recalculateTilesInRange();
+        }
+
+        private void recalculateTilesInRange()
+        {
+            var rangeSquared = parameters.Range.Squared;
+
+            var owner = turret.Owner;
+            var game = owner.Game;
+            var level = game.Level;
+
+            tilesInRange = new LevelVisibilityChecker<TileInfo>()
+                .EnumerateVisibleTiles(level, owner.Position, parameters.Range,
+                    t => !t.IsValid || !t.Info.IsPassableFor(TileInfo.PassabilityLayer.Projectile))
+                .Where(t => !t.visibility.IsBlocking && t.visibility.VisiblePercentage > 0.2 &&
+                            (level.GetPosition(t.tile) - owner.Position).LengthSquared < rangeSquared)
+                .Select(t => t.tile)
+                .ToList();
+
+            nextTileInRangeRecalculationTime = game.Time + parameters.ReCalculateTilesInRangeInterval;
+        }
+
+        private void ensureTargetingState()
+        {
+            if (target?.Deleted == true)
+                target = null;
+
+            if (target != null && !tilesInRange.Contains(target.CurrentTile))
+                target = null;
+
+            if (target != null)
+                return;
+
+            tryFindTarget();
+        }
+
+        private void shootTarget()
+        {
+            var p = new Projectile(
+                ownerAsBuilding.Position,
+                (target.Position - ownerAsBuilding.Position).Direction,
+                20.U() / 1.S(),
+                parameters.Damage,
+                ownerAsBuilding
+            );
+
+            ownerAsBuilding.Game.Add(p);
+        }
+
+        private void tryFindTarget()
+        {
+            target = tilesInRange
+                .SelectMany(t => t.Info.Enemies)
+                .FirstOrDefault();
+        }
+
+        public void Draw(GeometryManager geometries)
+        {
+            var owner = turret.Owner;
+
+            if (owner.SelectionState == SelectionState.Default)
+                return;
+
+            recalculateTilesInRange();
+
+            var geo = geometries.ConsoleBackground;
+
+            geo.Color = Color.Green * (owner.SelectionState == SelectionState.Selected ? 0.15f : 0.1f);
+
+            var level = owner.Game.Level;
+
+            foreach (var tile in tilesInRange)
+            {
+                geo.DrawCircle(level.GetPosition(tile).NumericValue, Constants.Game.World.HexagonSide, true, 6);
+            }
+        }
+    }
+}
