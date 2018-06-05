0687dd613a786d815d466242d92ae44aa23c176b
amulware <amul@amulware.net>
2018-06-05 20:34:26 +0100

    :video_camera: Make game control render itself correctly

--- src/Bearded.TD/Rendering/UI/RenderLayerCompositeControl.cs
+++ src/Bearded.TD/Rendering/UI/RenderLayerCompositeControl.cs
@@ -1,5 +1,4 @@
-﻿
-using Bearded.UI.Controls;
+﻿using Bearded.UI.Controls;
 using Bearded.UI.Rendering;
 using OpenTK;
 
@@ -10,6 +9,7 @@ namespace Bearded.TD.Rendering.UI
         private readonly FrameCompositor compositor;
 
         private IRendererRouter renderRouter;
+        private bool skipNextRender;
 
         public RenderLayerCompositeControl(FrameCompositor compositor)
         {
@@ -18,17 +18,53 @@ namespace Bearded.TD.Rendering.UI
 
         public override void Render(IRendererRouter router)
         {
+            if (skipNextRender)
+            {
+                skipNextRender = false;
+                return;
+            }
+
+            RenderAsLayer(router);
+        }
+
+        public void RenderAsLayer(IRendererRouter router)
+        {
+            renderDescendendRenderLayerControls(router, this);
+
             renderRouter = router;
-            
             compositor.RenderLayer(this);
-            
             renderRouter = null;
+        }
+
+        private void renderDescendendRenderLayerControls(IRendererRouter router, IControlParent parent)
+        {
+            foreach (var control in parent.Children)
+            {
+                switch (control)
+                {
+                    case RenderLayerCompositeControl renderLayer:
+                        renderLayer.RenderAsLayer(router);
+                        renderLayer.SkipNextRender();
+                        break;
+                    case IControlParent controlParent:
+                        renderDescendendRenderLayerControls(router, controlParent);
+                        break;
+                }
+            }
+        }
+
+        public void SkipNextRender()
+        {
+            skipNextRender = true;
         }
 
         abstract public Matrix4 ViewMatrix { get; }
         abstract public Matrix4 ProjectionMatrix { get; }
         abstract public RenderOptions RenderOptions { get; }
 
-        public virtual void Draw() => base.Render(renderRouter);
+        public virtual void Draw()
+        {
+            base.Render(renderRouter);
+        }
     }
 }
