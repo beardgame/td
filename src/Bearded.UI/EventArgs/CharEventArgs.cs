8f6a0df2fd37f9feb14d294de0be1d665332a475
Tom <tom@tomrijnbeek.nl>
2018-06-19 22:31:08 +0100

    :musical_keyboard: Add character typed events.

---
+++ src/Bearded.UI/EventArgs/CharEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Bearded.UI.EventArgs
+{
+    public class CharEventArgs : RoutedEventArgs
+    {
+        public char Character { get; }
+
+        public CharEventArgs(char character)
+        {
+            Character = character;
+        }
+    }
+}
