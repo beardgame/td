ebc93a4b1a21b393d1d324bb28dd252f62900f0f
Tom <tom@tomrijnbeek.nl>
2018-06-26 20:59:14 +0100

    :link: Implement basic connection networking

--- src/Bearded.TD/UI/Controls/lobbyList/LobbyList.cs
+++ src/Bearded.TD/UI/Controls/lobbyList/LobbyList.cs
@@ -1,4 +1,7 @@
-﻿using Bearded.TD.Meta;
+﻿using Bearded.TD.Game;
+using Bearded.TD.Game.Players;
+using Bearded.TD.Meta;
+using Bearded.TD.Mods;
 using Bearded.TD.Networking;
 using Bearded.UI.Navigation;
 using Bearded.Utilities;
@@ -9,12 +12,17 @@ namespace Bearded.TD.UI.Controls
 {
     sealed class LobbyList : NavigationNode<Void>, INetworkMessageHandler
     {
+        private Logger logger;
+        private ContentManager contentManager;
         private ClientNetworkInterface networkInterface;
 
         protected override void Initialize(DependencyResolver dependencies, Void parameters)
         {
+            logger = dependencies.Resolve<Logger>();
+            contentManager = dependencies.Resolve<ContentManager>();
+
             networkInterface = new ClientNetworkInterface();
-            networkInterface.RegisterMessageHandler(new NetworkDebugMessageHandler(dependencies.Resolve<Logger>()));	
+            networkInterface.RegisterMessageHandler(new NetworkDebugMessageHandler(logger));
             networkInterface.RegisterMessageHandler(this);
         }
 
@@ -32,12 +40,64 @@ namespace Bearded.TD.UI.Controls
 
         public void Handle(NetIncomingMessage message)
         {
-            // TODO
+            switch (message.MessageType)
+            {
+                case NetIncomingMessageType.StatusChanged:
+                    handleStatusChange(message);
+                    break;
+                case NetIncomingMessageType.UnconnectedData:
+                    // Data coming from an unconnected source. Must be master server.	
+                    handleIncomingLobby(Proto.Lobby.Parser.ParseFrom(message.ReadBytes(message.LengthBytes)));
+                    break;
+                case NetIncomingMessageType.NatIntroductionSuccess:
+                    // NAT introduction success. Means we successfully connected with lobby.	
+                    networkInterface.Connect(message.SenderEndPoint, new ClientInfo(playerName));
+                    break;
+            }
+        }
+
+        private void handleIncomingLobby(Proto.Lobby lobby)
+        {
+
+        }
+
+        private void handleStatusChange(NetIncomingMessage msg)
+        {
+            var status = (NetConnectionStatus) msg.ReadByte(); // Read status byte.	
+            switch (status)
+            {
+                case NetConnectionStatus.Connected:
+                    goToLobby(msg);
+                    return;
+                case NetConnectionStatus.Disconnected:
+                    var rejectionReason = msg.ReadString();
+                    logger.Info.Log(string.IsNullOrEmpty(rejectionReason)
+                        ? "Disconnected"
+                        : $"Disconnected with reason: {rejectionReason}");
+                    networkInterface.Shutdown();
+                    break;
+            }
+        }
+
+        private void goToLobby(NetIncomingMessage msg)
+        {
+            var info = LobbyPlayerInfo.FromBuffer(msg.SenderConnection.RemoteHailMessage);
+            var game = new GameInstance(
+                new ClientGameContext(networkInterface, logger),
+                contentManager,
+                new Player(info.Id, playerName),
+                null);
+
+            Navigation.Replace<Lobby, LobbyManager>(
+                new ClientLobbyManager(game, networkInterface), this);
         }
 
         public void OnConnectManualButtonClicked(string host)
         {
             networkInterface.Connect(host, new ClientInfo(playerName));
+
+            UserSettings.Instance.Misc.SavedNetworkAddress = host;
+            UserSettings.Save(logger);
         }
 
         public void OnBackToMenuButtonClicked()
@@ -47,8 +107,8 @@ namespace Bearded.TD.UI.Controls
         }
 
         private static string playerName
-            => UserSettings.Instance.Misc.Username?.Length > 0	
-                ? UserSettings.Instance.Misc.Username	
+            => UserSettings.Instance.Misc.Username?.Length > 0
+                ? UserSettings.Instance.Misc.Username
                 : "player";
     }
 }
