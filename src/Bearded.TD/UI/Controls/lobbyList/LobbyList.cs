d17c610034221275a13891fa66ae618734c1b0ca
Tom <tom@tomrijnbeek.nl>
2018-06-26 20:48:31 +0100

    :construction: Implement basic lobby list UI

--- src/Bearded.TD/UI/Controls/lobbyList/LobbyList.cs
+++ src/Bearded.TD/UI/Controls/lobbyList/LobbyList.cs
@@ -1,13 +1,54 @@
-﻿using Bearded.UI.Navigation;
+﻿using Bearded.TD.Meta;
+using Bearded.TD.Networking;
+using Bearded.UI.Navigation;
 using Bearded.Utilities;
+using Bearded.Utilities.IO;
+using Lidgren.Network;
 
 namespace Bearded.TD.UI.Controls
 {
-    sealed class LobbyList : NavigationNode<Void>
+    sealed class LobbyList : NavigationNode<Void>, INetworkMessageHandler
     {
+        private ClientNetworkInterface networkInterface;
+
         protected override void Initialize(DependencyResolver dependencies, Void parameters)
         {
-            throw new System.NotImplementedException();
+            networkInterface = new ClientNetworkInterface();
+            networkInterface.RegisterMessageHandler(new NetworkDebugMessageHandler(dependencies.Resolve<Logger>()));	
+            networkInterface.RegisterMessageHandler(this);
         }
+
+        public override void Terminate()
+        {
+            base.Terminate();
+
+            networkInterface.UnregisterMessageHandler(this);
+        }
+
+        public bool Accepts(NetIncomingMessage message)
+            => message.MessageType == NetIncomingMessageType.StatusChanged
+                || message.MessageType == NetIncomingMessageType.UnconnectedData
+                || message.MessageType == NetIncomingMessageType.NatIntroductionSuccess;
+
+        public void Handle(NetIncomingMessage message)
+        {
+            // TODO
+        }
+
+        public void OnConnectManualButtonClicked(string host)
+        {
+            networkInterface.Connect(host, new ClientInfo(playerName));
+        }
+
+        public void OnBackToMenuButtonClicked()
+        {
+            networkInterface.Shutdown();
+            Navigation.Replace<MainMenu>(this);
+        }
+
+        private static string playerName
+            => UserSettings.Instance.Misc.Username?.Length > 0	
+                ? UserSettings.Instance.Misc.Username	
+                : "player";
     }
 }
--- src/Bearded.TD/UI/Controls/lobbyList/LobbyListControl.cs
+++ src/Bearded.TD/UI/Controls/lobbyList/LobbyListControl.cs
@@ -4,9 +4,26 @@ namespace Bearded.TD.UI.Controls
 {
     sealed class LobbyListControl : CompositeControl
     {
-        public LobbyListControl(LobbyList lobbyList)
+        public LobbyListControl(LobbyList model)
         {
+            Add(new Button {new Label("Back to menu")}
+                .Anchor(a => a
+                    .Bottom(margin: 20, height: 50)
+                    .Left(margin: 20, width: 250))
+                .Subscribe(b => b.Clicked += model.OnBackToMenuButtonClicked));
 
+            var manualTextInput = new TextInput();
+
+            Add(new CompositeControl
+            {
+                manualTextInput.Anchor(a => a.Right(margin: 120)),
+                new Button {new Label("Connect")}
+                    .Anchor(a => a.Right(width: 120))
+                    .Subscribe(b => b.Clicked += () => model.OnConnectManualButtonClicked(manualTextInput.Text))
+            }.Anchor(a => a
+                .Bottom(margin: 46, height: 24)
+                .Right(margin: 20)
+                .Left(relativePercentage: .5, margin: 20)));
         }
     }
 }
