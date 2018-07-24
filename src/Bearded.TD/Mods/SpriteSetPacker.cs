79f6e5d4b1b7ab7d4dde0ff07e816fc8e39bcbba
Tom <tom@tomrijnbeek.nl>
2018-07-24 20:21:49 +0100

    :construction: Include sprite positions in spriteset packing output

--- src/Bearded.TD/Mods/SpriteSetPacker.cs
+++ src/Bearded.TD/Mods/SpriteSetPacker.cs
@@ -11,46 +11,66 @@ namespace Bearded.TD.Mods
 {
     sealed class SpriteSetPacker
     {
-        public Texture GetPackedTextureForSpriteSet(DirectoryInfo directory)
+        public PackedSpriteSet LoadPackedSpriteSet(DirectoryInfo directory, ModLoadingContext modLoadingContext)
         {
-            var bitmaps = pngFilesIn(directory).Select(file => new Bitmap(file.OpenRead())).ToList();
+            var bitmaps = annotatedPngFilesInRecursive(directory)
+                .Select(tuple => (name: tuple.name, bitmap: new Bitmap(tuple.file.OpenRead()))).ToList();
             var binPackingResult = BinPacking.Pack(
-                bitmaps.Select(bitmap => new BinPacking.Rectangle<Bitmap>(bitmap, bitmap.Width, bitmap.Height)));
+                bitmaps.Select(tuple
+                    => new BinPacking.Rectangle<(string name, Bitmap bitmap)>(
+                        tuple, tuple.bitmap.Width, tuple.bitmap.Height)));
 
-            var packedBitmap = new PackedSpriteSetBitmap(binPackingResult.Width, binPackingResult.Height);
+            var spriteSetBuilder = new PackedSpriteSetBuilder(binPackingResult.Width, binPackingResult.Height);
 
             foreach (var rectangle in binPackingResult.Rectangles)
             {
-                packedBitmap.CopyBitmap(rectangle.Value, rectangle.X, rectangle.Y);
-                rectangle.Value.Dispose();
+                spriteSetBuilder.CopyBitmap(rectangle.Value, rectangle.X, rectangle.Y);
+                rectangle.Value.bitmap.Dispose();
             }
 
-            // TODO: write data array to texture
-            return null;
+            return spriteSetBuilder.Build(modLoadingContext);
         }
 
-        private static IEnumerable<FileInfo> pngFilesIn(DirectoryInfo directory)
-            => directory.GetFiles("*.png", SearchOption.AllDirectories);
+        private static IEnumerable<(string name, FileInfo file)> annotatedPngFilesInRecursive(
+            DirectoryInfo directory, string prefix = "")
+        {
+            foreach (var file in pngFilesInFlat(directory))
+                yield return (prefix + Path.GetFileNameWithoutExtension(file.Name), file);
 
-        private class PackedSpriteSetBitmap
+            var newPrefix = prefix + "." + directory.Name;
+
+            foreach (var dir in directory.GetDirectories("*", SearchOption.TopDirectoryOnly))
+            foreach (var tuple in annotatedPngFilesInRecursive(dir, newPrefix))
+                yield return tuple;
+        }
+
+        private static IEnumerable<FileInfo> pngFilesInFlat(DirectoryInfo directory)
+            => directory.GetFiles("*.png", SearchOption.TopDirectoryOnly);
+
+        private class PackedSpriteSetBuilder
         {
             private readonly int width;
+            private readonly int height;
             private readonly byte[] data;
+            private readonly Dictionary<string, Rectangle> nameToRectangle = new Dictionary<string, Rectangle>();
 
-            public PackedSpriteSetBitmap(int width, int height)
+            public PackedSpriteSetBuilder(int width, int height)
             {
                 this.width = width;
+                this.height = height;
                 data = new byte[width * height * 4];
             }
 
-            public void CopyBitmap(Bitmap bitmap, int x, int y)
+            public void CopyBitmap((string name, Bitmap bitmap) tuple, int x, int y)
             {
-                var bitmapData = bitmap.LockBits(
-                    new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                nameToRectangle.Add(tuple.name, new Rectangle(x, y, tuple.bitmap.Width, tuple.bitmap.Height));
+
+                var bitmapData = tuple.bitmap.LockBits(
+                    new Rectangle(0, 0, tuple.bitmap.Width, tuple.bitmap.Height),
                     ImageLockMode.ReadOnly,
                     PixelFormat.Format32bppArgb);
 
-                for (var i = 0; i < bitmap.Height; i++)
+                for (var i = 0; i < tuple.bitmap.Height; i++)
                 {
                     Marshal.Copy(
                         bitmapData.Scan0 + bitmapData.Stride,
@@ -59,11 +79,31 @@ namespace Bearded.TD.Mods
                         bitmapData.Stride);
                 }
 
-                bitmap.UnlockBits(bitmapData);
+                tuple.bitmap.UnlockBits(bitmapData);
             }
 
             private int flatCoordinate(int x, int y)
                 => 4 * (y * width + x);
+
+            public PackedSpriteSet Build(ModLoadingContext modLoadingContext)
+            {
+                return new PackedSpriteSet(
+                    getTexture(modLoadingContext),
+                    nameToRectangle.ToDictionary(pair => pair.Key, pair => toUvRectangle(pair.Value)));
+            }
+
+            private UVRectangle toUvRectangle(Rectangle rect)
+            {
+                return new UVRectangle(
+                    (float)rect.Left / width, (float)rect.Right / width,
+                    (float)rect.Top / width, (float)rect.Bottom / width);
+            }
+
+            private Texture getTexture(ModLoadingContext modLoadingContext)
+            {
+                // TODO: copy data
+                return modLoadingContext.GlActions.RunAndReturn(() => new Texture(width, height));
+            }
         }
     }
 }
