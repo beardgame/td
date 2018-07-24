0e1386296e615d6da23bc77e6146eb12da008082
Tom <tom@tomrijnbeek.nl>
2018-07-24 20:33:38 +0100

    :sparkles: Make mining work again

--- src/Bearded.TD/UI/Controls/game/hud/ActionBar.cs
+++ src/Bearded.TD/UI/Controls/game/hud/ActionBar.cs
@@ -1,5 +1,4 @@
 ﻿using Bearded.TD.Game;
-using Bearded.TD.Game.Buildings;
 using Bearded.TD.Game.Input;
 using Bearded.Utilities;
 
@@ -18,7 +17,8 @@ namespace Bearded.TD.UI.Controls
 
         public event VoidEventHandler ActionsChanged;
 
-        private readonly IBuildingBlueprint[] blueprints = new IBuildingBlueprint[Constants.Game.UI.ActionBarSize];
+        private readonly InteractionHandler[] handlers = new InteractionHandler[Constants.Game.UI.ActionBarSize];
+        private readonly string[] labels = new string[Constants.Game.UI.ActionBarSize];
         private GameInstance game;
 
         public void Initialize(GameInstance game)
@@ -27,18 +27,23 @@ namespace Bearded.TD.UI.Controls
 
             for (var i = 0; i < buildings.Length; i++)
             {
-                blueprints[i] = game.Blueprints.Buildings[buildings[i]];
+                var blueprint = game.Blueprints.Buildings[buildings[i]];
+                handlers[i] = new BuildingInteractionHandler(game, game.Me.Faction, blueprint);
+                labels[i] = blueprint.Name;
             }
+
+            handlers[Constants.Game.UI.ActionBarSize - 1] = new MiningInteractionHandler(game, game.Me.Faction);
+            labels[Constants.Game.UI.ActionBarSize - 1] = "Mine tile";
 
             ActionsChanged?.Invoke();
         }
 
-        public string ActionLabelForIndex(int i) => blueprints[i]?.Name;
+        public string ActionLabelForIndex(int i) => labels[i];
 
         public void OnActionClicked(int actionIndex)
         {
-            if (blueprints[actionIndex] == null) return;
-            game.PlayerInput.SetInteractionHandler(new BuildingInteractionHandler(game, game.Me.Faction, blueprints[actionIndex]));
+            if (handlers[actionIndex] == null) return;
+            game.PlayerInput.SetInteractionHandler(handlers[actionIndex]);
         }
     }
 }
