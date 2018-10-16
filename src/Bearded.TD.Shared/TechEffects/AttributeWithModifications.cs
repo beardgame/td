9142548df8866871f76aeb0e0a0e8dba4434dae3
Tom <tom@tomrijnbeek.nl>
2018-10-16 20:31:22 +0100

    :sparkles: Add AttributeWithModifications and Modification

---
+++ src/Bearded.TD.Shared/TechEffects/AttributeWithModifications.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bearded.TD.Shared.TechEffects
+{
+    sealed class AttributeWithModifications<T>
+    {
+        private readonly double baseValue;
+        private readonly Func<double, T> valueTransformer;
+        private readonly List<Modification> modifications = new List<Modification>();
+
+        private bool currentValueDirty;
+        private T currentValue;
+
+        public T Value
+        {
+            get
+            {
+                if (currentValueDirty)
+                {
+                    recalculateCurrentValue();
+                }
+                return currentValue;
+            }
+        }
+
+        public AttributeWithModifications(double baseValue, Func<double, T> valueTransformer)
+        {
+            this.baseValue = baseValue;
+            this.valueTransformer = valueTransformer;
+        }
+
+        private void recalculateCurrentValue()
+        {
+            currentValue = valueTransformer(modifications.Aggregate(baseValue, applyModification));
+            currentValueDirty = false;
+        }
+
+        private double applyModification(double val, Modification modification)
+        {
+            switch (modification.Type)
+            {
+                case Modification.ModificationType.Additive:
+                    return val + modification.Value;
+                case Modification.ModificationType.Multiplicative:
+                    return val + modification.Value * baseValue;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void AddModification(Modification modification)
+        {
+            modifications.Add(modification);
+            currentValueDirty = true;
+        }
+    }
+}
---
+++ src/Bearded.TD.Shared/TechEffects/Modification.cs
@@ -0,0 +1,20 @@
+﻿namespace Bearded.TD.Shared.TechEffects
+{
+    struct Modification
+    {
+        public enum ModificationType : byte
+        {
+            Additive,
+            Multiplicative
+        }
+
+        public ModificationType Type { get; }
+        public double Value { get; }
+
+        public Modification(ModificationType type, double value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}
