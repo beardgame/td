4a9287c8b166955d99eb009683aeb97cdbc0bebe
amulware <amul@amulware.net>
2018-10-30 20:33:45 +0000

    :art: Continue level gemoetry batcher

--- src/Bearded.TD/Rendering/Deferred/LevelGeometryBatcher.cs
+++ src/Bearded.TD/Rendering/Deferred/LevelGeometryBatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using amulware.Graphics;
 using Bearded.TD.Game;
 using Bearded.TD.Game.World;
 
@@ -11,16 +12,14 @@ namespace Bearded.TD.Rendering.Deferred
 
         private readonly int tileMapRadius;
         private readonly int batchesPerRow;
-        private readonly Batch[] batches;
+        private readonly Batch[] batchLookup;
+        private readonly List<Batch> batches = new List<Batch>();
 
         private readonly Level level;
-
-        private readonly HashSet<int> dirtyBatches = new HashSet<int>();
-
-        // TODO: inject render context for surface creation
+        
         // TODO: actually create this on game creation (in GameWorldControl?)
-        // TODO: in the future this also needs to know about textures and stuff
-        public LevelGeometryBatcher(GameInstance game)
+        // TODO: in the future this also needs to know about textures and stuff from mods
+        public LevelGeometryBatcher(GameInstance game, RenderContext context)
         {
             level = game.State.Level;
             tileMapRadius = level.Tilemap.Radius;
@@ -29,21 +28,35 @@ namespace Bearded.TD.Rendering.Deferred
 
             batchesPerRow = (tileMapWidth + batchWidth - 1) / batchWidth;
 
-            batches = new Batch[batchesPerRow * batchesPerRow];
+            batchLookup = new Batch[batchesPerRow * batchesPerRow];
 
-            createValidBatches();
+            createValidBatches(context);
         }
 
-        // TODO: method to render all batches
+        // TODO: call this anytime a tile is changed
+        public void MarkDirty((int X, int Y) tile)
+            => batchLookup[batchIndexFor(tile)].MarkAsDirty();
 
-        private void createValidBatches()
+        // TODO: call this from deferred renderer
+        public void RenderAll()
+        {
+            foreach (var batch in batches)
+            {
+                batch.Render();
+            }
+        }
+
+        private void createValidBatches(RenderContext context)
         {
             foreach (var i in Enumerable
-                .Range(0, batches.Length)
+                .Range(0, batchLookup.Length)
                 .Where(isValidBatch))
             {
-                batches[i] = new Batch();
-                dirtyBatches.Add(i);
+                var baseTile = baseTileFor(i);
+                var batch = new Batch(context, level, baseTile);
+
+                batchLookup[i] = batch;
+                batches.Add(batch);
             }
         }
 
@@ -67,10 +80,6 @@ namespace Bearded.TD.Rendering.Deferred
                 Y: (batchIndex / batchesPerRow) * batchWidth - tileMapRadius
             );
 
-        // TODO: call this anytime a tile is changed
-        public void MarkDirty((int X, int Y) tile)
-            => dirtyBatches.Add(batchIndexFor(tile));
-
         private int batchIndexFor((int x, int y) tile)
             => batchIndexFor(tile.x, tile.y);
 
@@ -80,15 +89,60 @@ namespace Bearded.TD.Rendering.Deferred
 
         class Batch
         {
+            private readonly (int X, int Y) baseTile;
+            private readonly IndexedSurface<LevelVertex> surface;
 
-            public Batch()
+            private bool isDirty = true;
+
+            public Batch(RenderContext context, Level level, (int X, int Y) baseTile)
             {
-                // TODO: create surface, etc.
+                this.baseTile = baseTile;
+                surface = createSurface(context);
             }
 
-            // TODO: method to recreate vertices
-            // TODO: need to move LevelGeometry code here? need to probably find a way to organise that code better
-            // TODO: method to render surface
+            private static IndexedSurface<LevelVertex> createSurface(RenderContext context)
+            {
+                return new IndexedSurface<LevelVertex>
+                    {
+                        ClearOnRender = false,
+                        IsStatic = true
+                    }
+                    .WithShader(context.Surfaces.Shaders["deferred/gLevel"])
+                    .AndSettings(
+                        context.Surfaces.ViewMatrix,
+                        context.Surfaces.ProjectionMatrix,
+                        context.Surfaces.FarPlaneDistance
+                    );
+            }
+
+            public void MarkAsDirty()
+            {
+                isDirty = true;
+            }
+
+            public void Render()
+            {
+                if (isDirty)
+                {
+                    surface.Clear();
+                    generateGeometry();
+                }
+
+                surface.Render();
+            }
+
+            private void generateGeometry()
+            {
+                for (var x = 0; x < batchWidth; x++)
+                    for (var y = 0; y < batchWidth; y++)
+                    {
+                        var (tileX, tileY) = (baseTile.X + x, baseTile.Y + y);
+
+                        // TODO: create tile vertices
+                    }
+
+                // TODO: need to move LevelGeometry code here? need to probably find a way to organise that code better
+            }
         }
     }
 }
