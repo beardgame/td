06222c7971add42b30ccf98755bc3183a90a57c6
amulware <amul@amulware.net>
2018-11-20 21:03:57 +0000

    :white_medium_square: Add Tile class

---
+++ src/Bearded.TD/Game/World/Tile.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Bearded.TD.Game.World
+{
+    struct Tile
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Tile(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
