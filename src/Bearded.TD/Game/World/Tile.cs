4d2032514c52efb4de041f7c643d174a54793cc6
amulware <amul@amulware.net>
2018-11-20 21:09:55 +0000

    :black_large_square: Implement Tile members

--- src/Bearded.TD/Game/World/Tile.cs
+++ src/Bearded.TD/Game/World/Tile.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using Bearded.TD.Tiles;
+
 namespace Bearded.TD.Game.World
 {
     struct Tile
@@ -11,5 +13,40 @@ namespace Bearded.TD.Game.World
             X = x;
             Y = y;
         }
+
+        public int Radius => X * Y >= 0
+            ? Math.Abs(X + Y)
+            : Math.Max(Math.Abs(X), Math.Abs(Y));
+        
+        public Tile Neighbour(Direction direction) => Offset(direction.Step());
+        public Tile Offset(Step step) => new Tile(X + step.X, Y + step.Y);
+        
+        public int DistanceTo(Tile other)
+        {
+            if (Equals(other)) return 0;
+
+            var diffHorizontal = other.X - X; // -
+            var diffVertical = other.Y - Y; // \
+
+            // Combination of \ and - can be combined into /
+            var reduction = 0;
+            if (Math.Sign(diffHorizontal) != Math.Sign(diffVertical))
+                reduction = Math.Min(Math.Abs(diffHorizontal), Math.Abs(diffVertical));
+            return Math.Abs(diffHorizontal) + Math.Abs(diffVertical) - reduction;
+        }
+
+        public override bool Equals(object obj)
+            => obj is Tile tile && Equals(tile);
+
+        public bool Equals(Tile other)
+            => X == other.X && Y == other.Y;
+
+        public override int GetHashCode() => (X * 397) ^ Y;
+
+        public static bool operator ==(Tile t1, Tile t2) => t1.Equals(t2);
+        public static bool operator !=(Tile t1, Tile t2) => !(t1 == t2);
+
+        public override string ToString()
+            => $"{X},{Y}";
     }
 }
