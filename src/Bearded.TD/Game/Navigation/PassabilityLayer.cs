a6a81ce71b9123c61799bfa6cd5f79c3b46d7e04
Tom <tom@tomrijnbeek.nl>
2018-11-27 21:54:42 +0000

    :construction: Split passability layer and passability manager

--- src/Bearded.TD/Game/Navigation/PassabilityLayer.cs
+++ src/Bearded.TD/Game/Navigation/PassabilityLayer.cs
@@ -1,120 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Bearded.TD.Game.Events;
 using Bearded.TD.Game.World;
 using Bearded.TD.Tiles;
-using static Bearded.TD.Game.Navigation.TilePassability.Passability;
+using Bearded.TD.Utilities;
 using static Bearded.TD.Game.World.TileGeometry.TileType;
 
 namespace Bearded.TD.Game.Navigation
 {
+    enum Passability : byte
+    {
+        WalkingUnit = 1,
+        FlyingUnit = 2,
+        Worker = 3,
+        Projectile = 4
+    }
+
+    [Flags]
+    enum Passabilities : byte
+    {
+        None = 0,
+
+        WalkingUnit = 1 << (Passability.WalkingUnit - 1),
+        FlyingUnit = 1 << (Passability.FlyingUnit - 1),
+        Worker = 1 << (Passability.Worker - 1),
+        Projectile = 1 << (Passability.Projectile - 1),
+
+        All = 0xff
+    }
+
+    static class PassabilityExtensions
+    {
+        public static Passabilities AsFlags(this Passability passability)
+            => (Passabilities) (1 << ((byte) passability - 1));
+    }
+
     struct TilePassability
     {
-        [Flags]
-        public enum Passability : byte
-        {
-            None = 0,
-            Building = 1 << 0,
-            Unit = 1 << 1,
-            Flying = 1 << 2,
-            Worker = 1 << 3,
-            Projectile = 1 << 4,
+        public bool IsPassable { get; }
+        public Directions PassableDirections { get; }
 
-            All = 0xff,
-        }
-        
-        // TODO: OMG improve the naming of all of this, and probably invert 'blocked'
-        // TODO: should we store this value at all? we can just calculate it when needed?
-        //     or maybe a separate layer, and then a bunch of 'open directions for X' layers?
-        public Passability BlockedFor { get; }
-        public Directions OpenDirectionsForUnits { get; private set; }
-
-        public TilePassability(Passability blockedFor, Directions openDirectionsForUnits)
+        public TilePassability(bool isPassable, Directions passableDirections)
         {
-            BlockedFor = blockedFor;
-            OpenDirectionsForUnits = openDirectionsForUnits;
+            IsPassable = isPassable;
+            PassableDirections = passableDirections;
         }
 
-        public TilePassability WithPassability(Passability blockedFor)
+        public TilePassability WithPassability(bool isPassable)
         {
-            return new TilePassability(blockedFor, OpenDirectionsForUnits);
+            return new TilePassability(isPassable, PassableDirections);
         }
 
-        public TilePassability WithOpenDirectionsForUnits(Directions openDirectionsForUnits)
+        public TilePassability WithPassableDirections(Directions passableDirections)
         {
-            return new TilePassability(BlockedFor, openDirectionsForUnits);
-        }
-
-        public bool IsPassableFor(Passability layer)
-        {
-            return (BlockedFor & layer) == None;
+            return new TilePassability(IsPassable, passableDirections);
         }
     }
 
-    class PassabilityLayer : IListener<TileTypeChanged>
+    class PassabilityLayer
     {
-        private static readonly Dictionary<TileGeometry.TileType, TilePassability.Passability>
-            blockedLayersByType = new Dictionary<TileGeometry.TileType, TilePassability.Passability>
-            {
-                { Unknown, None },
-                { Floor, None },
-                { Wall, All },
-                { Crevice, ~(Flying | Projectile) },
-            };
-        
         private readonly Level level;
-        private readonly GameEvents events;
         private readonly Tilemap<TilePassability> tilemap;
 
-        public PassabilityLayer(Level level, GameEvents events)
+        public PassabilityLayer(Level level)
         {
             this.level = level;
-            this.events = events;
             tilemap = new Tilemap<TilePassability>(level.Radius);
-            
-            events.Subscribe(this);
         }
 
-        // TODO: need different open/closed values for walking, flying,
-        //     burrowing (different events for different pathfinding trees?)
         // TODO: take buildings into account
-        // TODO: stretch goal: only send events if it actually changed
         
-        public void HandleEvent(TileTypeChanged @event)
+        public bool HandleTilePassabilityChanged(Tile tile, bool isPassable)
         {
-            var (tile, type) = (@event.Tile, @event.Type);
-            var tileIsBlockedFor = blockedLayersByType[type];
+            var currentPassability = tilemap[tile];
+            if (currentPassability.IsPassable == isPassable)
+            {
+                return false;
+            }
 
-            var tilePassability = tilemap[tile].WithPassability(tileIsBlockedFor);
-            
-            tilemap[tile] = tilePassability;
-
-            var isPassable = tilePassability.IsPassableFor(Unit);
+            tilemap[tile] = currentPassability.WithPassability(isPassable);
 
             foreach (var dir in level.ValidDirectionsFrom(tile))
             {
                 var neighbour = tile.Neighbour(dir);
                 if (isPassable)
-                    openForUnitsTo(neighbour, dir.Opposite());
+                    openDirection(neighbour, dir.Opposite());
                 else
-                    closeForUnitsTo(neighbour, dir.Opposite());
+                    closeDirection(neighbour, dir.Opposite());
             }
 
-            events.Send(new TilePassabilityChanged(tile));
+            return true;
         }
 
-        private void closeForUnitsTo(Tile tile, Direction direction)
+        private void closeDirection(Tile tile, Direction direction)
         {
             var passability = tilemap[tile];
-            var openDirections = passability.OpenDirectionsForUnits.Except(direction);
-            tilemap[tile] = passability.WithOpenDirectionsForUnits(openDirections);
+            var passableDirections = passability.PassableDirections.Except(direction);
+            tilemap[tile] = passability.WithPassableDirections(passableDirections);
         }
 
-        private void openForUnitsTo(Tile tile, Direction direction)
+        private void openDirection(Tile tile, Direction direction)
         {
             var passability = tilemap[tile];
-            var openDirections = passability.OpenDirectionsForUnits.And(direction);
-            tilemap[tile] = passability.WithOpenDirectionsForUnits(openDirections);
+            var passableDirections = passability.PassableDirections.And(direction);
+            tilemap[tile] = passability.WithPassableDirections(passableDirections);
+        }
+    }
+
+    sealed class PassabilityManager : IListener<TileTypeChanged>
+    {
+        private static readonly Dictionary<TileGeometry.TileType, Passabilities>
+            passabilityByTileType = new Dictionary<TileGeometry.TileType, Passabilities>
+            {
+                { Unknown, Passabilities.All },
+                { Floor, Passabilities.All },
+                { Wall, Passabilities.None },
+                { Crevice, ~Passabilities.WalkingUnit }
+            };
+
+        private readonly GameEvents events;
+
+        private readonly Dictionary<Passability, PassabilityLayer> passabilityLayers;
+
+        public PassabilityManager(Level level, GameEvents events)
+        {
+            this.events = events;
+            
+            events.Subscribe(this);
+
+            passabilityLayers = ((Passability[]) Enum.GetValues(typeof(Passability)))
+                .ToDictionary(p => p, _ => new PassabilityLayer(level));
+        }
+
+        public void HandleEvent(TileTypeChanged @event)
+        {
+            var (tile, type) = (@event.Tile, @event.Type);
+
+            var hasChangedPassability = false;
+
+            foreach (var (passability, layer) in passabilityLayers)
+            {
+                var isPassable = (passability.AsFlags() & passabilityByTileType[type]) != Passabilities.None;
+                hasChangedPassability |= layer.HandleTilePassabilityChanged(tile, isPassable);
+            }
+
+            if (hasChangedPassability)
+            {
+                events.Send(new TilePassabilityChanged(tile));
+            }
         }
     }
 }
