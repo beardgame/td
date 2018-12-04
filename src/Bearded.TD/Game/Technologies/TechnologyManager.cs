5d42dff8d2345fd2c73672ccfb4fe5db453aeb5f
Tom <tom@tomrijnbeek.nl>
2018-12-04 21:34:12 +0000

    :bulb: Update technology manager around upgrades

--- src/Bearded.TD/Game/Technologies/TechnologyManager.cs
+++ src/Bearded.TD/Game/Technologies/TechnologyManager.cs
@@ -3,26 +3,34 @@ using System.Linq;
 using Bearded.TD.Game.Buildings;
 using Bearded.TD.Game.Components;
 using Bearded.TD.Game.Upgrades;
-using Bearded.Utilities;
 
 namespace Bearded.TD.Game.Technologies
 {
     sealed class TechnologyManager
     {
+        private readonly GameEvents events;
+
         private readonly HashSet<IBuildingBlueprint> unlockedBuildings = new HashSet<IBuildingBlueprint>();
         private readonly List<UpgradeBlueprint> unlockedUpgrades = new List<UpgradeBlueprint>();
 
-        public IEnumerable<IBuildingBlueprint> UnlockedBuildings => unlockedBuildings.ToList();
-
-        public event GenericEventHandler<IBuildingBlueprint> BuildingUnlocked;
+        public TechnologyManager(GameEvents events)
+        {
+            this.events = events;
+        }
+        
+        // The following 2 methods should probably be "unlock technology" or something like that, but we don't have the
+        // concept of technology yet.
 
         public void UnlockBlueprint(IBuildingBlueprint blueprint)
         {
-            // This should probably be "unlock technology" or something like that, but we don't have the concept of
-            // technology yet.
-
             unlockedBuildings.Add(blueprint);
-            BuildingUnlocked?.Invoke(blueprint);
+            events.Send(new BuildingTechnologyUnlocked(blueprint));
+        }
+
+        public void UnlockUpgrade(UpgradeBlueprint blueprint)
+        {
+            unlockedUpgrades.Add(blueprint);
+            events.Send(new UpgradeTechnologyUnlocked(blueprint));
         }
 
         public IEnumerable<UpgradeBlueprint> GetApplicableUpgradesFor(Building building)
---
+++ src/Bearded.TD/Game/Technologies/events/BuildingTechnologyUnlocked.cs
@@ -0,0 +1,14 @@
+﻿using Bearded.TD.Game.Buildings;
+
+namespace Bearded.TD.Game.Technologies
+{
+    sealed class BuildingTechnologyUnlocked : IEvent
+    {
+        public IBuildingBlueprint Blueprint { get; }
+
+        public BuildingTechnologyUnlocked(IBuildingBlueprint blueprint)
+        {
+            Blueprint = blueprint;
+        }
+    }
+}
