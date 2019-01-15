e91a3804698367a2abcf84770d885e6a5c83af61
amulware <amul@amulware.net>
2019-01-15 20:30:14 +0000

    :up: Show applied upgrades in building UI

--- src/Bearded.TD/UI/Controls/game/hud/entityStatus/BuildingStatusUIControl.cs
+++ src/Bearded.TD/UI/Controls/game/hud/entityStatus/BuildingStatusUIControl.cs
@@ -6,6 +6,7 @@ using Bearded.TD.Game.Buildings;
 using Bearded.TD.Game.Commands;
 using Bearded.TD.Game.Components.Generic;
 using Bearded.TD.Game.Upgrades;
+using Bearded.TD.UI.Layers;
 using Bearded.UI.Controls;
 using Bearded.UI.Rendering;
 
@@ -13,28 +14,32 @@ namespace Bearded.TD.UI.Controls
 {
     sealed class BuildingStatusUIControl : CompositeControl
     {
+        private readonly ListControl upgradeList = new ListControl(new ViewportClippingLayerControl());
+        
+        private BuildingStatusUI buildingStatus;
+        private Building building;
+        private UpgradeListItemSource upgradeItemSource;
+
         public BuildingStatusUIControl(BuildingStatusUI buildingStatus)
         {
-            var building = buildingStatus.Building as Building;
-            var health = building?.GetComponent<Health>();
-
+            this.buildingStatus = buildingStatus;
+            building = (Building)buildingStatus.Building;
+            
             Add(new BackgroundBox());
 
-            Add(new Label(buildingStatus.Building.Blueprint.Name) {FontSize = 24}
+            Add(new Label(building.Blueprint.Name) {FontSize = 24}
                 .Anchor(a => a.Top(margin: 4, height: 24).Left(margin: 4).Right(margin: 4)));
-            Add(new Label($"Owned by {buildingStatus.Building.Faction.Name ?? "nobody"}") {FontSize = 16}
+            Add(new Label($"Owned by {building.Faction.Name ?? "nobody"}") {FontSize = 16}
                 .Anchor(a => a.Top(margin: 32, height: 16).Left(margin: 4).Right(margin: 4)));
 
-            if (health != null)
+            if (building.GetComponent<Health>() is Health health)
             {
                 Add(new DynamicLabel(() => $"Hitpoints: {health.CurrentHealth} / {health.MaxHealth}")
                         {FontSize = 16}
                     .Anchor(a => a.Top(margin: 52, height: 16).Left(margin: 4).Right(margin: 4)));
             }
 
-            Add(new ListControl
-                    {ItemSource = new UpgradeListItemSource(buildingStatus.Game, building, buildingStatus.UpgradesForBuilding)}
-                .Anchor(a => a.Top(margin: 72).Bottom(margin: 40).Left(margin: 4).Right(margin: 4)));
+            Add(upgradeList.Anchor(a => a.Top(margin: 72).Bottom(margin: 40).Left(margin: 4).Right(margin: 4)));
 
             Add(Default.Button("Close")
                 .Anchor(a => a.Bottom(margin: 4, height: 32).Left(margin: 4).Right(relativePercentage: .5, margin: 2))
@@ -42,6 +47,28 @@ namespace Bearded.TD.UI.Controls
             Add(Default.Button("Delete")
                 .Anchor(a => a.Bottom(margin: 4, height: 32).Right(margin: 4).Left(relativePercentage: .5, margin: 2))
                 .Subscribe(btn => btn.Clicked += buildingStatus.OnDeleteBuildingClicked));
+            
+            updateUpgradeList();
+        }
+        
+        public override void Render(IRendererRouter r)
+        {
+            updateUpgradeListIfNeeded();
+            
+            base.Render(r);
+        }
+
+        private void updateUpgradeListIfNeeded()
+        {
+            if (upgradeItemSource?.AppliedUpgradesCount != building.AppliedUpgrades.Count)
+                updateUpgradeList();
+        }
+
+        private void updateUpgradeList()
+        {
+            upgradeItemSource = new UpgradeListItemSource(
+                buildingStatus.Game, building, buildingStatus.UpgradesForBuilding);
+            upgradeList.ItemSource = upgradeItemSource;
         }
 
         protected override void RenderStronglyTyped(IRendererRouter r) => r.Render(this);
@@ -50,32 +77,66 @@ namespace Bearded.TD.UI.Controls
         {
             private readonly GameInstance game;
             private readonly Building building;
-            private readonly List<UpgradeBlueprint> upgrades;
-            
+            private readonly List<UpgradeBlueprint> availableUpgrades;
+            private readonly List<UpgradeBlueprint> appliedUpgrades;
+
             public int ItemCount { get; }
+
+            public int AppliedUpgradesCount => appliedUpgrades.Count;
 
             public UpgradeListItemSource(GameInstance game, Building building, IEnumerable<UpgradeBlueprint> upgrades)
             {
                 this.game = game;
                 this.building = building;
-                this.upgrades = upgrades.ToList();
-                ItemCount = this.upgrades.Count;
+                appliedUpgrades = building.AppliedUpgrades.ToList();
+                availableUpgrades = upgrades.ToList();
+                ItemCount = availableUpgrades.Count + appliedUpgrades.Count;
             }
 
             public double HeightOfItemAt(int index)
             {
-                return 32;
+                return indexIsAvailableUpgrade(index) ? 32 : 16;
             }
 
             public Control CreateItemControlFor(int index)
             {
-                var upgrade = upgrades[index];
+                return indexIsAvailableUpgrade(index)
+                    ? createAvailableUpgradeButton(index)
+                    : createAppliedUpgradeControl(index);
+            }
+
+            private Control createAppliedUpgradeControl(int index)
+            {
+                var upgrade = appliedUpgrades[index - availableUpgrades.Count];
+                var control = new AppliedUpgradeControl(upgrade);
+                return control;
+            }
+
+            private Control createAvailableUpgradeButton(int index)
+            {
+                var upgrade = availableUpgrades[index];
                 var ctrl = new UpgradeButton(building, upgrade);
                 ctrl.Clicked += () => game.Request(UpgradeBuilding.Request, building, upgrade);
                 return ctrl;
             }
 
+            private bool indexIsAvailableUpgrade(int index)
+            {
+                return index < availableUpgrades.Count;
+            }
+
             public void DestroyItemControlAt(int index, Control control) {}
+        }
+
+        private class AppliedUpgradeControl : Label
+        {
+            public AppliedUpgradeControl(UpgradeBlueprint upgrade)
+            {
+                Text = upgrade.Name;
+                FontSize = 16;
+            }
+
+            protected override void RenderStronglyTyped(IRendererRouter r) => r.Render(this);
         }
         
         private class UpgradeButton : Button
@@ -89,7 +150,7 @@ namespace Bearded.TD.UI.Controls
                 this.building = building;
                 this.upgrade = upgrade;
                 this.WithDefaultStyle(upgrade.Name);
-                progressBar = new BackgroundBox { Color = Color.White.WithAlpha(0.25f).Premultiplied };
+                progressBar = new BackgroundBox { Color = Color.White * 0.25f };
                 Add(progressBar);
             }
 
