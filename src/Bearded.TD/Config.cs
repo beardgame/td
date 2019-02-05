6eb5371bb932ef7126858b92a27b817ddb47c633
amulware <amul@amulware.net>
2019-02-05 19:37:16 +0000

    :shipit:

--- src/Bearded.TD/Config.cs
+++ src/Bearded.TD/Config.cs
@@ -2,7 +2,7 @@
 {
     static class Config
     {
-        public const string BaseVersionString = "0.1";
+        public const string BaseVersionString = "0.2";
 
 #if DEBUG
         private const string versionSuffix = "-dev";
