8d8eb5ae48e42a3820f5491b362cc41db6c8ee0d
Tom <tom@tomrijnbeek.nl>
2019-02-12 20:05:36 +0000

    :sparkles: Add grant resources debug command

---
+++ src/Bearded.TD/Game/Commands/Debug/GrantResources.cs
@@ -0,0 +1,56 @@
+using Bearded.TD.Commands;
+using Bearded.TD.Game.Factions;
+using Bearded.TD.Networking.Serialization;
+using Bearded.TD.Utilities;
+using Bearded.Utilities;
+
+namespace Bearded.TD.Game.Commands.Debug
+{
+    static class GrantResources
+    {
+        public static IRequest<GameInstance> Request(Faction faction, double amount)
+            => new Implementation(faction, amount);
+
+        private class Implementation : UnifiedRequestCommand
+        {
+            private readonly Faction faction;
+            private readonly double amount;
+
+            public Implementation(Faction faction, double amount)
+            {
+                this.faction = faction;
+                this.amount = amount;
+            }
+
+            public override bool CheckPreconditions() => DebugGuards.IsInDebugMode && faction.HasResources;
+
+            public override void Execute() => faction.Resources.ProvideOneTimeResource(amount);
+
+            protected override UnifiedRequestCommandSerializer GetSerializer() => new Serializer(faction, amount);
+        }
+
+        private class Serializer : UnifiedRequestCommandSerializer
+        {
+            private Id<Faction> faction;
+            private double amount;
+            
+            // ReSharper disable once UnusedMember.Local
+            public Serializer() { }
+
+            public Serializer(Faction faction, double amount)
+            {
+                this.faction = faction.Id;
+                this.amount = amount;
+            }
+
+            protected override UnifiedRequestCommand GetSerialized(GameInstance game)
+                => new Implementation(game.State.FactionFor(faction), amount);
+
+            public override void Serialize(INetBufferStream stream)
+            {
+                stream.Serialize(ref faction);
+                stream.Serialize(ref amount);
+            }
+        }
+    }
+}
--- src/Bearded.TD/Game/Debug/GameDebugCommands.cs
+++ src/Bearded.TD/Game/Debug/GameDebugCommands.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Bearded.TD.Game.Commands;
 using Bearded.TD.Game.Commands.Debug;
 using Bearded.TD.Game.Units;
+using Bearded.TD.Utilities;
 using Bearded.TD.Utilities.Console;
 using Bearded.Utilities.IO;
 
@@ -14,6 +15,35 @@ namespace Bearded.TD.Game.Debug
         private static void killAll(Logger logger, CommandParameters p) => run(logger, gameInstance =>
         {
             gameInstance.RequestDispatcher.Dispatch(KillAllEnemies.Request(gameInstance));
+        });
+
+        [Command("game.resources")]
+        private static void giveResources(Logger logger, CommandParameters p) => run(logger, gameInstance =>
+        {
+            if (p.Args.Length != 1)
+            {
+                logger.Warning?.Log("Usage: \"game.resources <amount>\"");
+                return;
+            }
+
+            if (!double.TryParse(p.Args[0], out var amount))
+            {
+                logger.Warning?.Log($"Invalid amount: {amount}");
+                return;
+            }
+
+            var faction = gameInstance.Me.Faction;
+            while (faction != null && !faction.HasResources)
+            {
+                faction = faction.Parent;
+            }
+
+            if (faction == null)
+            {
+                logger.Warning?.Log($"Cannot add resources: player is not part of a faction with resource management.");
+            }
+            
+            gameInstance.RequestDispatcher.Dispatch(GrantResources.Request(faction, amount));
         });
 
         private static void run(Logger logger, Action<GameInstance> command) =>
