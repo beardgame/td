41086c59b2deee534259b3029bc9df863643c8cd
Tom Rijnbeek <tom@tomrijnbeek.nl>
2019-07-26 20:40:13 +0100

    :bug: Fix IsAncestorOf for factions

--- src/Bearded.TD/Game/Factions/FactionExtensions.cs
+++ src/Bearded.TD/Game/Factions/FactionExtensions.cs
@@ -2,17 +2,20 @@ namespace Bearded.TD.Game.Factions
 {
     static class FactionExtensions
     {
-        public static bool IsAncestorOf(this Faction toCheck, Faction potentialAncestor)
+        public static bool IsAncestorOf(this Faction thisFaction, Faction thatFaction)
         {
-            for (var current = toCheck; current != null; current = current.Parent)
+            for (var current = thatFaction; current != null; current = current.Parent)
             {
-                if (current == potentialAncestor) return true;
+                if (current == thisFaction) return true;
             }
             return false;
         }
 
         public static bool IsDescendantOf(this Faction toCheck, Faction potentialDescendant) =>
             IsAncestorOf(potentialDescendant, toCheck);
+
+        public static bool SharesResourcesWith(this Faction thisFaction, Faction thatFaction) =>
+            thisFaction.Resources != null && thisFaction.Resources == thatFaction.Resources;
 
         public static bool SharesWorkersWith(this Faction thisFaction, Faction thatFaction) =>
             thisFaction.Workers != null && thisFaction.Workers == thatFaction.Workers;
