01ee40a16f155544b952e78a951b423c7eafbcac
Tom Rijnbeek <tom@tomrijnbeek.nl>
2019-11-12 20:40:36 +0000

    :bug: Make game not crash when applying fire damage upgrade

--- src/Bearded.TD/Game/Upgrades/UpgradeEffectBase.cs
+++ src/Bearded.TD/Game/Upgrades/UpgradeEffectBase.cs
@@ -18,19 +18,13 @@ namespace Bearded.TD.Game.Upgrades
         public virtual void ApplyTo<T>(ComponentCollection<T> subject)
             => subject.Components.ForEach(c => c.ApplyUpgradeEffect(this));
 
-        public virtual void ApplyTo<T>(IParametersTemplate<T> subject) where T : IParametersTemplate<T>
-        {
-            throw new System.InvalidOperationException("Cannot apply upgrade effect to parameters template.");
-        }
+        public virtual void ApplyTo<T>(IParametersTemplate<T> subject) where T : IParametersTemplate<T> {}
 
         protected virtual Maybe<IComponentFactory<T>> TryCreateComponentFactory<T>() => Maybe.Nothing;
 
         public virtual bool RemoveFrom<T>(ComponentCollection<T> subject)
             => subject.Components.Aggregate(false, (b, c) => c.RemoveUpgradeEffect(this) || b);
 
-        public virtual bool RemoveFrom<T>(IParametersTemplate<T> subject) where T : IParametersTemplate<T>
-        {
-            throw new System.InvalidOperationException("Cannot remove upgrade effect from parameters template.");
-        }
+        public virtual bool RemoveFrom<T>(IParametersTemplate<T> subject) where T : IParametersTemplate<T> => false;
     }
 }
