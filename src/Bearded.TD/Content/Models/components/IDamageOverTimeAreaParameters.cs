04f8bf7fd67d6a2b6eb4ce34914e1ff20172493f
amulware <amul@amulware.net>
2019-12-03 23:02:29 +0000

    :fire: Make napalm deal damage

---
+++ src/Bearded.TD/Content/Models/components/IDamageOverTimeAreaParameters.cs
@@ -0,0 +1,17 @@
+using Bearded.TD.Game.Damage;
+using Bearded.TD.Shared.TechEffects;
+using Bearded.Utilities.SpaceTime;
+
+namespace Bearded.TD.Content.Models
+{
+    interface IDamageOverTimeAreaParameters : IParametersTemplate<IDamageOverTimeAreaParameters>
+    {
+        [Modifiable(10, Type = AttributeType.Damage)]
+        int DamagePerSecond { get; }
+
+        DamageType Type { get; }
+
+        [Modifiable(2, Type = AttributeType.Range)]
+        Unit Range { get; }
+    }
+}
