1fb2b20fc9d5caf40fb0aa1a6da2799636a5af75
amulware <amul@amulware.net>
2019-12-23 00:34:51 +0000

    🔍 Add enumerable MaybeFirst extension with predicate

--- src/Bearded.TD/Utilities/MaybeExtensions.cs
+++ src/Bearded.TD/Utilities/MaybeExtensions.cs
@@ -7,16 +7,32 @@ namespace Bearded.TD.Utilities
 {
     static class MaybeExtensions
     {
+        public static Maybe<T> MaybeFirst<T>(this IEnumerable<T> enumerable, Predicate<T> predicate)
+        {
+            switch (enumerable)
+            {
+                case null:
+                    throw new ArgumentNullException(nameof(enumerable));
+                default:
+                    foreach (var item in enumerable)
+                    {
+                        if (predicate(item))
+                            return Just(item);
+                    }
+                    break;
+            }
+
+            return Nothing;
+        }
+
         public static Maybe<T> MaybeFirst<T>(this IEnumerable<T> enumerable)
         {
             switch (enumerable)
             {
                 case null:
                     throw new ArgumentNullException(nameof(enumerable));
-                case IList<T> sourceList:
-                    if (sourceList.Count > 0)
-                        return Just(sourceList[0]);
-                    break;
+                case IList<T> sourceList when sourceList.Count > 0:
+                    return Just(sourceList[0]);
                 default:
                     using (var enumerator = enumerable.GetEnumerator())
                     {
@@ -45,9 +61,9 @@ namespace Bearded.TD.Utilities
                     {
                         if (!enumerator.MoveNext())
                             break;
-                        
+
                         var value = enumerator.Current;
-                        
+
                         if (enumerator.MoveNext())
                             throw new InvalidOperationException("Enumerable has more than one element.");
 
