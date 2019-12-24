ee56d322bcc61550ead73237334e10f451baaeac
amulware <amul@amulware.net>
2019-12-24 22:57:03 +0000

    🐛 Fix ui scale setting

--- src/Bearded.TD/UI/Layers/ViewportClippingLayerControl.cs
+++ src/Bearded.TD/UI/Layers/ViewportClippingLayerControl.cs
@@ -1,29 +1,38 @@
-﻿using Bearded.UI.Rendering;
+﻿using System;
+using Bearded.TD.Meta;
+using Bearded.UI.Rendering;
 
 namespace Bearded.TD.UI.Layers
 {
     class ViewportClippingLayerControl : DefaultRenderLayerControl
     {
         public override RenderOptions RenderOptions => new RenderOptions(getViewportFromFrame());
-        
+
         private ((int, int), (int, int)) getViewportFromFrame()
         {
             var frame = Frame;
             var topLeft = frame.TopLeft;
             var size = frame.Size;
 
-            var (x, y, w, h) = ((int)topLeft.X, (int)topLeft.Y, (int)size.X, (int)size.Y);
+            var scale = UserSettings.Instance.UI.UIScale;
+
+            var (x, y, w, h) = (
+                (int)(topLeft.X * scale),
+                (int)(topLeft.Y * scale),
+                (int)Math.Ceiling(size.X * scale),
+                (int)Math.Ceiling(size.Y * scale)
+                );
 
             var openGly = ViewportSize.Height - (y + h);
 
             return ((x, openGly), (w, h));
         }
-        
+
         protected override void RenderAsLayerBeforeAncestorLayer(IRendererRouter router)
         {
             SkipNextRender();
         }
-        
+
         protected override void RenderAsLayerAfterAncestorLayer(IRendererRouter router)
         {
             RenderAsLayer(router);
