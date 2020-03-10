4d215bd73441f0d427dbd638141c5e2802e84321
Tom Rijnbeek <tom@tomrijnbeek.nl>
2020-03-10 21:25:54 +0000

    :zap: Add more extensions

--- src/Bearded.TD/UI/Extensions.cs
+++ src/Bearded.TD/UI/Extensions.cs
@@ -9,5 +9,14 @@ namespace Bearded.TD.UI
 
         public static AnchorTemplate Below(this AnchorTemplate a, Control control, double? height = null) =>
             a.Top(margin: control.VerticalAnchors.Bottom.Offset, height: height);
+
+        public static AnchorTemplate Above(this AnchorTemplate a, Control control, double? height = null) =>
+            a.Bottom(margin: control.VerticalAnchors.Top.Offset, height: height);
+
+        public static AnchorTemplate RightOf(this AnchorTemplate a, Control control, double? width = null) =>
+            a.Left(margin: control.HorizontalAnchors.Right.Offset, width: width);
+
+        public static AnchorTemplate LeftOf(this AnchorTemplate a, Control control, double? width = null) =>
+            a.Right(margin: control.HorizontalAnchors.Left.Offset, width: width);
     }
 }
