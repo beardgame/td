9e4dcef1fef0a72ade0d52ff36d066bf405b5070
amulware <amul@amulware.net>
2020-10-17 15:23:56 +0100

    💡 Fix pointlight geometry

--- src/Bearded.TD/Rendering/Deferred/PointLightGeometry.cs
+++ src/Bearded.TD/Rendering/Deferred/PointLightGeometry.cs
@@ -1,4 +1,6 @@
 ﻿using amulware.Graphics;
+using amulware.Graphics.MeshBuilders;
+using amulware.Graphics.Shapes;
 using Bearded.Utilities;
 using OpenToolkit.Mathematics;
 
@@ -6,30 +8,21 @@ namespace Bearded.TD.Rendering.Deferred
 {
     class PointLightGeometry
     {
-        private readonly IndexedSurface<PointLightVertex> surface;
+        private readonly ShapeDrawer3<PointLightVertex, (Vector3, float, Color)> drawer;
 
-        public PointLightGeometry(IndexedSurface<PointLightVertex> surface)
+        public PointLightGeometry(IIndexedTrianglesMeshBuilder<PointLightVertex, ushort> meshBuilder)
         {
-            this.surface = surface;
+            drawer = new ShapeDrawer3<PointLightVertex, (Vector3 Center, float RadiusSquared, Color Color)>(
+                meshBuilder, (xyz, p) => new PointLightVertex(xyz, p.Center, p.RadiusSquared, p.Color));
         }
 
         public void Draw(Vector3 center, float radius, Color color)
         {
-            var x0 = center.X - radius;
-            var x1 = center.X + radius;
-            var y0 = center.Y - radius;
-            var y1 = center.Y + radius;
-
             var rSquared = radius.Squared();
+            var parameters = (center, rSquared, color);
 
-            // TODO: account for perspective to prevent light cutoff
-            // OMG - we can use the ColorShapeDrawer3 if we just make it generic in the vertex!
-            surface.AddQuad(
-                new PointLightVertex(new Vector3(x0, y0, 0), center, rSquared, color),
-                new PointLightVertex(new Vector3(x1, y0, 0), center, rSquared, color),
-                new PointLightVertex(new Vector3(x1, y1, 0), center, rSquared, color),
-                new PointLightVertex(new Vector3(x0, y1, 0), center, rSquared, color)
-            );
+            // TODO: confirm this draws at the right scale so that radius is the inner radius of the icosahedron
+            drawer.DrawIcosahedron(center, radius, parameters);
         }
     }
 }
