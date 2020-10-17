dda6211b93bab85713ce6c2c9e5f8a9224ebc4c6
Tom Rijnbeek <tom@tomrijnbeek.me>
2020-10-17 12:37:42 +0100

    🚧 Initialise text drawers in geometry manager

--- src/Bearded.TD/Rendering/GeometryManager.cs
+++ src/Bearded.TD/Rendering/GeometryManager.cs
@@ -3,19 +3,20 @@ using amulware.Graphics.MeshBuilders;
 using amulware.Graphics.Shapes;
 using amulware.Graphics.Text;
 using Bearded.TD.Rendering.Deferred;
+using OpenToolkit.Mathematics;
 
 namespace Bearded.TD.Rendering
 {
-    class GeometryManager
+    sealed class GeometryManager
     {
         private readonly SurfaceManager surfaces;
 
         public ColorShapeDrawer2 Primitives { get; }
 
-        public TextDrawer<UVColorVertex> ConsoleFont { get; }
+        public TextDrawerWithDefaults<Color> ConsoleFont { get; }
         public PrimitiveGeometry ConsoleBackground { get; }
 
-        public FontGeometry UIFont { get; }
+        public TextDrawerWithDefaults<Color> UIFont { get; }
 
         public PointLightGeometry PointLight { get; }
         public SpotlightGeometry Spotlight { get; }
@@ -27,10 +28,24 @@ namespace Bearded.TD.Rendering
             Primitives = new ColorShapeDrawer2(surfaces.Primitives);
             ConsoleBackground = new PrimitiveGeometry(surfaces.ConsoleBackground);
 
-            ConsoleFont =
-            UIFont = new FontGeometry(surfaces.UIFontSurface, surfaces.UIFont);
+            ConsoleFont = createTextDrawerWithDefaults(surfaces.ConsoleFont, surfaces.ConsoleFontMeshBuilder);
+            UIFont = createTextDrawerWithDefaults(surfaces.UIFont, surfaces.UIFontMeshBuilder);
             PointLight = new PointLightGeometry(surfaces.PointLights);
             Spotlight = new SpotlightGeometry(surfaces.Spotlights);
+        }
+
+        private static TextDrawerWithDefaults<Color> createTextDrawerWithDefaults(
+            Font consoleFont, IIndexedTrianglesMeshBuilder<UVColorVertex, ushort> meshBuilder)
+        {
+            return createTextDrawer(consoleFont, meshBuilder)
+                .WithDefaults(Constants.UI.Text.FontSize, 0, 0, Vector3.UnitX, Vector3.UnitY, Color.White);
+        }
+
+        private static ITextDrawer<Color> createTextDrawer(
+            Font consoleFont, IIndexedTrianglesMeshBuilder<UVColorVertex, ushort> meshBuilder)
+        {
+            return new TextDrawer<UVColorVertex, Color>(
+                consoleFont, meshBuilder, (xyz, uv, color) => new UVColorVertex(xyz, uv, color));
         }
     }
 }
