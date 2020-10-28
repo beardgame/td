004ddc109687e604c6aaeb5016c7db0a6d294d90
Tom Rijnbeek <tom@tomrijnbeek.me>
2020-10-28 19:50:55 +0000

    🚧 Add Bearded.TD.Generators project boilerplate

---
+++ src/Bearded.TD.Generators/TechEffects/TechEffectGenerator.cs
@@ -0,0 +1,15 @@
+using Microsoft.CodeAnalysis;
+
+namespace Bearded.TD.Generators.TechEffects
+{
+    [Generator]
+    public sealed class TechEffectGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) {}
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+
+        }
+    }
+}
