4c03daed4002f4e624c95e61405086e262f5486c
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-01-05 21:08:21 +0000

    ❓ Add transform functions for bindings

--- src/Bearded.TD/Utilities/Binding.cs
+++ src/Bearded.TD/Utilities/Binding.cs
@@ -1,3 +1,4 @@
+using System;
 using Bearded.Utilities;
 
 namespace Bearded.TD.Utilities
@@ -33,13 +34,37 @@ namespace Bearded.TD.Utilities
 
     static class Binding
     {
-        public static Binding<T> Create<T>(T initialValue) => new Binding<T>(initialValue);
+        public static Binding<T> Create<T>(T initialValue) => new(initialValue);
 
         public static Binding<T> Create<T>(T initialValue, GenericEventHandler<T> syncBack)
         {
             var binding = new Binding<T>(initialValue);
             binding.ControlUpdated += syncBack;
             return binding;
+        }
+
+        public static Binding<bool> And(Binding<bool> left, Binding<bool> right) =>
+            Combine(left, right, (l, r) => l && r);
+
+        public static Binding<bool> Negate(this Binding<bool> binding) => binding.Transform(b => !b);
+
+        public static Binding<TOut> Transform<TIn, TOut>(this Binding<TIn> binding, Func<TIn, TOut> func)
+        {
+            var transformed = new Binding<TOut>(func(binding.Value));
+            binding.ControlUpdated += v => transformed.SetFromControl(func(v));
+            binding.SourceUpdated += v => transformed.SetFromSource(func(v));
+            return transformed;
+        }
+
+        public static Binding<TOut> Combine<TLeft, TRight, TOut>(
+            Binding<TLeft> left, Binding<TRight> right, Func<TLeft, TRight, TOut> combine)
+        {
+            var combined = new Binding<TOut>(combine(left.Value, right.Value));
+            left.ControlUpdated += v => combined.SetFromControl(combine(v, right.Value));
+            right.ControlUpdated += v => combined.SetFromControl(combine(left.Value, v));
+            left.SourceUpdated += v => combined.SetFromSource(combine(v, right.Value));
+            right.SourceUpdated += v => combined.SetFromSource(combine(left.Value, v));
+            return combined;
         }
     }
 }
