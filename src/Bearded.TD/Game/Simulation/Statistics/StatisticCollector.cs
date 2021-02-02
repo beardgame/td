5fb14a273e097394ed22e05c45263badc6c01bd7
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-02-02 21:46:28 +0000

    🔁 Synchronise total building kills and damage

--- src/Bearded.TD/Game/Simulation/Statistics/StatisticCollector.cs
+++ src/Bearded.TD/Game/Simulation/Statistics/StatisticCollector.cs
@@ -5,12 +5,14 @@ using Bearded.TD.Game.Simulation.Damage;
 using Bearded.TD.Game.Simulation.Events;
 using Bearded.TD.Game.Simulation.GameLoop;
 using Bearded.TD.Game.Simulation.Upgrades;
+using Bearded.TD.Game.Synchronization;
+using Bearded.TD.Networking.Serialization;
 using Bearded.TD.Rendering;
 using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
 
 namespace Bearded.TD.Game.Simulation.Statistics
 {
-    sealed class StatisticCollector<T> : IComponent<T> where T : GameObject
+    sealed class StatisticCollector<T> : IComponent<T>, ISyncable where T : GameObject
     {
         private long totalDamage;
         private long totalKills;
@@ -49,5 +51,36 @@ namespace Bearded.TD.Game.Simulation.Statistics
         public void ApplyUpgradeEffect(IUpgradeEffect effect) => throw new InvalidOperationException();
 
         public bool RemoveUpgradeEffect(IUpgradeEffect effect) => throw new InvalidOperationException();
+
+        public IStateToSync GetCurrentStateToSync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class StatisticCollectorStateToSync : IStateToSync
+        {
+            private readonly StatisticCollector<T> source;
+            private long totalDamage;
+            private long totalKills;
+
+            public StatisticCollectorStateToSync(StatisticCollector<T> source)
+            {
+                this.source = source;
+                totalDamage = source.totalDamage;
+                totalKills = source.totalKills;
+            }
+
+            public void Serialize(INetBufferStream stream)
+            {
+                stream.Serialize(ref totalDamage);
+                stream.Serialize(ref totalKills);
+            }
+
+            public void Apply()
+            {
+                source.totalDamage = totalDamage;
+                source.totalKills = totalKills;
+            }
+        }
     }
 }
