ca87e9f672aa1e218da64543fc93ef9f4b8b23be
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-02-09 20:38:44 +0000

    ⚱ Add a disposer helper class

---
+++ src/Bearded.TD/Utilities/Diposer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bearded.TD.Utilities
+{
+    sealed class Disposer : IDisposable
+    {
+        private bool isDisposed;
+        private readonly List<IDisposable> disposables = new();
+
+        public void AddDisposable(IDisposable disposable)
+        {
+            if (isDisposed)
+            {
+                throw new InvalidOperationException("Cannot register new disposables to a disposer after disposing.");
+            }
+            disposables.Add(disposable);
+        }
+
+        public void Dispose()
+        {
+            if (isDisposed)
+            {
+                throw new InvalidOperationException("Can only dispose a disposer once.");
+            }
+
+            foreach (var d in disposables)
+            {
+                d.Dispose();
+            }
+            isDisposed = true;
+        }
+
+        public void DisposeAndReset()
+        {
+            Dispose();
+            disposables.Clear();
+            isDisposed = false;
+        }
+    }
+}
