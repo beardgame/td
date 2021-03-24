56cf81662f170d2df9ed16dd50428ea7d91c7744
amulware <amul@amulware.net>
2021-03-24 22:55:26 +0000

    🚧 Finish abstract fitness function

--- src/Bearded.TD/Game/Generation/Fitness/Fitness.cs
+++ src/Bearded.TD/Game/Generation/Fitness/Fitness.cs
@@ -1,43 +1,22 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
 
 namespace Bearded.TD.Game.Generation.Fitness
 {
-    sealed class Fitness<T> : IComparable<Fitness<T>>
+    class Fitness<T> : IComparable<Fitness<T>>
     {
-        private readonly ImmutableArray<FitnessComponent<T>> components;
         public double Value { get; }
 
-        private Fitness(IEnumerable<FitnessComponent<T>> components)
-        {
-            this.components = components.ToImmutableArray();
-            Value = this.components.Sum(c => c.Value);
-        }
+        public FitnessFunction<T> Function { get; }
 
-        public static Fitness<T> From(params FitnessComponent<T>[] components)
+        public Fitness(FitnessFunction<T> function, double value)
         {
-            return new(components);
+            Function = function;
+            Value = value;
         }
-
-        private static readonly char[] newLines = {'\r', '\n'};
 
         public override string ToString()
         {
-            var stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine($"Fitness: {Value}");
-            foreach (var component in components)
-            {
-                var componentLines = component.ToString() ?? "";
-                foreach (var line in componentLines.Split(newLines, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    stringBuilder.Append("  ");
-                    stringBuilder.AppendLine(line);
-                }
-            }
-            return stringBuilder.ToString();
+            return $"- {Function.Name}: {Value}";
         }
 
         public int CompareTo(Fitness<T>? other)
--- src/Bearded.TD/Game/Generation/Fitness/FitnessFunction.cs
+++ src/Bearded.TD/Game/Generation/Fitness/FitnessFunction.cs
@@ -1,18 +1,75 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
 
 namespace Bearded.TD.Game.Generation.Fitness
 {
+    static class FitnessFunction
+    {
+        public static FitnessFunction<T> From<T>(params FitnessFunction<T>[] functions)
+        {
+            return new CompositeFitnessFunction<T>(functions);
+        }
+
+        private sealed class CompositeFitnessFunction<T> : FitnessFunction<T>
+        {
+            private readonly ImmutableArray<FitnessFunction<T>> functions;
+
+            public CompositeFitnessFunction(FitnessFunction<T>[] functions)
+            {
+                this.functions = functions.ToImmutableArray();
+            }
+
+            public override string Name => "Composite Fitness";
+            public override Fitness<T> FitnessOf(T instance)
+            {
+                var components = functions.Select(f => f.FitnessOf(instance)).ToImmutableArray();
+                return new CompositeFitness<T>(this, components.Sum(c => c.Value), components);
+            }
+        }
+        private sealed class CompositeFitness<T> : Fitness<T>
+        {
+            private readonly ImmutableArray<Fitness<T>> components;
+
+            public CompositeFitness(FitnessFunction<T> function, double value, ImmutableArray<Fitness<T>> components)
+                : base(function, value)
+            {
+                this.components = components;
+            }
+
+            private static readonly char[] newLines = {'\r', '\n'};
+
+            public override string ToString()
+            {
+                var stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine($"{Function.Name}: {Value}");
+                foreach (var component in components)
+                {
+                    var componentLines = component.ToString() ?? "";
+                    foreach (var line in componentLines.Split(newLines, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        stringBuilder.Append("  ");
+                        stringBuilder.AppendLine(line);
+                    }
+                }
+                return stringBuilder.ToString();
+            }
+        }
+    }
+
     abstract class FitnessFunction<T>
     {
         public abstract string Name { get; }
 
         public abstract Fitness<T> FitnessOf(T instance);
 
-        public static FitnessFunction<T> operator *(FitnessFunction<T> function, double scalar) => scalar * function;
-        public static FitnessFunction<T> operator *( double scalar, FitnessFunction<T> function)
-        {
-            return new ScaledFitnessFunction(function, scalar);
-        }
+        public static FitnessFunction<T> operator *(FitnessFunction<T> function, double scalar)
+            => new ScaledFitnessFunction(function, scalar);
+
+        public static FitnessFunction<T> operator *(double scalar, FitnessFunction<T> function)
+            => new ScaledFitnessFunction(function, scalar);
 
         private sealed class ScaledFitnessFunction : FitnessFunction<T>
         {
@@ -26,6 +83,7 @@ namespace Bearded.TD.Game.Generation.Fitness
             }
 
             public override string Name => $"{function.Name} * {scalar}";
+
             public override Fitness<T> FitnessOf(T instance)
             {
                 throw new NotImplementedException();
