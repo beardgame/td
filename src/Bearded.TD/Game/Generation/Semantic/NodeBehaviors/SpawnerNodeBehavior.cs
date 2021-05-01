71f4bfc06b7e92fc49770fe996f00f3c0be42be4
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-05-01 15:07:30 +0100

    🏝 Define node blueprints in JSON

--- src/Bearded.TD/Game/Generation/Semantic/NodeBehaviors/SpawnerNodeBehavior.cs
+++ src/Bearded.TD/Game/Generation/Semantic/NodeBehaviors/SpawnerNodeBehavior.cs
@@ -3,7 +3,7 @@ using Bearded.TD.Game.Generation.Semantic.Features;
 
 namespace Bearded.TD.Game.Generation.Semantic.NodeBehaviors
 {
-    [NodeBehavior("spawnerNode")]
+    [NodeBehavior("spawner")]
     sealed class SpawnerNodeBehavior : NodeBehavior
     {
         public override ImmutableArray<NodeTag> Tags { get; } = ImmutableArray.Create(new NodeTag("spawner"));
