97bf7f6fe5c8e6952c03117a5cb3efdc45401742
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-05-04 21:27:24 +0100

    🤐 Polymorphism after all

--- src/Bearded.TD/Game/Generation/Semantic/Commands/LevelGenerationCommandAccumulator.cs
+++ src/Bearded.TD/Game/Generation/Semantic/Commands/LevelGenerationCommandAccumulator.cs
@@ -6,13 +6,12 @@ using Bearded.TD.Game.Simulation.Components;
 using Bearded.TD.Game.Simulation.World;
 using Bearded.Utilities.Geometry;
 using Bearded.Utilities.SpaceTime;
-using static Bearded.TD.Game.Generation.Semantic.Commands.LevelGenerationCommand;
 
 namespace Bearded.TD.Game.Generation.Semantic.Commands
 {
     sealed class LevelGenerationCommandAccumulator
     {
-        private readonly List<LevelGenerationCommand> commands = new();
+        private readonly List<ILevelGenerationCommand> commands = new();
 
         public void PlaceBuilding(IBuildingBlueprint blueprint, PositionedFootprint footprint)
         {
@@ -21,7 +20,7 @@ namespace Bearded.TD.Game.Generation.Semantic.Commands
 
         public ImmutableArray<CommandFactory> ToCommandFactories()
         {
-            return commands.Select(LevelGenerationCommandConverter.ToCommandFactory).ToImmutableArray();
+            return commands.Select(cmd => cmd.ToCommandFactory()).ToImmutableArray();
         }
 
         public void PlaceGameObject(IComponentOwnerBlueprint blueprint, Position3 position, Direction2 direction)
