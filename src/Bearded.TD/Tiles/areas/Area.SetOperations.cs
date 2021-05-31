f01b3c2762f148d26d15c665849393274aa41ec8
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-05-31 13:16:34 +0100

    🗺 Implement set operations for areas

---
+++ src/Bearded.TD/Tiles/areas/Area.SetOperations.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Bearded.TD.Tiles
+{
+    static partial class Area
+    {
+        public static IArea Intersect(IArea left, IArea right)
+        {
+            // TODO: consider if this always needs to be precalculated
+            return new HashSetArea(left.ToImmutableHashSet().Intersect(right.ToImmutableHashSet()));
+        }
+
+        public static IArea Union(IArea left, IArea right)
+        {
+            // TODO: consider if this always needs to be precalculated
+            return new HashSetArea(left.ToImmutableHashSet().Union(right.ToImmutableHashSet()));
+        }
+
+        public static IArea Union(IEnumerable<IArea> areas)
+        {
+            // TODO: consider if this always needs to be precalculated
+            var setBuilder = ImmutableHashSet.CreateBuilder<Tile>();
+            foreach (var area in areas)
+            {
+                setBuilder.UnionWith(area.Enumerated);
+            }
+
+            return new HashSetArea(setBuilder.ToImmutable());
+        }
+
+        public static IArea Except(IArea left, IArea right)
+        {
+            // TODO: consider if this always needs to be precalculated
+            return new HashSetArea(left.ToImmutableHashSet().Except(right.ToImmutableHashSet()));
+        }
+    }
+}
