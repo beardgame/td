2e89e94b9c1790013200698bf2c8da2fbd691a65
amulware <amul@amulware.net>
2021-10-05 21:11:49 +0100

    🧹

--- src/Bearded.TD/Game/Simulation/Weapons/BeamEmitter.cs
+++ src/Bearded.TD/Game/Simulation/Weapons/BeamEmitter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Bearded.Graphics;
 using Bearded.Graphics.Shapes;
 using Bearded.TD.Content.Models;
-using Bearded.TD.Game.Simulation.Buildings;
 using Bearded.TD.Game.Simulation.Components;
 using Bearded.TD.Game.Simulation.Damage;
 using Bearded.TD.Game.Simulation.Navigation;
