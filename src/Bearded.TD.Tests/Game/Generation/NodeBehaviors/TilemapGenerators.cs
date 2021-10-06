e1af64b530baad90463ff037d35248ff531d3966
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-10-06 18:14:37 +0100

    ✅ Fix build errors related to new FsCheck version

--- src/Bearded.TD.Tests/Game/Generation/NodeBehaviors/TilemapGenerators.cs
+++ src/Bearded.TD.Tests/Game/Generation/NodeBehaviors/TilemapGenerators.cs
@@ -2,10 +2,11 @@ using System;
 using System.Linq;
 using Bearded.TD.Game.Simulation.World;
 using FsCheck;
+using FsCheck.Fluent;
 
 namespace Bearded.TD.Tests.Game.Generation.NodeBehaviors
 {
-    public class TilemapGenerators
+    public sealed class TilemapGenerators
     {
         public static Arbitrary<TileType> TileTypes() => Arb.From(Gen.Elements(
             Enum.GetValues<TileType>().Except(new[] { TileType.Unknown }).ToArray()
