cec06c8525a2ee7b96d0810b7ad249a094f0b708
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-10-12 21:05:23 +0100

    🚧 Extract common worker task behaviour to abstract base

--- src/Bearded.TD/Game/Simulation/Buildings/BuildingWorkerTask.cs
+++ src/Bearded.TD/Game/Simulation/Buildings/BuildingWorkerTask.cs
@@ -1,91 +1,33 @@
 using System.Collections.Generic;
-using Bearded.TD.Content.Models;
 using Bearded.TD.Game.Simulation.Resources;
 using Bearded.TD.Game.Simulation.Workers;
 using Bearded.TD.Tiles;
 using Bearded.Utilities;
-using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
 
 namespace Bearded.TD.Game.Simulation.Buildings
 {
-    sealed class BuildingWorkerTask : IWorkerTask
+    sealed class BuildingWorkerTask : WorkerTaskBase
     {
         private readonly IIncompleteBuilding incompleteBuilding;
-        private readonly ResourceConsumer resourceConsumer;
 
-        private bool buildStarted;
-
-        public Id<IWorkerTask> Id { get; }
-        public string Name => $"Build {incompleteBuilding.StructureName}";
-        public IEnumerable<Tile> Tiles { get; }
-        public double PercentCompleted => incompleteBuilding.PercentageComplete;
-        public bool CanAbort => !buildStarted;
-        public bool Finished { get; private set; }
+        public override string Name => $"Build {incompleteBuilding.StructureName}";
+        public override double PercentCompleted => incompleteBuilding.PercentageComplete;
+        protected override bool IsCompleted => incompleteBuilding.IsCompleted;
+        protected override bool IsCancelled => incompleteBuilding.IsCancelled;
 
         public BuildingWorkerTask(
             Id<IWorkerTask> taskId,
             GameState gameState,
             IIncompleteBuilding incompleteBuilding,
             IEnumerable<Tile> tiles,
-            FactionResources.IResourceReservation resourceReservation)
+            FactionResources.IResourceReservation resourceReservation) : base(taskId, tiles, gameState, resourceReservation)
         {
-            Id = taskId;
             this.incompleteBuilding = incompleteBuilding;
-            Tiles = tiles;
-            // The resources per second gets overwritten each frame to account for worker speed changes.
-            resourceConsumer = new ResourceConsumer(gameState, resourceReservation, 0.ResourcesPerSecond());
         }
 
-        public void Progress(TimeSpan elapsedTime, IWorkerParameters workerParameters)
-        {
-            if (incompleteBuilding.IsCompleted)
-            {
-                resourceConsumer.CompleteIfNeeded();
-            }
-            if (incompleteBuilding.IsCompleted || incompleteBuilding.IsCancelled)
-            {
-                Finished = true;
-                return;
-            }
-
-            if (workerParameters.BuildingSpeed != resourceConsumer.ConsumptionRate)
-            {
-                resourceConsumer.UpdateConsumptionRate(workerParameters.BuildingSpeed);
-            }
-
-            resourceConsumer.PrepareIfNeeded();
-            if (!resourceConsumer.CanConsume)
-            {
-                return;
-            }
-
-            if (!buildStarted)
-            {
-                buildStarted = true;
-                incompleteBuilding.StartBuild();
-            }
-
-            resourceConsumer.Update();
-            updateBuildingToMatch();
-            if (resourceConsumer.IsDone)
-            {
-                incompleteBuilding.CompleteBuild();
-            }
-        }
-
-        public void OnAbort()
-        {
-            resourceConsumer.Abort();
-            incompleteBuilding.CancelBuild();
-        }
-
-        private void updateBuildingToMatch()
-        {
-            // Building was set to completed, probably because of command from server.
-            // We want to keep consuming resources though to make sure the resources stay in sync.
-            if (incompleteBuilding.IsCompleted) return;
-
-            incompleteBuilding.SetBuildProgress(resourceConsumer.PercentageDone);
-        }
+        protected override void Start() => incompleteBuilding.StartBuild();
+        protected override void Complete() => incompleteBuilding.CompleteBuild();
+        protected override void Cancel() => incompleteBuilding.CancelBuild();
+        protected override void UpdateToMatch() => incompleteBuilding.SetBuildProgress(PercentCompleted);
     }
 }
---
+++ src/Bearded.TD/Game/Simulation/Workers/WorkerTaskBase.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using Bearded.TD.Content.Models;
+using Bearded.TD.Game.Simulation.Resources;
+using Bearded.TD.Tiles;
+using Bearded.Utilities;
+using Bearded.Utilities.SpaceTime;
+
+namespace Bearded.TD.Game.Simulation.Workers
+{
+    abstract class WorkerTaskBase : IWorkerTask
+    {
+        private readonly ResourceConsumer resourceConsumer;
+        private bool started;
+
+        public Id<IWorkerTask> Id { get; }
+        public abstract string Name { get; }
+        public IEnumerable<Tile> Tiles { get; }
+        public abstract double PercentCompleted { get; }
+        public bool CanAbort => !started;
+        public bool Finished { get; private set; }
+
+        protected abstract bool IsCompleted { get; }
+        protected abstract bool IsCancelled { get; }
+
+        protected WorkerTaskBase(Id<IWorkerTask> id,
+            IEnumerable<Tile> tiles, GameState gameState, FactionResources.IResourceReservation resourceReservation)
+        {
+            Id = id;
+            Tiles = tiles;
+            // The resources per second gets overwritten each frame to account for worker speed changes.
+            resourceConsumer = new ResourceConsumer(gameState, resourceReservation, 0.ResourcesPerSecond());
+        }
+
+        public void Progress(TimeSpan elapsedTime, IWorkerParameters workerParameters)
+        {
+            if (IsCompleted)
+            {
+                resourceConsumer.CompleteIfNeeded();
+            }
+            if (IsCompleted || IsCancelled)
+            {
+                Finished = true;
+                return;
+            }
+
+            if (workerParameters.BuildingSpeed != resourceConsumer.ConsumptionRate)
+            {
+                resourceConsumer.UpdateConsumptionRate(workerParameters.BuildingSpeed);
+            }
+
+            resourceConsumer.PrepareIfNeeded();
+            if (!resourceConsumer.CanConsume)
+            {
+                return;
+            }
+
+            if (!started)
+            {
+                started = true;
+                Start();
+            }
+
+            resourceConsumer.Update();
+            if (!IsCompleted)
+            {
+                UpdateToMatch();
+            }
+            if (resourceConsumer.IsDone)
+            {
+                Complete();
+            }
+        }
+
+        public void OnAbort()
+        {
+            resourceConsumer.Abort();
+            Cancel();
+        }
+
+        protected abstract void Start();
+        protected abstract void Complete();
+        protected abstract void Cancel();
+        protected abstract void UpdateToMatch();
+    }
+}
