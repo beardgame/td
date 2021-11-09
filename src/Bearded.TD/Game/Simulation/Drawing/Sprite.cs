8625ae0ecf6326466aed398ad2e90fa595be69c5
amulware <amul@amulware.net>
2021-11-09 21:00:19 +0000

    :fire: Maybe

--- src/Bearded.TD/Game/Simulation/Drawing/Sprite.cs
+++ src/Bearded.TD/Game/Simulation/Drawing/Sprite.cs
@@ -5,7 +5,6 @@ using Bearded.TD.Content.Mods;
 using Bearded.TD.Game.Simulation.Components;
 using Bearded.TD.Rendering;
 using Bearded.TD.Rendering.Vertices;
-using Bearded.Utilities;
 using Bearded.Utilities.Geometry;
 using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
 
@@ -15,7 +14,7 @@ namespace Bearded.TD.Game.Simulation.Drawing
     class Sprite<T> : Component<T, ISpriteParameters>
         where T : IGameObject, IPositionable
     {
-        private Maybe<IDirected> ownerAsDirected;
+        private IDirected? ownerAsDirected;
 
         private IDrawableSprite<Color> sprite = null!;
 
@@ -25,7 +24,7 @@ namespace Bearded.TD.Game.Simulation.Drawing
 
         protected override void OnAdded()
         {
-            ownerAsDirected = Maybe.FromNullable(Owner as IDirected);
+            ownerAsDirected = Owner as IDirected;
             sprite = Parameters.Sprite.MakeConcreteWith(Owner.Game.Meta.SpriteRenderers, UVColorVertex.Create,
                 Parameters.Shader ?? Owner.Game.Meta.Blueprints.Shaders[ModAwareId.ForDefaultMod("default-sprite")]);
         }
@@ -39,15 +38,11 @@ namespace Bearded.TD.Game.Simulation.Drawing
             var p = Owner.Position.NumericValue;
             p.Z += Parameters.HeightOffset.NumericValue;
 
-            var angle = ownerAsDirected
-                .Select(adjustSpriteAngleToDirection)
-                .ValueOrDefault(0);
+            var angle = ownerAsDirected != null
+                ? (ownerAsDirected.Direction - 90.Degrees()).Radians
+                : 0f;
 
             sprite.Draw(p, Parameters.Size.NumericValue, angle, Parameters.Color);
         }
-
-        // ReSharper disable once StaticMemberInGenericType
-        private static readonly Func<IDirected, float> adjustSpriteAngleToDirection =
-            directed => (directed.Direction - 90.Degrees()).Radians;
     }
 }
