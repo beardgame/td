f7b2424556a0cb931b44e01185b2d931f81ab68c
paulcscharf <paul@paulscharf.com>
2021-11-25 18:20:02 +0000

    ⚔ Increase damage in overdrive

--- src/Bearded.TD/Game/Simulation/Buildings/components/Overdrive.cs
+++ src/Bearded.TD/Game/Simulation/Buildings/components/Overdrive.cs
@@ -1,19 +1,36 @@
 using Bearded.TD.Game.Simulation.Components;
+using Bearded.TD.Game.Simulation.Upgrades;
 using Bearded.TD.Rendering;
+using Bearded.TD.Shared.TechEffects;
+using Bearded.Utilities;
 using Bearded.Utilities.SpaceTime;
 
 namespace Bearded.TD.Game.Simulation.Buildings
 {
     sealed class Overdrive<T> : Component<T>
+        where T : IComponentOwner<T>, IGameObject
     {
+        private IUpgradeEffect fireRate = null!;
+        private IUpgradeEffect damageOverTime = null!;
+
         protected override void OnAdded()
         {
+            var ids = Owner.Game.GamePlayIds;
 
+            fireRate = new ParameterModifiableWithId(AttributeType.FireRate, damageModification(ids));
+            damageOverTime = new ParameterModifiableWithId(AttributeType.DamageOverTime, damageModification(ids));
+
+            fireRate.ApplyTo(Owner);
+            damageOverTime.ApplyTo(Owner);
         }
+
+        private static ModificationWithId damageModification(IdManager ids)
+            => new(ids.GetNext<Modification>(), Modification.MultiplyWith(1.5));
 
         public override void OnRemoved()
         {
-
+            fireRate.RemoveFrom(Owner);
+            damageOverTime.RemoveFrom(Owner);
         }
 
         public override void Update(TimeSpan elapsedTime)
