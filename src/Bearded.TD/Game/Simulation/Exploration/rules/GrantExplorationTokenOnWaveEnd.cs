bb3e68da9ebbed2afceb224452fb90e1b3f706bc
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-12-07 19:53:46 +0000

    🔭 Award exploration token each wave end

---
+++ src/Bearded.TD/Game/Simulation/Exploration/rules/GrantExplorationTokenOnWaveEnd.cs
@@ -0,0 +1,31 @@
+using Bearded.TD.Game.Simulation.GameLoop;
+using Bearded.TD.Game.Simulation.Rules;
+using Bearded.TD.Shared.Events;
+
+namespace Bearded.TD.Game.Simulation.Exploration
+{
+    [GameRule("grantExplorationTokenOnWaveEnd")]
+    sealed class GrantExplorationTokenOnWaveEnd : GameRule
+    {
+        public override void Execute(GameRuleContext context)
+        {
+            context.Events.Subscribe(new Listener(context.GameState.ExplorationManager));
+        }
+
+        private sealed class Listener : IListener<WaveEnded>
+        {
+            private readonly ExplorationManager explorationManager;
+
+            public Listener(ExplorationManager explorationManager)
+            {
+                this.explorationManager = explorationManager;
+            }
+
+            public void HandleEvent(WaveEnded @event)
+            {
+                explorationManager.AwardExplorationToken();
+
+            }
+        }
+    }
+}
