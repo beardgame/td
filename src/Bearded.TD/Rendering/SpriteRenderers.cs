ab89807c99666eade53eec694ba93ff26d57dd6a
paulcscharf <paul@paulscharf.com>
2021-12-19 22:23:20 +0000

    🧚‍♀️ Improve sprite renderer

--- src/Bearded.TD/Rendering/SpriteRenderers.cs
+++ src/Bearded.TD/Rendering/SpriteRenderers.cs
@@ -6,7 +6,6 @@ using Bearded.Graphics.RenderSettings;
 using Bearded.Graphics.Vertices;
 using Bearded.TD.Content.Models;
 using Bearded.TD.Rendering.Loading;
-using Bearded.TD.Utilities.Collections;
 using Bearded.Utilities.Linq;
 
 namespace Bearded.TD.Rendering;
@@ -15,17 +14,25 @@ using Renderers = List<(int DrawOrderKey, IRenderer Renderer)>;
 
 sealed class SpriteRenderers
 {
-    private sealed class DrawOrderKeyComparer : IComparer<(int DrawOrderKey, IRenderer Renderer)>
+    private sealed class DrawOrderKeyComparer : IComparer<(int DrawOrderKey, IRenderer)>
     {
-        public int Compare((int DrawOrderKey, IRenderer Renderer) x, (int DrawOrderKey, IRenderer Renderer) y)
-        {
-            return x.DrawOrderKey.CompareTo(y.DrawOrderKey);
-        }
+        public int Compare((int DrawOrderKey, IRenderer) x, (int DrawOrderKey, IRenderer) y)
+            => x.DrawOrderKey.CompareTo(y.DrawOrderKey);
     }
+
+    // ReSharper disable NotAccessedPositionalProperty.Local
+    private readonly record struct DrawGroup(SpriteDrawGroup Group, int OrderKey);
+    private readonly record struct Key(
+        Type Vertex, Type VertexData, SpriteSet SpriteSet, Shader Shader, DrawGroup DrawGroup);
+    // ReSharper restore NotAccessedPositionalProperty.Local
+
+    private static Key key<TVertex, TVertexData>(
+        SpriteSet spriteSet, Shader shader, SpriteDrawGroup group, int orderKey)
+        => new(typeof(TVertex), typeof(TVertexData), spriteSet, shader, new DrawGroup(group, orderKey));
 
     private static readonly DrawOrderKeyComparer drawOrderKeyComparer = new();
 
-    private readonly Dictionary<SpriteSet, Dictionary<(Type, Type, Shader, SpriteDrawGroup, int), object>> knownSpriteSets = new();
+    private readonly Dictionary<Key, object> knownSpriteSets = new();
     private readonly Dictionary<SpriteDrawGroup, Renderers> renderersByDrawGroup =
         Enum.GetValues<SpriteDrawGroup>().ToDictionary(g => g, _ => new Renderers());
 
@@ -38,29 +45,30 @@ sealed class SpriteRenderers
             context.Settings.ProjectionMatrix,
             context.Settings.ViewMatrix,
             context.Settings.FarPlaneDistance,
-            context.Settings.ViewMatrixLevel
+            context.Settings.ViewMatrixLevel,
+            context.Settings.CameraPosition,
+            context.Settings.FarPlaneBaseCorner,
+            context.Settings.FarPlaneUnitX,
+            context.Settings.FarPlaneUnitY,
+            context.Settings.Time,
         };
     }
 
     public DrawableSpriteSet<TVertex, TVertexData> GetOrCreateDrawableSpriteSetFor<TVertex, TVertexData>(
-        SpriteSet spriteSet, Shader shader, SpriteDrawGroup drawGroup, int drawGroupOrderKey, Func<DrawableSpriteSet<TVertex, TVertexData>> createDrawable)
+        SpriteSet spriteSet, Shader shader, SpriteDrawGroup drawGroup, int drawGroupOrderKey,
+        Func<DrawableSpriteSet<TVertex, TVertexData>> createDrawable)
         where TVertex : struct, IVertexData
     {
-        var key = (typeof(TVertex), typeof(TVertexData), shader, drawGroup, drawGroupOrderKey);
+        var key = key<TVertex, TVertexData>(spriteSet, shader, drawGroup, drawGroupOrderKey);
 
-        var spriteSets = knownSpriteSets.GetValueOrInsertNewDefaultFor(spriteSet);
+        if (knownSpriteSets.TryGetValue(key, out var obj))
+            return (DrawableSpriteSet<TVertex, TVertexData>)obj;
 
-        var drawable = (DrawableSpriteSet<TVertex, TVertexData>)
-            spriteSets.GetOrInsert(key, createDrawableAndRegisterDefaultRenderer);
+        var newDrawable = createDrawable();
+        createAndRegisterRenderer(newDrawable, drawGroup, drawGroupOrderKey);
+        knownSpriteSets.Add(key, newDrawable);
 
-        return drawable;
-
-        DrawableSpriteSet<TVertex, TVertexData> createDrawableAndRegisterDefaultRenderer()
-        {
-            var d = createDrawable();
-            createAndRegisterRenderer(spriteSet, d, drawGroup, drawGroupOrderKey);
-            return d;
-        }
+        return newDrawable;
     }
 
     public IRenderer CreateCustomRendererFor<TVertex, TVertexData>(
@@ -68,15 +76,13 @@ sealed class SpriteRenderers
         IRenderSetting[] customRenderSettings)
         where TVertex : struct, IVertexData
     {
-        var renderer = drawable.CreateRendererWithSettings(
+        return drawable.CreateRendererWithSettings(
             customRenderSettings.Concat(defaultRenderSettings)
         );
-
-        return renderer;
     }
 
     private void createAndRegisterRenderer<TVertex, TVertexData>(
-        SpriteSet spriteSet, DrawableSpriteSet<TVertex, TVertexData> drawable,
+        DrawableSpriteSet<TVertex, TVertexData> drawable,
         SpriteDrawGroup drawGroup, int drawGroupOrderKey)
         where TVertex : struct, IVertexData
     {
@@ -98,12 +104,9 @@ sealed class SpriteRenderers
 
     public void Dispose()
     {
-        foreach (var spriteSet in knownSpriteSets.Values)
+        foreach (var spriteSet in knownSpriteSets.Values.Cast<IDisposable>())
         {
-            foreach (var drawableSpriteSet in spriteSet.Values.Cast<IDisposable>())
-            {
-                drawableSpriteSet.Dispose();
-            }
+            spriteSet.Dispose();
         }
 
         foreach (var drawGroup in renderersByDrawGroup.Values)
@@ -117,12 +120,9 @@ sealed class SpriteRenderers
 
     public void ClearAll()
     {
-        foreach (var spriteSet in knownSpriteSets.Values)
+        foreach (var spriteSet in knownSpriteSets.Values.Cast<IClearable>())
         {
-            foreach (var drawableSpriteSet in spriteSet.Values.Cast<IClearable>())
-            {
-                drawableSpriteSet.Clear();
-            }
+            spriteSet.Clear();
         }
     }
 }
