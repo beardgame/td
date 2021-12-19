6bd04c3a9d9cbe3a2cc88848564cf1c84d3ad1f3
paulcscharf <paul@paulscharf.com>
2021-12-19 22:23:34 +0000

    👻 Improve ghost rendering

--- src/Bearded.TD/assets/mods/default/gfx/shaders/building-ghost.fs
+++ src/Bearded.TD/assets/mods/default/gfx/shaders/building-ghost.fs
@@ -10,8 +10,6 @@ in vec4 fragmentColor;
 in float fragmentDepth;
 
 out vec4 outRGBA;
-out vec4 outNormal;
-out vec4 outDepth;
 
 void main()
 {
@@ -24,17 +22,20 @@ void main()
     float a = aX * aX + aY * aY;
 
 
-    vec3 nX = (texture(diffuse, fragmentUV + uvX).xyz * 2 - 1) - (texture(diffuse, fragmentUV - uvX).xyz * 2 - 1);
-    vec3 nY = (texture(diffuse, fragmentUV + uvY).xyz * 2 - 1) - (texture(diffuse, fragmentUV - uvY).xyz * 2 - 1);
+    vec3 nX = (texture(diffuse, fragmentUV + uvX).xyz * 2 - 1)
+            - (texture(diffuse, fragmentUV - uvX).xyz * 2 - 1);
+    vec3 nY = (texture(diffuse, fragmentUV + uvY).xyz * 2 - 1)
+            - (texture(diffuse, fragmentUV - uvY).xyz * 2 - 1);
 
     float n = dot(nX, nX) + dot(nY, nY);
 
-    bool isOutline = n > 0.5 || a > 0.01;
+    bool isOutline = n > 0.01 || a > 0.01;
+
     if (isOutline)
     {
-        outRGBA = vec4(fragmentColor.rgb, 0);
-        outNormal = vec4(0);
-        outDepth = vec4(fragmentDepth, 0, 0, 1);
+        float flicker = 0.5 + 0.3 * sin(fragmentPosition.y * 3 + time * 5);
+
+        outRGBA = vec4(fragmentColor.rgb * max(a, n) * flicker, 0);
     }
     else
     {
