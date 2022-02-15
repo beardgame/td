f8c6de3504e539258e9efde89bd0286a83ea09c5
paulcscharf <paul@paulscharf.com>
2022-02-15 21:57:25 +0000

    🍧 Make towers only shoot if target is in specified cone

--- src/Bearded.TD/Content/Models/components/ITargetEnemiesInRange.cs
+++ src/Bearded.TD/Content/Models/components/ITargetEnemiesInRange.cs
@@ -1,4 +1,5 @@
 using Bearded.TD.Shared.TechEffects;
+using Bearded.Utilities.Geometry;
 using Bearded.Utilities.SpaceTime;
 
 namespace Bearded.TD.Content.Models;
@@ -8,4 +9,6 @@ interface ITargetEnemiesInRange : IParametersTemplate<ITargetEnemiesInRange>
     [Modifiable(Type = AttributeType.Range)] Unit Range { get; }
     [Modifiable(0.2)] TimeSpan NoTargetIdleInterval { get; }
     [Modifiable(1)] TimeSpan ReCalculateTilesInRangeInterval { get; }
+
+    Angle? ConeOfFire { get; }
 }
