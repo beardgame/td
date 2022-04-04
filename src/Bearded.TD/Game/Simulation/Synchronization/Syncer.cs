2bc24fee41a58952e4bfdbf5d401a617bd774bf6
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-04-04 19:58:36 +0200

    💄 Make syncing code non-generic

--- src/Bearded.TD/Game/Simulation/Synchronization/Syncer.cs
+++ src/Bearded.TD/Game/Simulation/Synchronization/Syncer.cs
@@ -7,10 +7,10 @@ using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
 
 namespace Bearded.TD.Game.Simulation.Synchronization;
 
-sealed class Syncer : Component, ISyncer<GameObject>
+sealed class Syncer : Component, ISyncer
 {
     private IIdProvider? idProvider;
-    public Id<GameObject> EntityId =>
+    public Id<GameObject> GameObjectId =>
         idProvider?.Id ?? throw new InvalidOperationException("Synced object must have an ID.");
 
     protected override void OnAdded()
