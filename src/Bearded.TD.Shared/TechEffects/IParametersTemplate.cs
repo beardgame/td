5a011a997158379e78c5d77bbb44b14185301eca
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-07-31 16:36:27 +0200

    🪓 Split non-generic part of parameters template

--- src/Bearded.TD.Shared/TechEffects/IParametersTemplate.cs
+++ src/Bearded.TD.Shared/TechEffects/IParametersTemplate.cs
@@ -2,12 +2,16 @@
 
 namespace Bearded.TD.Shared.TechEffects;
 
-public interface IParametersTemplate<out T> where T : IParametersTemplate<T>
+public interface IParametersTemplate
 {
     bool HasAttributeOfType(AttributeType type);
     bool AddModification(AttributeType type, Modification modification);
     bool AddModificationWithId(AttributeType type, ModificationWithId modification);
     bool UpdateModification(AttributeType type, Id<Modification> id, Modification modification);
     bool RemoveModification(AttributeType type, Id<Modification> id);
+}
+
+public interface IParametersTemplate<out T> : IParametersTemplate where T : IParametersTemplate<T>
+{
     T CreateModifiableInstance();
 }
