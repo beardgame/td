9ec0f5d2b159eb76ba6e7a756f1272e0dcbde6d7
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-08-02 22:18:16 +0200

    🔫 Implement burst upgrade

---
+++ src/Bearded.TD/Game/Simulation/Weapons/components/Burst.cs
@@ -0,0 +1,72 @@
+using Bearded.TD.Game.Simulation.GameObjects;
+using Bearded.TD.Shared.Events;
+using Bearded.TD.Shared.TechEffects;
+using Bearded.Utilities.SpaceTime;
+
+namespace Bearded.TD.Game.Simulation.Weapons;
+
+[Component("burst")]
+sealed class Burst : Component<Burst.IParameters>, IListener<ShotProjectile>, IPreviewListener<PreviewDelayNextShot>
+{
+    public interface IParameters : IParametersTemplate<IParameters>
+    {
+        public int BurstSize { get; }
+
+        public double FireRateFactor { get; }
+
+        public double OverallDamageFactor { get; }
+    }
+
+    private int shotsFiredInBurst;
+    private Instant lastShotFired = Instant.Zero;
+    private TimeSpan lastRecordedDelay = TimeSpan.Zero;
+
+    private double correctionFactor => 1 / Parameters.OverallDamageFactor - 1 / Parameters.FireRateFactor;
+    private TimeSpan delayBetweenBursts => lastRecordedDelay * Parameters.BurstSize * correctionFactor;
+
+    public Burst(IParameters parameters) : base(parameters) { }
+
+    protected override void OnAdded()
+    {
+        Events.Subscribe<ShotProjectile>(this);
+        Events.Subscribe<PreviewDelayNextShot>(this);
+    }
+
+    public override void Update(TimeSpan elapsedTime)
+    {
+        // Reset burst if longer than the expected burst delay has passed.
+        if (shotsFiredInBurst > 0 && Owner.Game.Time - lastShotFired >= delayBetweenBursts)
+        {
+            shotsFiredInBurst = 0;
+            Owner.Game.Meta.Logger.Debug?.Log(correctionFactor);
+            Owner.Game.Meta.Logger.Debug?.Log($"Resetting burst because of time passed. Last shot: {lastShotFired}, now: {Owner.Game.Time}, delay: ${delayBetweenBursts}");
+        }
+    }
+
+    public override void OnRemoved()
+    {
+        base.OnRemoved();
+        Events.Unsubscribe<ShotProjectile>(this);
+        Events.Unsubscribe<PreviewDelayNextShot>(this);
+    }
+
+    public void HandleEvent(ShotProjectile @event)
+    {
+        shotsFiredInBurst++;
+        lastShotFired = Owner.Game.Time;
+    }
+
+    public void PreviewEvent(ref PreviewDelayNextShot @event)
+    {
+        lastRecordedDelay = @event.Delay;
+        if (shotsFiredInBurst < Parameters.BurstSize)
+        {
+            @event = new PreviewDelayNextShot(@event.Delay / Parameters.FireRateFactor);
+            return;
+        }
+
+        @event = new PreviewDelayNextShot(delayBetweenBursts);
+        shotsFiredInBurst = 0;
+        Owner.Game.Meta.Logger.Debug?.Log("Resetting burst because of numbers of shots fired");
+    }
+}
