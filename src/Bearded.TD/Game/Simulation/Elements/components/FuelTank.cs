57849a6e47e0c9c364bbc2f81e8bbcffc2f8eeef
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-08-22 20:40:56 +0200

    🧯 Add fuel tank component

---
+++ src/Bearded.TD/Game/Simulation/Elements/components/FuelTank.cs
@@ -0,0 +1,104 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Bearded.TD.Game.Simulation.GameLoop;
+using Bearded.TD.Game.Simulation.GameObjects;
+using Bearded.TD.Game.Simulation.Weapons;
+using Bearded.TD.Shared.Events;
+using Bearded.TD.Shared.TechEffects;
+using Bearded.Utilities.SpaceTime;
+
+namespace Bearded.TD.Game.Simulation.Elements;
+
+[Component("fuelTank")]
+sealed class FuelTank : Component<FuelTank.IParameters>, IListener<ShotProjectile>, IListener<WaveEnded>
+{
+    public interface IParameters : IParametersTemplate<IParameters>
+    {
+        [Modifiable(Type = AttributeType.FuelCapacity)]
+        public int FuelCapacity { get; }
+    }
+
+    private ImmutableArray<KnownWeapon> knownWeapons;
+    private int fuelUsed;
+    private WeaponDisabledReason? disabledReason;
+
+    public FuelTank(IParameters parameters) : base(parameters) { }
+
+    protected override void OnAdded() { }
+
+    public override void Activate()
+    {
+        base.Activate();
+        Owner.Game.Meta.Events.Subscribe<WaveEnded>(this);
+        knownWeapons = Owner.GetComponents<ITurret>()
+            .Select(t =>
+            {
+                var weapon = t.Weapon;
+                var spy = new ListenerComponent(this);
+                weapon.AddComponent(spy);
+                return new KnownWeapon(weapon, weapon.GetComponents<IWeaponState>().Single(), spy);
+            })
+            .ToImmutableArray();
+    }
+
+    public override void OnRemoved()
+    {
+        base.OnRemoved();
+        foreach (var knownWeapon in knownWeapons)
+        {
+            knownWeapon.Weapon.RemoveComponent(knownWeapon.Spy);
+        }
+    }
+
+    public override void Update(TimeSpan elapsedTime) { }
+
+    public void HandleEvent(ShotProjectile @event)
+    {
+        fuelUsed++;
+        if (fuelUsed < Parameters.FuelCapacity || disabledReason != null)
+        {
+            return;
+        }
+
+        disabledReason = new WeaponDisabledReason();
+        foreach (var knownWeapon in knownWeapons)
+        {
+            knownWeapon.State.Disable(disabledReason);
+        }
+    }
+
+    public void HandleEvent(WaveEnded @event)
+    {
+        fuelUsed = 0;
+    }
+
+    private readonly record struct KnownWeapon(GameObject Weapon, IWeaponState State, ListenerComponent Spy);
+
+    private sealed class ListenerComponent : Component, IListener<ShotProjectile>
+    {
+        private readonly FuelTank @delegate;
+
+        public ListenerComponent(FuelTank @delegate)
+        {
+            this.@delegate = @delegate;
+        }
+
+        protected override void OnAdded()
+        {
+            Events.Subscribe(this);
+        }
+
+        public override void OnRemoved()
+        {
+            base.OnRemoved();
+            Events.Unsubscribe(this);
+        }
+
+        public void HandleEvent(ShotProjectile @event)
+        {
+            @delegate.HandleEvent(@event);
+        }
+
+        public override void Update(TimeSpan elapsedTime) {}
+    }
+}
