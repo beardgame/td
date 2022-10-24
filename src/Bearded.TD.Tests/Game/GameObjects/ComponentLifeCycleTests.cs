dc3889d1dd7fbfcdcca5869c47306aa5d066c024
paulcscharf <paul@paulscharf.com>
2022-10-24 21:08:50 +0100

    ✅

--- src/Bearded.TD.Tests/Game/GameObjects/ComponentLifeCycleTests.cs
+++ src/Bearded.TD.Tests/Game/GameObjects/ComponentLifeCycleTests.cs
@@ -27,6 +27,7 @@ public sealed class ComponentLifeCycleTests
         typeof(GhostBuildingRenderer), // Requests hardcoded access to a blueprint.
         typeof(HitTargetOnActivate), // Expects a target property to always be present.
         typeof(ProjectileEmitter), // Attempts to instantiate a blueprint on added.
+        typeof(SpawnObjectOnBuildingPlaced), // Expects building state to always be present.
         typeof(Turret) // Attempts to instantiate a blueprint on added.
     );
 
