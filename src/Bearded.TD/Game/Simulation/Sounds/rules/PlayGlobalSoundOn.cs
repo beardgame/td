3cf43edec64ec114b8f753ff684986cbc140bb51
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-11-07 21:53:11 +0100

    🎵🌊 Add sound on wave start and complete

---
+++ src/Bearded.TD/Game/Simulation/Sounds/rules/PlayGlobalSoundOn.cs
@@ -0,0 +1,38 @@
+using System;
+using Bearded.TD.Audio;
+using Bearded.TD.Game.Simulation.Events;
+using Bearded.TD.Game.Simulation.Rules;
+using Bearded.TD.Shared.Events;
+
+namespace Bearded.TD.Game.Simulation.Sounds;
+
+abstract class PlayGlobalSoundOn<TParameters, TEvent> : GameRule<TParameters>
+    where TEvent : struct, IGlobalEvent
+{
+    protected abstract ISoundEffect SoundEffect { get; }
+
+    protected PlayGlobalSoundOn(TParameters parameters) : base(parameters) { }
+
+    public override void Execute(GameRuleContext context)
+    {
+        context.Events.Subscribe(new Listener(context.GameState.Meta.SoundScape, () => SoundEffect));
+    }
+
+    private sealed class Listener : IListener<TEvent>
+    {
+        private readonly ISoundScape soundScape;
+        private readonly Func<ISoundEffect> getSoundEffect;
+
+        public Listener(ISoundScape soundScape, Func<ISoundEffect> getSoundEffect)
+        {
+            this.soundScape = soundScape;
+            this.getSoundEffect = getSoundEffect;
+        }
+
+        public void HandleEvent(TEvent @event)
+        {
+            var soundEffect = getSoundEffect();
+            soundScape.PlayGlobalSound(soundEffect.Sound);
+        }
+    }
+}
