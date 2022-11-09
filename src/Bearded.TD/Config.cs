8c576842cd8a03d136e7f04e0642b682ccd624c0
Tom Rijnbeek <tom@tomrijnbeek.me>
2022-11-09 16:53:58 +0100

    ⬆ Bump version number

--- src/Bearded.TD/Config.cs
+++ src/Bearded.TD/Config.cs
@@ -8,7 +8,7 @@ namespace Bearded.TD;
 
 static class Config
 {
-    public const string BaseVersionString = "0.10";
+    public const string BaseVersionString = "0.11";
 
 #if DEBUG
     private const string versionSuffix = "-dev";
