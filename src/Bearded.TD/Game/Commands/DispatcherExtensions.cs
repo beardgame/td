d38fa812ad0ed7ac13ca8a5bbf22427f0775c2bf
paulcscharf <paul@paulscharf.com>
2022-11-09 10:10:08 +0000

    💆‍♂️ Make dispatcher extensions less generic

--- src/Bearded.TD/Game/Commands/DispatcherExtensions.cs
+++ src/Bearded.TD/Game/Commands/DispatcherExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Bearded.TD.Commands;
 using Bearded.TD.Game.Simulation;
+using Bearded.TD.Game.Simulation.GameObjects;
 
 namespace Bearded.TD.Game.Commands;
 
@@ -8,85 +9,41 @@ static class DispatcherExtensions
 {
     private static IDispatcher<GameInstance> d(IGameObject obj) => obj.Game.Meta.Dispatcher;
 
-    // direct pass throughs
-
-    public static void Sync<T>(this T obj, Action action)
-        where T : IGameObject
+    public static void Sync(this GameObject obj, Action<ICommandDispatcher<GameInstance>> action)
         => d(obj).RunOnlyOnServer(action);
-    public static void Sync<T, T1>(this T obj, Action<T1> action, T1 p1)
-        where T : IGameObject
+    public static void Sync<T1>(this GameObject obj, Action<T1, ICommandDispatcher<GameInstance>> action, T1 p1)
         => d(obj).RunOnlyOnServer(action, p1);
-    public static void Sync<T, T1, T2>(this T obj, Action<T1, T2> action, T1 p1, T2 p2)
-        where T : IGameObject
+    public static void Sync<T1, T2>(this GameObject obj, Action<T1, T2, ICommandDispatcher<GameInstance>> action, T1 p1, T2 p2)
         => d(obj).RunOnlyOnServer(action, p1, p2);
-    public static void Sync<T, T1, T2, T3>(this T obj, Action<T1, T2, T3> action, T1 p1, T2 p2, T3 p3)
-        where T : IGameObject
+    public static void Sync<T1, T2, T3>(this GameObject obj, Action<T1, T2, T3, ICommandDispatcher<GameInstance>> action, T1 p1, T2 p2, T3 p3)
         => d(obj).RunOnlyOnServer(action, p1, p2, p3);
-    public static void Sync<T, T1, T2, T3, T4>(this T obj, Action<T1, T2, T3, T4> action, T1 p1, T2 p2, T3 p3, T4 p4)
-        where T : IGameObject
+    public static void Sync<T1, T2, T3, T4>(this GameObject obj, Action<T1, T2, T3, T4, ICommandDispatcher<GameInstance>> action, T1 p1, T2 p2, T3 p3, T4 p4)
         => d(obj).RunOnlyOnServer(action, p1, p2, p3, p4);
 
-    public static void Sync<T>(this T obj, Action<ICommandDispatcher<GameInstance>> action)
-        where T : IGameObject
-        => d(obj).RunOnlyOnServer(action);
-    public static void Sync<T, T1>(this T obj, Action<T1, ICommandDispatcher<GameInstance>> action, T1 p1)
-        where T : IGameObject
-        => d(obj).RunOnlyOnServer(action, p1);
-    public static void Sync<T, T1, T2>(this T obj, Action<T1, T2, ICommandDispatcher<GameInstance>> action, T1 p1, T2 p2)
-        where T : IGameObject
-        => d(obj).RunOnlyOnServer(action, p1, p2);
-    public static void Sync<T, T1, T2, T3>(this T obj, Action<T1, T2, T3, ICommandDispatcher<GameInstance>> action, T1 p1, T2 p2, T3 p3)
-        where T : IGameObject
-        => d(obj).RunOnlyOnServer(action, p1, p2, p3);
-    public static void Sync<T, T1, T2, T3, T4>(this T obj, Action<T1, T2, T3, T4, ICommandDispatcher<GameInstance>> action, T1 p1, T2 p2, T3 p3, T4 p4)
-        where T : IGameObject
-        => d(obj).RunOnlyOnServer(action, p1, p2, p3, p4);
-
-    public static void Sync<T>(this T obj, Func<ISerializableCommand<GameInstance>> func)
-        where T : IGameObject
+    public static void Sync(this GameObject obj, Func<ISerializableCommand<GameInstance>> func)
         => d(obj).RunOnlyOnServer(func);
-    public static void Sync<T, T1>(this T obj, Func<T1, ISerializableCommand<GameInstance>> func, T1 p1)
-        where T : IGameObject
+    public static void Sync<T1>(this GameObject obj, Func<T1, ISerializableCommand<GameInstance>> func, T1 p1)
         => d(obj).RunOnlyOnServer(func, p1);
-    public static void Sync<T, T1, T2>(this T obj, Func<T1, T2, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2)
-        where T : IGameObject
+    public static void Sync<T1, T2>(this GameObject obj, Func<T1, T2, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2)
         => d(obj).RunOnlyOnServer(func, p1, p2);
-    public static void Sync<T, T1, T2, T3>(this T obj, Func<T1, T2, T3, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2, T3 p3)
-        where T : IGameObject
+    public static void Sync<T1, T2, T3>(this GameObject obj, Func<T1, T2, T3, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2, T3 p3)
         => d(obj).RunOnlyOnServer(func, p1, p2, p3);
-    public static void Sync<T, T1, T2, T3, T4>(this T obj, Func<T1, T2, T3, T4, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2, T3 p3, T4 p4)
-        where T : IGameObject
+    public static void Sync<T1, T2, T3, T4>(this GameObject obj, Func<T1, T2, T3, T4, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2, T3 p3, T4 p4)
         => d(obj).RunOnlyOnServer(func, p1, p2, p3, p4);
 
-    // convenience methods
-
-    public static void Sync<T>(this T obj, Action<T> action)
-        where T : IGameObject
+    public static void Sync(this GameObject obj, Action<GameObject, ICommandDispatcher<GameInstance>> action)
         => d(obj).RunOnlyOnServer(action, obj);
-    public static void Sync<T>(this T obj, Action<GameState> action)
-        where T : IGameObject
+    public static void Sync(this  GameObject obj, Action<GameState, ICommandDispatcher<GameInstance>> action)
         => d(obj).RunOnlyOnServer(action, obj.Game);
 
-    public static void Sync<T>(this T obj, Action<T, ICommandDispatcher<GameInstance>> action)
-        where T : IGameObject
-        => d(obj).RunOnlyOnServer(action, obj);
-    public static void Sync<T>(this T obj, Action<GameState, ICommandDispatcher<GameInstance>> action)
-        where T : IGameObject
-        => d(obj).RunOnlyOnServer(action, obj.Game);
-
-    public static void Sync<T>(this T obj, Func<T, ISerializableCommand<GameInstance>> func)
-        where T : IGameObject
+    public static void Sync(this GameObject obj, Func<GameObject, ISerializableCommand<GameInstance>> func)
         => d(obj).RunOnlyOnServer(func, obj);
-    public static void Sync<T>(this T obj, Func<GameState, ISerializableCommand<GameInstance>> func)
-        where T : IGameObject
+    public static void Sync(this GameObject obj, Func<GameState, ISerializableCommand<GameInstance>> func)
         => d(obj).RunOnlyOnServer(func, obj.Game);
-    public static void Sync<T, T1>(this T obj, Func<GameState, T1, ISerializableCommand<GameInstance>> func, T1 p1)
-        where T : IGameObject
+    public static void Sync<T1>(this GameObject obj, Func<GameState, T1, ISerializableCommand<GameInstance>> func, T1 p1)
         => d(obj).RunOnlyOnServer(func, obj.Game, p1);
-    public static void Sync<T, T1, T2>(this T obj, Func<GameState, T1, T2, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2)
-        where T : IGameObject
+    public static void Sync<T1, T2>(this GameObject obj, Func<GameState, T1, T2, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2)
         => d(obj).RunOnlyOnServer(func, obj.Game, p1, p2);
-    public static void Sync<T, T1, T2, T3>(this T obj, Func<GameState, T1, T2, T3, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2, T3 p3)
-        where T : IGameObject
+    public static void Sync<T1, T2, T3>(this GameObject obj, Func<GameState, T1, T2, T3, ISerializableCommand<GameInstance>> func, T1 p1, T2 p2, T3 p3)
         => d(obj).RunOnlyOnServer(func, obj.Game, p1, p2, p3);
 }
