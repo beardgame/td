8e6b50af9cfe3cbef06004ebdf026837b0e66317
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-02-13 21:52:54 +0100

    👩‍🎨 Add debug drawing for elemental resistance

--- src/Bearded.TD/Game/Simulation/Damage/components/DamageResistances.cs
+++ src/Bearded.TD/Game/Simulation/Damage/components/DamageResistances.cs
@@ -1,11 +1,15 @@
 using System.Collections.Immutable;
+using Bearded.Graphics.Shapes;
+using Bearded.TD.Game.Simulation.Drawing;
 using Bearded.TD.Game.Simulation.GameObjects;
+using Bearded.TD.Meta;
+using Bearded.TD.Rendering;
 using Bearded.TD.Shared.Events;
 using Bearded.Utilities.SpaceTime;
 
 namespace Bearded.TD.Game.Simulation.Damage;
 
-sealed class DamageResistances : Component, IPreviewListener<PreviewTakeDamage>
+sealed class DamageResistances : Component, IPreviewListener<PreviewTakeDamage>, IListener<DrawComponents>
 {
     private readonly ImmutableDictionary<DamageType, float> resistances;
 
@@ -14,15 +18,16 @@ sealed class DamageResistances : Component, IPreviewListener<PreviewTakeDamage>
         this.resistances = resistances;
     }
 
-    protected override void OnAdded() { }
     protected override void OnAdded()
     {
         Events.Subscribe<PreviewTakeDamage>(this);
+        Events.Subscribe<DrawComponents>(this);
     }
 
     public override void OnRemoved()
     {
         Events.Unsubscribe<PreviewTakeDamage>(this);
+        Events.Unsubscribe<DrawComponents>(this);
         base.OnRemoved();
     }
 
@@ -35,6 +40,29 @@ sealed class DamageResistances : Component, IPreviewListener<PreviewTakeDamage>
         if (resistances.TryGetValue(@event.TypedDamage.Type, out var resistance))
         {
             @event = @event.ResistedWith(resistance);
+        }
+    }
+
+    public void HandleEvent(DrawComponents @event)
+    {
+        if (UserSettings.Instance.Debug.DamageResistances)
+        {
+            debugDrawResistances(@event.Core);
+        }
+    }
+
+    private void debugDrawResistances(CoreDrawers coreDrawers)
+    {
+        const float radius = 0.3f;
+        const float fullResistanceLineWidth = radius / 6;
+
+        var currentRadius = radius;
+        foreach (var (damageType, amount) in resistances)
+        {
+            var lineWidth = fullResistanceLineWidth * amount;
+            coreDrawers.Primitives.DrawCircle(
+                Owner.Position.NumericValue.Xy, currentRadius, lineWidth, damageType.GetColor());
+            currentRadius -= lineWidth;
         }
     }
 }
