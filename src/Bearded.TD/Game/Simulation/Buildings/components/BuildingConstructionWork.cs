f94e298e01de852805ae054b38afaa27abe98be9
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-03-23 12:17:53 +0100

    🐛 Ensure build starts if enough resources after refund

--- src/Bearded.TD/Game/Simulation/Buildings/components/BuildingConstructionWork.cs
+++ src/Bearded.TD/Game/Simulation/Buildings/components/BuildingConstructionWork.cs
@@ -5,11 +5,18 @@ namespace Bearded.TD.Game.Simulation.Buildings;
 
 sealed class BuildingConstructionWork : BuildingWork
 {
+    private readonly ResourceAmount additionalCost;
+
+    private ResourceAmount baseCost => Owner.GetComponents<ICost>().SingleOrDefault()?.Resources ?? ResourceAmount.Zero;
+
     public new ResourceAmount? ResourcesInvestedSoFar => base.ResourcesInvestedSoFar;
 
-    protected override ResourceAmount Cost =>
-        Owner.GetComponents<ICost>().SingleOrDefault()?.Resources ?? ResourceAmount.Zero;
+    protected override ResourceAmount Cost => baseCost + additionalCost;
+
     protected override ResourceRate ConsumptionRate => Constants.Game.Resources.ConstructionSpeed;
 
-    public BuildingConstructionWork(IIncompleteBuilding incompleteBuilding) : base(incompleteBuilding) { }
+    public BuildingConstructionWork(IIncompleteBuilding incompleteBuilding, ResourceAmount additionalCost) : base(incompleteBuilding)
+    {
+        this.additionalCost = additionalCost;
+    }
 }
