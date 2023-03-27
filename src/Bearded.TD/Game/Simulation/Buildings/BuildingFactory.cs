0c8257d9f8640dd5f74aeef30450c7d7d16490e9
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-03-27 21:30:47 +0200

    ✂ Give towers manual override capabilities individually

--- src/Bearded.TD/Game/Simulation/Buildings/BuildingFactory.cs
+++ src/Bearded.TD/Game/Simulation/Buildings/BuildingFactory.cs
@@ -31,8 +31,6 @@ static class BuildingFactory
         }
 
         building.AddComponent(new GhostBuildingRenderer());
-        building.AddComponent(new AllowManualControl());
-        building.AddComponent(new AllowManualOverdrive());
         building.AddComponent(new BuildingStateManager());
         building.AddComponent(new BuildingUpgradeManager());
         building.AddComponent(new DamageAttributor());
