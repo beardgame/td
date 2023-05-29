98879b863a72df2fac27e0d3a5eca7e79c75dd04
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-05-29 20:08:02 +0200

    🔥 Remove damage over time from overdrive

--- src/Bearded.TD/Game/Simulation/Buildings/components/Overdrive.cs
+++ src/Bearded.TD/Game/Simulation/Buildings/components/Overdrive.cs
@@ -1,12 +1,10 @@
 using System;
-using Bearded.TD.Game.Simulation.Damage;
 using Bearded.TD.Game.Simulation.Elements;
 using Bearded.TD.Game.Simulation.Elements.events;
 using Bearded.TD.Game.Simulation.GameObjects;
 using Bearded.TD.Game.Simulation.Upgrades;
 using Bearded.TD.Shared.Events;
 using Bearded.TD.Shared.TechEffects;
-using Bearded.TD.Utilities;
 using Bearded.Utilities;
 using Bearded.Utilities.SpaceTime;
 using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
@@ -15,15 +13,11 @@ namespace Bearded.TD.Game.Simulation.Buildings;
 
 sealed class Overdrive : Component, IPreviewListener<PreviewTemperatureTick>
 {
-    private static readonly TimeSpan damageInterval = 0.5.S();
-    private const float damagePercentile = 0.025f;
     private static readonly TemperatureRate baseTemperatureIncreaseRate = new(14);
     private const float temperatureIncreaseExponent = 1.04f;
 
     private IUpgradeReceipt? upgradeReceipt;
     private Instant? startTime;
-
-    private Instant nextDamageTime;
 
     protected override void OnAdded() {}
 
@@ -41,7 +35,6 @@ sealed class Overdrive : Component, IPreviewListener<PreviewTemperatureTick>
 
         upgradeReceipt = Owner.ApplyUpgrade(upgrade);
         startTime = Owner.Game.Time;
-        nextDamageTime = Owner.Game.Time + damageInterval;
 
         Events.Subscribe(this);
     }
@@ -55,30 +48,7 @@ sealed class Overdrive : Component, IPreviewListener<PreviewTemperatureTick>
         Events.Unsubscribe(this);
     }
 
-    public override void Update(TimeSpan elapsedTime)
-    {
-        if (nextDamageTime > Owner.Game.Time)
-        {
-            return;
-        }
-
-        damageOwner();
-
-        nextDamageTime += damageInterval;
-    }
-
-    private void damageOwner()
-    {
-        var damage = new HitPoints(
-            Owner.TryGetSingleComponent<IHealth>(out var health)
-                ? MoreMath.CeilToInt(health.MaxHealth.NumericValue * damagePercentile)
-                : 10);
-
-        var overdriveDamage = new TypedDamage(damage, DamageType.DivineIntervention);
-
-        DamageExecutor.FromDamageSource(null).TryDoDamage(Owner, overdriveDamage, Hit.FromSelf());
-
-    }
+    public override void Update(TimeSpan elapsedTime) { }
 
     public void PreviewEvent(ref PreviewTemperatureTick @event)
     {
@@ -89,7 +59,6 @@ sealed class Overdrive : Component, IPreviewListener<PreviewTemperatureTick>
 
         var factor = MathF.Pow(temperatureIncreaseExponent, (float) timeSinceStart.NumericValue);
         var rate = baseTemperatureIncreaseRate * factor;
-        Console.WriteLine($"{rate} after {timeSinceStart}");
         @event = @event.WithAddedRate(rate);
     }
 }
