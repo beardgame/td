15793358653a504de2f7519a3fdde05406014d7d
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-07-17 21:18:38 +0200

    🚧 Add proportional enumerable

---
+++ src/Bearded.TD.Tests/Utilities/Collections/ProportionalEnumerableTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using Bearded.TD.Utilities.Collections;
+using FluentAssertions;
+using Xunit;
+
+namespace Bearded.TD.Tests.Utilities.Collections;
+
+public sealed class ProportionalEnumerableTests
+{
+    [Fact]
+    public void RepeatsSingleElement()
+    {
+        const string obj = "object";
+        var enumerable = new ProportionalEnumerable<object>(new Dictionary<object, int>
+        {
+            { obj, 3 }
+        });
+
+        enumerable.ToImmutableArray().Should().ContainInConsecutiveOrder(obj, obj, obj);
+    }
+
+    [Fact]
+    public void MixesTwoElements()
+    {
+        const string obj1 = "object 1";
+        const string obj2 = "object 2";
+        var enumerable = new ProportionalEnumerable<object>(new Dictionary<object, int>
+        {
+            { obj1, 3 },
+            { obj2, 2 }
+        });
+
+        enumerable.ToImmutableArray().Should().ContainInConsecutiveOrder(obj1, obj2, obj1, obj2, obj1);
+    }
+
+    [Fact]
+    public void AlternatesElementsWithEqualCounts()
+    {
+        const string obj1 = "object 1";
+        const string obj2 = "object 2";
+        var enumerable = new ProportionalEnumerable<object>(new Dictionary<object, int>
+        {
+            { obj1, 2 },
+            { obj2, 2 }
+        });
+
+        enumerable.ToImmutableArray().Should().ContainInConsecutiveOrder(obj1, obj2, obj1, obj2);
+    }
+}
---
+++ src/Bearded.TD/Utilities/Collections/ProportionalEnumerable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Bearded.Utilities.Collections;
+
+namespace Bearded.TD.Utilities.Collections;
+
+sealed class ProportionalEnumerable<T> : IEnumerable<T>
+{
+    private readonly IReadOnlyDictionary<T, int> targetCounts;
+
+    public ProportionalEnumerable(IReadOnlyDictionary<T, int> targetCounts)
+    {
+        this.targetCounts = targetCounts;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerator<T> GetEnumerator() => new ProportionalEnumerator(targetCounts);
+
+    private sealed class ProportionalEnumerator : IEnumerator<T>
+    {
+        private readonly IReadOnlyDictionary<T, int> targetCounts;
+        private readonly Dictionary<T, int> actualCounts = new();
+        private readonly StaticPriorityQueue<ProportionalEnumeratorKey, T> priorityQueue = new();
+
+        public T Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public ProportionalEnumerator(IReadOnlyDictionary<T, int> targetCounts)
+        {
+            this.targetCounts = targetCounts;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            actualCounts.Clear();
+            priorityQueue.Clear();
+            foreach (var (obj, targetCount) in targetCounts)
+            {
+                var priority = new ProportionalEnumeratorKey(targetCount, targetCount);
+                priorityQueue.Enqueue(priority, obj);
+            }
+
+            Current = default;
+        }
+
+        public bool MoveNext()
+        {
+            if (priorityQueue.Count == 0)
+            {
+                return false;
+            }
+
+            var (priority, obj) = priorityQueue.Dequeue();
+            Current = obj;
+            var currentCount = actualCounts.GetValueOrDefault(obj, 0);
+            var newCount = currentCount + 1;
+            actualCounts[obj] = newCount;
+
+            if (newCount >= priority.TotalRequested)
+            {
+                return true;
+            }
+
+            var newPriority = priority with { Ratio = priority.TotalRequested / (1.0 + newCount) };
+            priorityQueue.Enqueue(newPriority, obj);
+
+            return true;
+        }
+
+        public void Dispose() { }
+    }
+
+    private readonly record struct ProportionalEnumeratorKey(double Ratio, double TotalRequested)
+        : IComparable<ProportionalEnumeratorKey>
+    {
+        public int CompareTo(ProportionalEnumeratorKey other)
+        {
+            var ratioComparison = Ratio.CompareTo(other.Ratio);
+            if (ratioComparison != 0)
+            {
+                return -ratioComparison;
+            }
+
+            return TotalRequested.CompareTo(other.TotalRequested);
+        }
+    }
+}
