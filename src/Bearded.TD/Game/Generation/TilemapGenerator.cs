562b11a6b588ee00c7232c961102b7c5b094b1e2
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-07-24 20:07:21 +0200

    🔥 Delete old level generators

--- src/Bearded.TD/Game/Generation/TilemapGenerator.cs
+++ src/Bearded.TD/Game/Generation/TilemapGenerator.cs
@@ -10,8 +10,6 @@ static class TilemapGenerator
     {
         return method switch
         {
-            LevelGenerationMethod.Legacy => new DefaultLevelGenerator(logger, metadata),
-            LevelGenerationMethod.Perlin => new PerlinLevelGenerator(logger, metadata),
             LevelGenerationMethod.Empty => new EmptyLevelGenerator(),
             LevelGenerationMethod.Semantic => new SemanticLevelGenerator(logger, metadata),
             _ => throw new ArgumentOutOfRangeException(nameof(method), "Unsupported method")
