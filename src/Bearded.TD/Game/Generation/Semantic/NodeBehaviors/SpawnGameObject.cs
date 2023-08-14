820befecceb7cf6c0c49f5d6c9b8e658f1828463
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-08-14 20:18:53 +0200

    💄 Simplify game object spawning

--- src/Bearded.TD/Game/Generation/Semantic/NodeBehaviors/SpawnGameObject.cs
+++ src/Bearded.TD/Game/Generation/Semantic/NodeBehaviors/SpawnGameObject.cs
@@ -6,16 +6,13 @@ using Bearded.TD.Game.Simulation.Buildings;
 using Bearded.TD.Game.Simulation.GameObjects;
 using Bearded.TD.Game.Simulation.World;
 using Bearded.TD.Tiles;
-using Bearded.TD.Utilities;
-using Bearded.Utilities.Geometry;
 using Bearded.Utilities.SpaceTime;
 using JetBrains.Annotations;
 
 namespace Bearded.TD.Game.Generation.Semantic.NodeBehaviors;
 
 using static ObjectPlacement;
-using static SpawnGameObject.AlignmentMode;
-using static SpawnGameObject.RotationMode;
+using static ObjectPositioning;
 
 [NodeBehavior("spawnGameObject")]
 sealed class SpawnGameObject : NodeBehavior<SpawnGameObject.BehaviorParameters>
@@ -26,7 +23,7 @@ sealed class SpawnGameObject : NodeBehavior<SpawnGameObject.BehaviorParameters>
     {
         foreach (var location in getLocations(context))
         {
-            var direction = getDirection(context);
+            var direction = Parameters.Rotation.ToDirection(context.Random);
             context.Content.PlaceGameObject(Parameters.Blueprint, location, direction);
         }
     }
@@ -35,36 +32,15 @@ sealed class SpawnGameObject : NodeBehavior<SpawnGameObject.BehaviorParameters>
     {
         return Parameters.Placement
             .ToTiles(context, Parameters.Count, tileValidForFootprintPredicate(context))
-            .Select(tile => getPositionWithinTile(tile, context));
+            .Select(tile => Parameters.Alignment.ToPosition(tile, context.Random).WithZ(Parameters.Z));
     }
 
-    private Direction2 getDirection(NodeGenerationContext context)
-    {
-        return Parameters.Rotation switch
-        {
-            FixedDirection => Direction2.Zero,
-            RandomDirection => Direction2.FromDegrees(context.Random.NextSingle() * 360),
-            _ => throw new ArgumentOutOfRangeException($"Unhandled rotation mode: {Parameters.Rotation}")
-        };
-    }
 
     private Predicate<Tile> tileValidForFootprintPredicate(NodeGenerationContext context)
     {
         var footprint = Parameters.Blueprint.GetFootprint();
         var area = context.Tiles.All;
         return tile => footprint.Positioned(tile, Orientation.Default).OccupiedTiles.All(area.Contains);
-    }
-
-    private Position3 getPositionWithinTile(Tile tile, NodeGenerationContext context)
-    {
-        var pos2 = Parameters.Alignment switch
-        {
-            CenterOfTile => Level.GetPosition(tile),
-            RandomlyInTile => Level.GetPosition(tile) +
-                GeometricRandom.UniformRandomPointOnDisk(context.Random, Constants.Game.World.HexagonInnerRadius),
-            _ => throw new ArgumentOutOfRangeException($"Unhandled alignment mode: {Parameters.Alignment}")
-        };
-        return pos2.WithZ(Parameters.Z);
     }
 
     [UsedImplicitly]
@@ -75,16 +51,4 @@ sealed class SpawnGameObject : NodeBehavior<SpawnGameObject.BehaviorParameters>
         RotationMode Rotation,
         Unit Z,
         int? Count);
-
-    public enum AlignmentMode
-    {
-        CenterOfTile = 0,
-        RandomlyInTile,
-    }
-
-    public enum RotationMode
-    {
-        FixedDirection = 0,
-        RandomDirection,
-    }
 }
