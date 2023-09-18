567d97b53e3d82041080aa655504d9c80e2635d0
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-09-18 18:11:56 +0200

    🔥 Delete old FluentSourceGen code

--- src/Bearded.FluentSourceGen/Core/MethodBuilderBase.cs
+++
@@ -1,7 +0,0 @@
-namespace Bearded.FluentSourceGen
-{
-    public abstract partial class MethodBuilderBase<TMethodBuilder> where TMethodBuilder : MethodBuilderBase<TMethodBuilder>
-    {
-        protected abstract TMethodBuilder This { get; }
-    }
-}
