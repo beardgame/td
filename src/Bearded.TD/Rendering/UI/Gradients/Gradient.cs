7e8b026d4850c91f4e6f5ed582e5be1d3c5c0a22
Paul C. Scharf <paul@paulscharf.com>
2024-02-27 00:28:57 +0000

    🌈 Add gradient models

---
+++ src/Bearded.TD/Rendering/UI/Gradients/Gradient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Bearded.Graphics;
+
+namespace Bearded.TD.Rendering.UI.Gradients;
+
+readonly struct GradientStop(float position, Color color)
+{
+    public readonly float Position = position;
+    public readonly Color Color = color;
+
+    public static implicit operator GradientStop((float position, Color color) tuple)
+        => new(tuple.position, tuple.color);
+}
+
+readonly ref struct Gradient(ReadOnlySpan<GradientStop> stops)
+{
+    public ReadOnlySpan<GradientStop> Stops { get; } = stops;
+}
---
+++ src/Bearded.TD/Rendering/UI/Gradients/GradientParameters.cs
@@ -0,0 +1,41 @@
+﻿using Bearded.Graphics;
+using OpenTK.Mathematics;
+
+namespace Bearded.TD.Rendering.UI.Gradients;
+
+readonly struct GradientId(uint value)
+{
+    public readonly uint Value = value;
+}
+
+enum GradientType : byte
+{
+    Constant = 0,
+    Linear = 1,
+    Radial = 2,
+    AlongEdgeNormal = 3,
+}
+
+readonly struct GradientParameters
+{
+    private readonly uint type1gradientIndex3;
+    private readonly Vector4 parameters;
+
+    private GradientParameters(GradientType type, GradientId gradientId, Vector4 parameters)
+    {
+        type1gradientIndex3 = (uint) type | (gradientId.Value << 8);
+        this.parameters = parameters;
+    }
+
+    public static GradientParameters Constant(Color color)
+        => new(GradientType.Constant, default, color.AsRGBAVector);
+
+    public static GradientParameters Linear(GradientId gradientId, Vector2 start, Vector2 end)
+        => new(GradientType.Linear, gradientId, (start.X, start.Y, end.X, end.Y));
+
+    public static GradientParameters Radial(GradientId gradientId, Vector2 center, float radius)
+        => new(GradientType.Radial, gradientId, (center.X, center.Y, radius, 0));
+
+    public static GradientParameters AlongEdgeNormal(GradientId gradientId)
+        => new(GradientType.AlongEdgeNormal, gradientId, default);
+}
