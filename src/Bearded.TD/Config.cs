646eaabcee21266ab3f5a40b60ccdf6083e3b1bb
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-03-25 19:35:05 +0100

    ⬆️ Increment version string to v0.26

--- src/Bearded.TD/Config.cs
+++ src/Bearded.TD/Config.cs
@@ -8,7 +8,7 @@ namespace Bearded.TD;
 
 static class Config
 {
-    public const string BaseVersionString = "0.26";
+    public const string BaseVersionString = "0.27";
 
 #if DEBUG
     private const string versionSuffix = "-dev";
