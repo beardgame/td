c684d659710a390a3bb8fe1e0afed50c452ea732
Paul C. Scharf <paul@paulscharf.com>
2024-05-13 22:06:39 +0100

    👾 Render enemies with deferred sprites

---
+++ src/Bearded.TD/assets/mods/default/gfx/shaders/unit.fs
@@ -0,0 +1,31 @@
+﻿#version 150
+
+uniform sampler2D diffuse;
+uniform sampler2D normal;
+
+in vec2 fragmentUV;
+in vec4 fragmentColor;
+in float fragmentDepth;
+
+out vec4 outRGBA;
+out vec4 outNormal;
+out vec4 outDepth;
+
+void main()
+{
+	vec4 rgba = texture(diffuse, fragmentUV) * fragmentColor;
+
+	if (rgba.a < 0.001)
+		discard;
+		
+	vec3 normal = texture(normal, fragmentUV).xyz * 2 - 1;
+	
+	vec2 unitX = normalize(dFdx(fragmentUV));
+	vec2 unitY = -normalize(dFdy(fragmentUV));
+
+	vec3 rotatedNormal = vec3(unitX * normal.x + unitY * normal.y, normal.z);
+
+	outRGBA =  rgba;
+	outNormal = vec4(rotatedNormal * 0.5 + 0.5, 1) *  rgba.a;
+	outDepth = vec4(fragmentDepth, 0, 0, 1);
+}
---
+++ src/Bearded.TD/assets/mods/default/gfx/shaders/unit.vs
@@ -0,0 +1,28 @@
+﻿#version 150
+
+uniform mat4 projection;
+uniform mat4 view;
+
+uniform float farPlaneDistance;
+
+in vec3 v_position;
+in vec2 v_texcoord;
+in vec4 v_color;
+
+out vec3 fragmentPosition;
+out vec2 fragmentUV;
+out vec4 fragmentColor;
+out float fragmentDepth;
+
+
+void main()
+{
+	vec4 viewPosition = view * vec4(v_position, 1.0);
+	vec4 position = projection * viewPosition;
+	gl_Position = position;
+
+	fragmentColor = v_color;
+	fragmentUV = v_texcoord;
+	fragmentDepth = -viewPosition.z / farPlaneDistance;
+	fragmentPosition = v_position;
+}
