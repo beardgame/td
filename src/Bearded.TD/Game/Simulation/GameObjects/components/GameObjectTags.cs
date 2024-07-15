0bb1bdbc5cac99b6341ae73b8dae4ec2b32214ad
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-07-15 22:07:18 +0200

    ❌ Forbid combining charged and incendiary projectiles

--- src/Bearded.TD/Game/Simulation/GameObjects/components/GameObjectTags.cs
+++ src/Bearded.TD/Game/Simulation/GameObjects/components/GameObjectTags.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using Bearded.TD.Utilities.Collections;
-using Bearded.Utilities.SpaceTime;
+using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
 
 namespace Bearded.TD.Game.Simulation.GameObjects;
 
@@ -17,6 +18,11 @@ sealed class GameObjectTags : Component
 
     public void AddRange(IEnumerable<string> tagsToAdd)
     {
+        foreach (var se in tagsToAdd)
+        {
+            Console.WriteLine($"Adding tag {se}");
+        }
+
         tagsToAdd.ForEach(t => tags.Add(t));
     }
 }
