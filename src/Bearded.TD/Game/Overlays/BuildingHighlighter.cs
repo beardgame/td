35f2f069f979b73f467ba67a46f19f1af2df93b4
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-07-15 20:01:07 +0200

    🌟 Add helper class to highlight towers

---
+++ src/Bearded.TD/Game/Overlays/BuildingHighlighter.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using Bearded.Graphics;
+using Bearded.TD.Game.Simulation.Footprints;
+using Bearded.TD.Game.Simulation.GameObjects;
+
+namespace Bearded.TD.Game.Overlays;
+
+sealed class BuildingHighlighter(ActiveOverlays activeOverlays)
+{
+    public IHighlightedBuilding HighlightBuilding(GameObject obj)
+    {
+        if (!obj.TryGetTilePresence(out var presence) || !presence.OccupiedTiles.Any())
+        {
+            return NoopHighlightedBuilding.Instance;
+        }
+
+        var overlay = new HighlightedBuildingOverlay(presence);
+        var activeOverlay = activeOverlays.Activate(overlay);
+        return new HighlightedBuilding(activeOverlay);
+    }
+
+    private class HighlightedBuilding(IActiveOverlay overlay) : IHighlightedBuilding
+    {
+        public void EndHighlight()
+        {
+            overlay.Deactivate();
+        }
+    }
+
+    private class HighlightedBuildingOverlay(ITilePresence tilePresence) : IOverlayLayer
+    {
+        public DrawOrder DrawOrder => DrawOrder.TowerHighlight;
+
+        public void Draw(IOverlayDrawer context)
+        {
+            foreach (var tile in tilePresence.OccupiedTiles)
+            {
+                context.Tile(Color.DeepPink, tile);
+            }
+        }
+    }
+
+    private class NoopHighlightedBuilding : IHighlightedBuilding
+    {
+        public static readonly NoopHighlightedBuilding Instance = new();
+
+        private NoopHighlightedBuilding() { }
+
+        public void EndHighlight() { }
+    }
+
+    public interface IHighlightedBuilding
+    {
+        void EndHighlight();
+    }
+}
