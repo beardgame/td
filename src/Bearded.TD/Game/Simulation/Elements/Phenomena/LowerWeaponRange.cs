adc44a02d88811f1702f3288f59d25b2d1af97ae
Paul C. Scharf <paul@paulscharf.com>
2025-05-05 22:07:57 +0100

    👓 Make alchemy sprinter reduce bumped tower range

---
+++ src/Bearded.TD/Game/Simulation/Elements/Phenomena/LowerWeaponRange.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Bearded.TD.Game.Simulation.GameObjects;
+using Bearded.TD.Game.Simulation.StatusDisplays;
+using Bearded.TD.Game.Simulation.Upgrades;
+using Bearded.TD.Shared.TechEffects;
+using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
+
+namespace Bearded.TD.Game.Simulation.Elements.Phenomena;
+
+static class LowerWeaponRange
+{
+    public readonly record struct Effect(double Factor, TimeSpan Duration) : IElementalEffect
+    {
+        IElementalPhenomenon IElementalEffect.Phenomenon => phenomenon;
+    }
+
+    private static readonly IElementalPhenomenon phenomenon = new Phenomenon();
+
+    private sealed class Phenomenon : IElementalPhenomenon
+    {
+        public Type EffectType => typeof(Effect);
+
+        public IElementalPhenomenon.IScope NewScope(GameObject target) => new Scope(target);
+    }
+
+    private sealed class Scope(GameObject target) : ElementalPhenomenonScopeBase<Effect>(target)
+    {
+        private IUpgradeReceipt? receipt;
+
+        protected override bool TryChooseEffect(out Effect effect)
+        {
+            var effects = ActiveEffects.ToImmutableArray();
+            if (effects.IsEmpty)
+            {
+                effect = default;
+                return false;
+            }
+
+            effect = effects.MinBy(e => e.Factor);
+            return true;
+        }
+
+        protected override void ApplyEffectTick(GameObject target, Effect effect)
+        {
+            receipt?.Rollback();
+            var upgrade = Upgrade.FromEffects(createUpgradeEffect(effect));
+            if (!target.CanApplyUpgrade(upgrade)) return;
+            receipt = target.ApplyUpgrade(upgrade);
+        }
+
+        private static IUpgradeEffect createUpgradeEffect(Effect effect)
+        {
+            return new ModifyParameter(
+                AttributeType.Range,
+                Modification.MultiplyWith(effect.Factor),
+                UpgradePrerequisites.Empty,
+                false);
+        }
+
+        protected override void StartEffect(GameObject target)
+        {
+        }
+
+        protected override void EndEffect(GameObject target)
+        {
+            receipt?.Rollback();
+            receipt = null;
+        }
+
+        protected override ElementalStatus MakeStatus(Blueprints blueprints)
+        {
+            return new ElementalStatus("eye-disabled".ToStatusIconSpriteId());
+        }
+    }
+}
---
+++ src/Bearded.TD/Game/Simulation/Modules/LowerBuildingRange.cs
@@ -0,0 +1,31 @@
+﻿using Bearded.TD.Game.Commands;
+using Bearded.TD.Game.Simulation.Buildings;
+using Bearded.TD.Game.Simulation.GameObjects;
+using Bearded.TD.Shared.Events;
+using Bearded.TD.Shared.TechEffects;
+using Bearded.Utilities.SpaceTime;
+
+namespace Bearded.TD.Game.Simulation.Modules;
+
+[Component("lowerBumpedBuildingRange")]
+sealed class LowerWeaponRange(LowerWeaponRange.IParameters parameters)
+    : Component<LowerWeaponRange.IParameters>(parameters), IListener<BumpedBuilding>
+{
+    public interface IParameters : IParametersTemplate<IParameters>
+    {
+        TimeSpan Duration { get; }
+
+        [Modifiable(0.5f)]
+        double Factor { get; }
+    }
+
+    public override void Activate()
+    {
+        Events.Subscribe(this);
+    }
+
+    public void HandleEvent(BumpedBuilding e)
+    {
+        Owner.Sync(LowerBuildingRange.Command, e.Building, Parameters.Duration, Parameters.Factor);
+    }
+}
