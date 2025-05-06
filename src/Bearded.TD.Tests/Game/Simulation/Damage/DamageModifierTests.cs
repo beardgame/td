2568aa42e33055c8ec9b2e349e967ac6bcf8bcfd
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-05-06 17:59:47 +0200

    ✅ Add tests to ensure damage modifiers are well-ordered

---
+++ src/Bearded.TD.Tests/Game/Simulation/Damage/DamageModifierTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Bearded.TD.Game.Simulation.Damage;
+using FluentAssertions;
+using Xunit;
+
+namespace Bearded.TD.Tests.Game.Simulation.Damage;
+
+public sealed class DamageModifierTests
+{
+    public static IEnumerable<Type> GetAllDamageModifierTypes()
+    {
+        return AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes()
+                .Where(t => t.IsAssignableTo(typeof(IDamageModifier))))
+            .Where(type => type is { IsClass: true, IsAbstract: false });
+    }
+
+    [Fact]
+    public void AllDamageModifiersAreInExplicitOrder()
+    {
+        var orderedDamageModifiers = DamageModifiers.Order;
+        var allDamageModifiers = GetAllDamageModifierTypes().ToImmutableArray();
+
+        allDamageModifiers.Should().BeEquivalentTo(orderedDamageModifiers);
+    }
+}
