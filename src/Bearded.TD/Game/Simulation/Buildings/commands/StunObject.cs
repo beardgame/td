b169e5caa523f41f3ee2d59edc44a69fd534ed57
Paul C. Scharf <paul@paulscharf.com>
2025-05-25 22:40:50 +0100

    🧹

--- src/Bearded.TD/Game/Simulation/Buildings/commands/StunObject.cs
+++ src/Bearded.TD/Game/Simulation/Buildings/commands/StunObject.cs
@@ -1,62 +1,16 @@
-﻿using Bearded.TD.Commands;
-using Bearded.TD.Commands.Serialization;
-using Bearded.TD.Game.Simulation.Elements;
+﻿using Bearded.TD.Game.Simulation.Elements;
 using Bearded.TD.Game.Simulation.Elements.Phenomena;
 using Bearded.TD.Game.Simulation.GameObjects;
-using Bearded.TD.Networking.Serialization;
-using Bearded.TD.Utilities;
-using Bearded.Utilities;
+using Bearded.TD.Shared.Commands;
 using Bearded.Utilities.SpaceTime;
-using JetBrains.Annotations;
 
 namespace Bearded.TD.Game.Simulation.Buildings;
 
-static class StunObject
+[GenerateCommand]
+static partial class StunObject
 {
-    public static ISerializableCommand<GameInstance> Command(GameObject obj, TimeSpan duration) =>
-        new Implementation(obj, duration);
-
-    private sealed class Implementation : ISerializableCommand<GameInstance>
+    private static void execute(GameObject obj, TimeSpan duration)
     {
-        private readonly GameObject obj;
-        private readonly TimeSpan duration;
-
-        public Implementation(GameObject obj, TimeSpan duration)
-        {
-            this.obj = obj;
-            this.duration = duration;
-        }
-
-        public void Execute()
-        {
-            obj.TryApplyEffect(new Stunned.Effect(duration));
-        }
-
-        ICommandSerializer<GameInstance> ISerializableCommand<GameInstance>.Serializer => new Serializer(obj, duration);
-    }
-
-    private sealed class Serializer : ICommandSerializer<GameInstance>
-    {
-        private Id<GameObject> obj;
-        private double duration;
-
-        [UsedImplicitly] public Serializer() { }
-
-        public Serializer(GameObject obj, TimeSpan duration)
-        {
-            this.obj = obj.FindId();
-            this.duration = duration.NumericValue;
-        }
-
-        public ISerializableCommand<GameInstance> GetCommand(GameInstance game)
-        {
-            return new Implementation(game.State.Find(obj), duration.S());
-        }
-
-        public void Serialize(INetBufferStream stream)
-        {
-            stream.Serialize(ref obj);
-            stream.Serialize(ref duration);
-        }
+        obj.TryApplyEffect(new Stunned.Effect(duration));
     }
 }
--- src/Bearded.TD/Game/Synchronization/SerializerConverters.cs
+++ src/Bearded.TD/Game/Synchronization/SerializerConverters.cs
@@ -1,6 +1,8 @@
 ﻿using Bearded.TD.Game.Simulation.GameObjects;
 using Bearded.TD.Shared.Commands;
+using Bearded.TD.Utilities;
 using Bearded.Utilities;
+using TimeSpan = Bearded.Utilities.SpaceTime.TimeSpan;
 
 namespace Bearded.TD.Game.Synchronization;
 
@@ -12,4 +14,11 @@ static class SerializerConverters
                 obj => obj.FindId(),
                 (id, game) => game.State.Find(id)
             );
+
+    [SerializerConverter]
+    public static readonly ISerializerConverter<TimeSpan, double, GameInstance> TimeSpan =
+        new SerializerConverter<TimeSpan, double, GameInstance>(
+            t => t.NumericValue,
+            (d, _) => d.S()
+        );
 }
