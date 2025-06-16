2c4f6d06acdc62fdc6772f7ef6509922b40b4206
Tom Rijnbeek <tom@tomrijnbeek.me>
2025-06-16 21:21:20 +0200

    ✅ Add tests for validating enemy form deserialization

--- src/Bearded.TD.Tests/Content/ModIntegrationTests.cs
+++ src/Bearded.TD.Tests/Content/ModIntegrationTests.cs
@@ -1,21 +1,25 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Bearded.TD.Content.Mods;
-using Bearded.TD.Content.Serialization.Models;
+using Bearded.TD.Game;
+using Bearded.TD.Game.Simulation.Enemies;
+using Bearded.TD.Game.Simulation.GameObjects;
+using Bearded.TD.Game.Simulation.GameObjects.Parameters;
+using Bearded.Utilities.SpaceTime;
 using FluentAssertions;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace Bearded.TD.Tests.Content;
 
 public sealed class ModIntegrationTests : IAsyncLifetime
 {
-    private const string modForTestingPath = "assets/mods/mod-for-testing";
+    private const string testModId = "mod-for-testing";
 
     private Mod testMod = null!;
+    private Blueprints blueprints = null!;
 
     public async ValueTask InitializeAsync()
     {
@@ -35,12 +39,8 @@ public sealed class ModIntegrationTests : IAsyncLifetime
             loadedMods.Add(modForLoading.GetLoadedMod());
         }
 
-        var testModMetadataJson =
-            JsonConvert.DeserializeObject<Metadata>(await File.ReadAllTextAsync($"{modForTestingPath}/mod.json5"))!;
-        var testModMetadata = new ModMetadata(testModMetadataJson, new DirectoryInfo(modForTestingPath));
-        var testModForLoading = new ModForLoading(testModMetadata);
-        await testModForLoading.Load(context, loadedMods.AsReadOnly());
-        testMod = testModForLoading.GetLoadedMod();
+        testMod = loadedMods.Single(m => m.Id == testModId);
+        blueprints = Blueprints.Merge(loadedMods.Select(m => m.Blueprints));
     }
 
     public ValueTask DisposeAsync()
@@ -52,5 +52,34 @@ public sealed class ModIntegrationTests : IAsyncLifetime
     public void ModIsLoaded()
     {
         testMod.Name.Should().Be("Mod for testing");
+    }
+
+    [Fact]
+    public void EnemyFormRecipeIsLoaded()
+    {
+        var spawnEnemiesOnKilled = gameObjectInstance("spawn-enemies-on-killed");
+
+        var component = spawnEnemiesOnKilled.GetComponents<SpawnEnemiesOnKilled>().SingleOrDefault();
+
+        component.Should().NotBeNull();
+        var parameters = extractParameters(component);
+        var form = parameters.Form.ToForm(blueprints);
+        form.Blueprint.Should().Be(blueprints.GameObjects[ModAwareId.FromFullySpecified("default.swarm-enemy")]);
+        form.Modules[SocketShape.FromLiteral("minionBuff")].Should()
+            .Be(blueprints.Modules[ModAwareId.FromFullySpecified("default.minion-buff-fire")]);
+    }
+
+    private GameObject gameObjectInstance(string name) =>
+        GameObjectFactory.CreateFromBlueprintWithoutRenderer(gameObjectBlueprint(name), null, Position3.Zero);
+
+    private IGameObjectBlueprint gameObjectBlueprint(string name) => blueprints.GameObjects[id(name)];
+
+    private ModAwareId id(string name) => ModAwareId.FromFullySpecified($"{testMod.Id}.{name}");
+
+    private static T extractParameters<T>(Component<T> component) where T : IParametersTemplate<T>
+    {
+        var parametersProperty =
+            component.GetType().GetProperty("Parameters", BindingFlags.Instance | BindingFlags.NonPublic);
+        return (T) parametersProperty!.GetValue(component)!;
     }
 }
